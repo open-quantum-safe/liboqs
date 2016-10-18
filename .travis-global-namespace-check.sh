dea40be1b48cef20aa76488abfc167ee5dc5b956
Douglas Stebila <douglas@stebila.ca>
2016-10-17 22:11:49 -0400

    Another hopeful fix for travis global namespace issue.

--- .travis-global-namespace-check.sh
+++ .travis-global-namespace-check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $(nm -g liboqs.a | grep ' T ' | grep -v -i ' T [_]?OQS') ]]; 
+if [[ $(nm -g liboqs.a | grep ' T ' | grep -E -v -i ' T [_]?OQS') ]]; 
 then 
 	tput setaf 1;
 	echo "Code contains the following non-namespaced global symbols; see https://github.com/open-quantum-safe/liboqs/wiki/Coding-conventions for function naming conventions.";
