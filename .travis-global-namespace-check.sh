4475c73d9490a16d5d55185410139f8df1222dd3
Douglas Stebila <douglas@stebila.ca>
2016-10-17 21:31:28 -0400

    Handle symbol naming on both Linux and macOS.

--- .travis-global-namespace-check.sh
+++ .travis-global-namespace-check.sh
@@ -4,7 +4,7 @@ if [[ $(nm -g liboqs.a | grep ' T ' | grep -v -i ' T _OQS') ]];
 then 
 	tput setaf 1;
 	echo "Code contains the following non-namespaced global symbols; see https://github.com/open-quantum-safe/liboqs/wiki/Coding-conventions for function naming conventions.";
-	nm -g liboqs.a | grep ' T ' | grep -v -i ' T _OQS'
+	nm -g liboqs.a | grep ' T ' | grep -E -v -i ' T [_]?OQS'
 	exit 1;
 else 
 	tput setaf 2;
