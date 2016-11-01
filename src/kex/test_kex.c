20ecde9692444957ff645fdd0ace3e8a22f0343f
Alex Parent <alexparent@gmail.com>
2016-10-31 21:02:02 -0400

    Add option to only run tests/benchmarks for specified algorithms. (#44)
    * Add option to only run tests/benchmarks for a single algorithm.
    
    * Allow multiple algorithms to be specified on command-line.

--- src/kex/test_kex.c
+++ src/kex/test_kex.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include <oqs/rand.h>
 #include <oqs/kex.h>
@@ -12,13 +13,15 @@ struct kex_testcase {
 	unsigned char *seed;
 	size_t seed_len;
 	char *named_parameters;
+	char *id;
+	int run;
 };
 
 /* Add new testcases here */
 struct kex_testcase kex_testcases[] = {
-	{ OQS_KEX_alg_rlwe_bcns15, NULL, 0, NULL },
-	{ OQS_KEX_alg_rlwe_newhope, NULL, 0, NULL },
-	{ OQS_KEX_alg_lwe_frodo, (unsigned char *) "01234567890123456", 16, "recommended" },
+	{ OQS_KEX_alg_rlwe_bcns15, NULL, 0, NULL, "rlwe_bcns15", 0 },
+	{ OQS_KEX_alg_rlwe_newhope, NULL, 0, NULL, "rlwe_newhope", 0 },
+	{ OQS_KEX_alg_lwe_frodo, (unsigned char *) "01234567890123456", 16, "recommended", "lwe_frodo_recommended", 0 },
 };
 
 #define KEX_TEST_ITERATIONS 100
@@ -243,9 +246,30 @@ cleanup:
 
 }
 
-int main() {
+int main(int argc, char **argv) {
 
-	int success;
+	int success = 1;
+	bool run_all = true;
+	size_t kex_testcases_len = sizeof(kex_testcases) / sizeof(struct kex_testcase);
+
+	if (argc > 1) {
+		if ((strcmp(argv[1], "-h") == 0) || (strcmp(argv[1], "-help") == 0) || (strcmp(argv[1], "--help") == 0)) {
+			printf("Usage: ./test_kex [algorithms]\n\n");
+			printf("algorithms:\n");
+			for (size_t i = 0; i < kex_testcases_len; i++) {
+				printf("  %s\n", kex_testcases[i].id);
+			}
+			return EXIT_SUCCESS;
+		}
+		run_all = false;
+		for (int i = 1; i < argc; i++) {
+			for (size_t j = 0; j < kex_testcases_len; j++) {
+				if (strcmp(argv[i], kex_testcases[j].id) == 0) {
+					kex_testcases[j].run = 1;
+				}
+			}
+		}
+	}
 
 	/* setup RAND */
 	OQS_RAND *rand = OQS_RAND_new(OQS_RAND_alg_urandom_chacha20);
@@ -253,9 +277,10 @@ int main() {
 		goto err;
 	}
 
-	size_t kex_testcases_len = sizeof(kex_testcases) / sizeof(struct kex_testcase);
 	for (size_t i = 0; i < kex_testcases_len; i++) {
-		success = kex_test_correctness_wrapper(rand, kex_testcases[i].alg_name, kex_testcases[i].seed, kex_testcases[i].seed_len, kex_testcases[i].named_parameters, KEX_TEST_ITERATIONS);
+		if (run_all || kex_testcases[i].run == 1) {
+			success = kex_test_correctness_wrapper(rand, kex_testcases[i].alg_name, kex_testcases[i].seed, kex_testcases[i].seed_len, kex_testcases[i].named_parameters, KEX_TEST_ITERATIONS);
+		}
 		if (success != 1) {
 			goto err;
 		}
@@ -263,7 +288,9 @@ int main() {
 
 	PRINT_TIMER_HEADER
 	for (size_t i = 0; i < kex_testcases_len; i++) {
-		kex_bench_wrapper(rand, kex_testcases[i].alg_name, kex_testcases[i].seed, kex_testcases[i].seed_len, kex_testcases[i].named_parameters, KEX_BENCH_SECONDS);
+		if (run_all || kex_testcases[i].run == 1) {
+			kex_bench_wrapper(rand, kex_testcases[i].alg_name, kex_testcases[i].seed, kex_testcases[i].seed_len, kex_testcases[i].named_parameters, KEX_BENCH_SECONDS);
+		}
 	}
 	PRINT_TIMER_FOOTER
 
