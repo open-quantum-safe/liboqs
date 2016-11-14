20b1f0809ad3401851c06bfdaa5939b8fcb1b12f
Alex Parent <aparent@uwaterloo.ca>
2016-11-14 16:27:01 -0500

    Revert the previous change in 250681196f
    Make style check fail when astyle fails.

--- .travis-banned-functions-check.sh
+++ .travis-banned-functions-check.sh
@@ -6,6 +6,7 @@ if [[ $(find -E . -name '*.[ch]' -exec grep -H bzero {} \;) ]];
 then 
 	tput setaf 1;
 	echo "Code uses banned functions (bzero).";
+	tput sgr 0
 	retvalue=1;
 fi;
 
@@ -15,6 +16,7 @@ if [[ $retvalue == 0 ]];
 then
 	tput setaf 2;
 	echo "Code does not use banned functions.";
+	tput sgr 0
 fi;
 
 exit $retvalue;
