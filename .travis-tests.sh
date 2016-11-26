d5198776c3cd5a931c68dfc74fb1841046442ac6
smashra <shravan.mishra@gmail.com>
2016-11-26 10:10:17 -0500

    Fix enable disable macro logic

--- .travis-tests.sh
+++ .travis-tests.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
+: '
+Below is an example of the the logic how enable disable macros are implemented
+in the code. Use it as a guide to enable disable macros below
+
+void OQS_AES128_free_schedule(void *schedule) {
+#ifdef USE_OPENSSL
+	oqs_aes128_free_schedule_ossl(schedule);
+#else
+#ifndef AES_DISABLE_NI
+	oqs_aes128_free_schedule_ni(schedule);
+#else
+	oqs_aes128_free_schedule_c(schedule);
+#endif
+#endif
+}
+'
 
 autoreconf -i
-if [[ ${USE_OPENSSL} == 1 &&  ${AES_NI} == 0 ]];then 
-  ./configure --disable-aes-ni; make clean; make; make test
-  ./configure --enable-openssl; make clean; make; make test  
-else
-  ./configure; make; make test
-  ./configure --disable-aes-ni; make clean; make; make test
-  ./configure --enable-openssl; make clean; make; make test  
+enable_disable_str=
+if [[ ${USE_OPENSSL} == 1 ]];then
+  enable_disable_str="--enable-openssl"
 fi
+
+if [[ ${AES_NI} == 0 ]];then
+  enable_disable_str=${enable_disable_str}" --disable-aes-ni"
+fi
+
+
+./configure ${enable_disable_str}; make clean; make; make test
 
 bash .travis-style-check.sh
 bash .travis-global-namespace-check.sh
