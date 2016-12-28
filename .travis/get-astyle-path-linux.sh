58d2147960869ef11b151da69adc3d5f1d927740
Tancrède Lepoint <tancrede.lepoint@sri.com>
2016-12-28 17:49:19 -0500

    Correcting the script

--- .travis/get-astyle-path-linux.sh
+++ .travis/get-astyle-path-linux.sh
@@ -4,12 +4,13 @@
 # 
 
 get_astyle_path() {
-	if [ ! -x "$(which astyle)" ]; then return ""; fi;
-	# Download astyle
-	wget http://downloads.sourceforge.net/project/astyle/astyle/astyle%202.05.1/astyle_2.05.1_linux.tar.gz
-	tar xzf astyle_2.05.1_linux.tar.gz
-	cd astyle/build/gcc
-	make
-	# return astyle bin directory
-	return $(pwd)/bin
+	if [ ! -x "$(which astyle)" ]; then 
+		# Download astyle
+		wget http://downloads.sourceforge.net/project/astyle/astyle/astyle%202.05.1/astyle_2.05.1_linux.tar.gz
+		tar xzf astyle_2.05.1_linux.tar.gz
+		cd astyle/build/gcc
+		make
+		# return astyle bin directory
+		return $(pwd)/bin
+	fi;
 }
