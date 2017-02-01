ea9bd2faba21a37f69de202974d358b796e901ab
Shravan Mishra <s24mishra@uwaterloo.ca>
2017-02-01 11:02:36 -0500

    Reverting change

--- .travis/style-check.sh
+++ .travis/style-check.sh
@@ -7,20 +7,16 @@ else
 	CLANGFORMAT=clang-format-3.9 make prettyprint
 fi;
 
-modified=$(git status -s| awk '{print $1}')
+modified=$(git status -s)
 
-for  modi in $modified; do
-  if [ $modi = M ];
-  then
-    tput setaf 1;
-	  echo "Code does not adhere to the project standards. Run \"make prettyprint\".";
-	  tput sgr 0;
-	  exit 1;
-  fi
-done
-
-tput setaf 2;
-echo "Code adheres to the project standards (prettyprint).";
-tput sgr 0;
-exit 0;
-
+if [ "$modified" ]; then
+	tput setaf 1;
+	echo "Code does not adhere to the project standards. Run \"make prettyprint\".";
+	tput sgr 0;
+	exit 1;
+else
+	tput setaf 2;
+	echo "Code adheres to the project standards (prettyprint).";
+	tput sgr 0;
+	exit 0;
+fi;
