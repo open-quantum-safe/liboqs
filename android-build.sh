f3a9583846aca63556104dc45c50420472a94229
Vladimir Valyukh <vladimir.sensei@gmail.com>
2017-08-03 21:42:08 +0200

    andriod compilation cleanup wip

--- android-build.sh
+++ android-build.sh
@@ -6,13 +6,7 @@ COMPILE_ARCHITECTURES=("arm64")
 OLD_PATH=${PATH}
 for ARCH in ${COMPILE_ARCHITECTURES[@]}
 do
-  COMPILER_GROUP=""
   COMPILER_PREFIX=""
-  case ${ARCH} in
-    "arm64" )
-      COMPILER_GROUP=arm
-      ;;
-  esac
 
   export ANDROID_NDK_ROOT="/ram/arm"
 
@@ -21,25 +15,24 @@ do
 
   export PATH="${ANDROID_NDK_BIN}:${OLD_PATH}"
 
-  export CFLAGS="-pie -O3 -lm"
+  export CFLAGS="-O3 -lm"
 
   case ${ARCH} in
     "arm64" )
-      ABI_NAME=aarch64
       COMPILER_PREFIX=aarch64-linux-android
       ;;
   esac
 
 
-  export CC=${ANDROID_NDK_BIN}/${COMPILER_PREFIX}-gcc
-  export CPP=${ANDROID_NDK_BIN}/${COMPILER_PREFIX}-cpp
-  export CXX=${ANDROID_NDK_BIN}/${COMPILER_PREFIX}-g++
-  export LD=${ANDROID_NDK_BIN}/${COMPILER_PREFIX}-ld
-  export AR=${ANDROID_NDK_BIN}/${COMPILER_PREFIX}-ar
-  export RANLIB=${ANDROID_NDK_BIN}/${COMPILER_PREFIX}-ranlib
-  export STRIP=${ANDROID_NDK_BIN}/${COMPILER_PREFIX}-strip
+  #export CC=${ANDROID_NDK_BIN}/${COMPILER_PREFIX}-gcc
+  #export CPP=${ANDROID_NDK_BIN}/${COMPILER_PREFIX}-cpp
+  #export CXX=${ANDROID_NDK_BIN}/${COMPILER_PREFIX}-g++
+  #export LD=${ANDROID_NDK_BIN}/${COMPILER_PREFIX}-ld
+  #export AR=${ANDROID_NDK_BIN}/${COMPILER_PREFIX}-ar
+  #export RANLIB=${ANDROID_NDK_BIN}/${COMPILER_PREFIX}-ranlib
+  #export STRIP=${ANDROID_NDK_BIN}/${COMPILER_PREFIX}-strip
 
-  echo "---- Compiling for ${ARCH}"
+  #echo "---- Compiling for ${ARCH}"
 
   #make distclean
   ./configure --host="${COMPILER_PREFIX}" --disable-aes-ni --with-sysroot=${ANDROID_SYSROOT_DIR}
