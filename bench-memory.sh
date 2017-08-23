803ec374a5ccb6def487efcf7ec0e970bef5e6f8
Vladimir Valyukh <vladimir.sensei@gmail.com>
2017-08-23 17:43:47 +0200

    small fix in bench-memory script

--- bench-memory.sh
+++ bench-memory.sh
@@ -3,6 +3,7 @@
 DEFAULT_TMP_DIR=/tmp
 TMP_DIR=$DEFAULT_TMP_DIR
 ALGORITHMS=""
+ROOT_DIR=`dirname $0`
 
 #parse arguments
 for arg in "$@"
@@ -26,7 +27,7 @@ Usage: $0 [OPTION]... ALGORITHM
   --tmp-dir=DIR       temporary directory [default: $DEFAULT_TMP_DIR]
     ALGORITHM         algorithm to test
 
-Example usage: $0 
+Example usage: $0 ntru
 
 EOF
   exit 0
@@ -40,7 +41,7 @@ TMP_FILE_NAME="oqs_mem_bench"
 TMP_FILE_PATH=$TMP_DIR/$TMP_FILE_NAME
 
 rm -f $TMP_FILE_PATH
-valgrind --tool=massif --massif-out-file=$TMP_FILE_PATH ./test_kex -m $ALGORITHMS
+valgrind --tool=massif --massif-out-file=$TMP_FILE_PATH $ROOT_DIR/test_kex -m $ALGORITHMS
 ms_print $TMP_FILE_PATH
 rm -f $TMP_FILE_PATH
 
