986e960c3cab839e5bfd84b426d0731d0283d843
Vlad Gheorghiu <vsoftco@gmail.com>
2017-09-19 08:28:24 -0400

    commit

--- src/kex_lwe_frodo/kex_lwe_frodo_macrify.c
+++ src/kex_lwe_frodo/kex_lwe_frodo_macrify.c
@@ -1,8 +1,7 @@
+OQS_KEX *MACRIFY(OQS_KEX_lwe_frodo_new)(OQS_RAND *rand, const uint8_t *seed, const size_t seed_len, const char *named_parameters) {
 #if defined(WINDOWS)
 #define _CRT_NONSTDC_NO_DEPRECATE
 #endif
-OQS_KEX *MACRIFY(OQS_KEX_lwe_frodo_new)(OQS_RAND *rand, const uint8_t *seed, const size_t seed_len, const char *named_parameters) {
-
 	OQS_KEX *k;
 	struct oqs_kex_lwe_frodo_params *params;
 
