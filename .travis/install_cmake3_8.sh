d4b540eefdd7dd2631d9f5708d809dc50e970041
Vlad Gheorghiu <vsoftco@gmail.com>
2017-11-13 19:34:14 -0500

    commit

--- .travis/install_cmake3_8.sh
+++ .travis/install_cmake3_8.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+echo "Installing CMake 3.8 ..."
 wget http://www.cmake.org/files/v3.8/cmake-3.8.0.tar.gz
 tar zxf cmake-3.8.0.tar.gz
 cd cmake-3.8.0
-./configure
-make
-sudo make install
+./configure > /dev/null 2>&1
+make > /dev/null 2>&1
+sudo make install > /dev/null 2>&1
+echo "Done installing CMake 3.8"
