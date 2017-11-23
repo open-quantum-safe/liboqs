76cb9c929828004b5735e5ccfcfcb6c744725612
Eliott T <developpsoft@gmail.com>
2017-11-23 16:06:12 +0100

    Fix install script

--- .travis/install-clang-scan-build-linux.sh
+++ .travis/install-clang-scan-build-linux.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-#
-# Install clang-format on Linux
-# 
 
-if [ ! -x "$(which clang-format-3.9)" ]; then 
-	sudo add-apt-repository 'deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-3.9 main'
-	wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
-	sudo apt-get update -qq 
-	sudo apt-get install -qq -y scan-build
+if [ ! -x "$(which scan-build)" ]; then 
+	pip install scan-build
 fi;
