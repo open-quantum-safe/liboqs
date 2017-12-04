318d81e2e5cfb93059827f588253a62d5bf25ab4
Christian Paquin <cpaquin@microsoft.com>
2017-12-04 18:19:18 -0500

    Added force option to patch to avoid error when rebuilding.

--- src/sig_picnic/build_picnic.sh
+++ src/sig_picnic/build_picnic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p external/build
-patch -p1 -d external < oqs_sig_picnic.patch
+patch -p1 -f -d external < oqs_sig_picnic.patch
 cd external/build
 cmake -UWITH_LTO -DWITH_LTO:BOOL=OFF ..
 make
