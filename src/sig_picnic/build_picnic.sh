a1a042d1541f1c4b06cd8219a45f76bad8a82fc9
Christian Paquin <cpaquin@microsoft.com>
2017-12-12 16:58:59 -0500

    Updated external picnic, and modified how it is patched and built (to avoid multiple patching).

--- src/sig_picnic/build_picnic.sh
+++ src/sig_picnic/build_picnic.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 mkdir -p external/build
-patch -p1 -f -d external < oqs_sig_picnic.patch
+# check if patch has already been applied
+patch -p1 -N -f --dry-run --silent -d external < oqs_sig_picnic.patch 2>/dev/null
+if [ $? -eq 0 ];
+then
+    #apply the patch
+    echo Patching Picnic external
+    patch -p1 -N -f -d external < oqs_sig_picnic.patch
+fi
 cd external/build
 cmake -UWITH_LTO -DWITH_LTO:BOOL=OFF ..
 make
