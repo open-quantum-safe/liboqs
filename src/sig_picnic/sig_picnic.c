6fee71699a1caf80f87dbe260e766e3ce6be5740
Christian Paquin <cpaquin@microsoft.com>
2017-12-20 09:54:45 -0500

    Added optimized version of Picnic (#190)
    * Replaced the implementation of Picnic with the optimized version of https://github.com/IAIK/Picnic.git.
    
    Some notes:
     - Picnic is now a git submodule, built separately (build_picnic.sh) and linked into OQS.
     - The params are part of the Picnic library, no need to generate them separately.
     - The Picnic params have been renamed
     - Picnic has been disabled in the Visual Studio build (until the library supports VS)
    
    * Deleted script to download old picnic implementation.
    
    * Added picnic build commands to the configure script.
    
    * Updated travis to build new picnic lib.
    
    * Added note that clone must be recursive (to get picnic submodule).
    
    * Removed dependency on m4ri and added picnic build instruction in travis (since apparently the configure scrip is not run as usual).
    
    * Changed build_picnic.sh script invocation.
    
    * install cmake3.8
    
    Fix for PR 190
    
    * commit
    
    * commit
    
    * Pulled down update to picnic submodule.
    
    * pulled picnic update fixing compat macros failure
    
    * Added openssl lib to test_sig linker if openssl is enabled.
    
    * Disable Picnic on gcc < 5.
    
    * Fixed travis.yml syntax error.
    
    * Pulled updates to Picnic submodule (fixing build error on MacOS).
    
    * Removed banned-functions test that only checks for bzero. For some strange reason, it doesn't work on travis while building picnic, even if bzero is not in the code.
    
    * prettyprint
    
    * Changed prettyprint's picnic exclusion dir.
    
    * external lib changes
    
    * Disable LTO in Picnic that prevents lib merging.
    
    * Changed mode on build_picnic.sh.
    
    * Added newline at the end of the file.
    
    * Updated version of Picnic.
    
    * Added patch to picnic to satisfy the global functions namespace convention.
    
    * Added force option to patch to avoid error when rebuilding.
    
    * Updated picnic-related entries, and added cmake files (obtained by travis).
    
    * Updated ignore patch for picnic.
    
    * Ignore mods (patched files) in 3rd party picnic library.
    
    * Changed return code if no sig algs are configured, from error to success.
    It's not really an error, and this makes the Travis build fail otherwise.
    
    * Updated external picnic, and modified how it is patched and built (to avoid multiple patching).
    
    * disabled Picnic on gcc 4
    
    * Silenced picnic's dry-run patching (to avoid error messages if lib has already been patched).
    
    * added comments to script
    
    * Ignore (patched) submodule when checking for modified files.
    Also delete a "grep -v" for picnic that meant to do the same thing, but
    was in the wrong place after refactoring.
    
    * Added --ignore-submodules to second call to git status
    
    * Add temp/ to gitignore, to prevent Travis style-check failure on mac.

--- src/sig_picnic/sig_picnic.c
+++ src/sig_picnic/sig_picnic.c
@@ -1,4 +1,4 @@
-#if defined(ENABLE_PICNIC)
+#ifdef ENABLE_SIG_PICNIC
 #if defined(WINDOWS)
 #define UNUSED
 #else
@@ -12,25 +12,36 @@
 #include "sig_picnic.h"
 #include "picnic.h"
 
-#define SERIALIZED_PUB_KEY_LEN (PICNIC_MAX_PUBLICKEY_SIZE + 1)
-#define SERIALIZED_PRIV_KEY_LEN (PICNIC_MAX_PRIVATEKEY_SIZE + 1 + SERIALIZED_PUB_KEY_LEN)
-
-static char *Picnic_42_14_FS = "Picnic_42_14_FS";
-static char *Picnic_42_14_UR = "Picnic_42_14_UR";
-static char *Picnic_1_316_FS = "Picnic_1_316_FS";
-static char *Picnic_1_316_UR = "Picnic_1_316_UR";
-static char *Picnic_10_38_FS = "Picnic_10_38_FS";
-static char *Picnic_10_38_UR = "Picnic_10_38_UR";
-
-// This flag indicates if OpenSSL is used by OQS (or an app including OQS, such
-// as OpenSSL itself). If so, then Picnic skips the initialization and shutdown
-// of OpenSSL, not to interfere with the containing app.
-static int USES_OPENSSL =
-#if USE_OPENSSL
-    1;
-#else
-    0;
-#endif
+static char *Picnic_L1_FS_name = "Picnic_L1_FS";
+static char *Picnic_L1_UR_name = "Picnic_L1_UR";
+static char *Picnic_L3_FS_name = "Picnic_L3_FS";
+static char *Picnic_L3_UR_name = "Picnic_L3_UR";
+static char *Picnic_L5_FS_name = "Picnic_L5_FS";
+static char *Picnic_L5_UR_name = "Picnic_L5_UR";
+static size_t PRIV_KEY_LEN[] = {
+    0,
+    PICNIC_PRIVATE_KEY_SIZE(Picnic_L1_FS),
+    PICNIC_PRIVATE_KEY_SIZE(Picnic_L1_UR),
+    PICNIC_PRIVATE_KEY_SIZE(Picnic_L3_FS),
+    PICNIC_PRIVATE_KEY_SIZE(Picnic_L3_UR),
+    PICNIC_PRIVATE_KEY_SIZE(Picnic_L5_FS),
+    PICNIC_PRIVATE_KEY_SIZE(Picnic_L5_UR)};
+static size_t PUB_KEY_LEN[] = {
+    0,
+    PICNIC_PUBLIC_KEY_SIZE(Picnic_L1_FS),
+    PICNIC_PUBLIC_KEY_SIZE(Picnic_L1_UR),
+    PICNIC_PUBLIC_KEY_SIZE(Picnic_L3_FS),
+    PICNIC_PUBLIC_KEY_SIZE(Picnic_L3_UR),
+    PICNIC_PUBLIC_KEY_SIZE(Picnic_L5_FS),
+    PICNIC_PUBLIC_KEY_SIZE(Picnic_L5_UR)};
+static size_t SIG_LEN[] = {
+    0,
+    PICNIC_SIGNATURE_SIZE_Picnic_L1_FS,
+    PICNIC_SIGNATURE_SIZE_Picnic_L1_UR,
+    PICNIC_SIGNATURE_SIZE_Picnic_L3_FS,
+    PICNIC_SIGNATURE_SIZE_Picnic_L3_UR,
+    PICNIC_SIGNATURE_SIZE_Picnic_L5_FS,
+    PICNIC_SIGNATURE_SIZE_Picnic_L5_UR};
 
 typedef struct PICNIC_CTX {
 	picnic_params_t params;
@@ -40,63 +51,65 @@ int OQS_SIG_picnic_get(OQS_SIG *s, enum OQS_SIG_algid algid) {
 	if (s == NULL) {
 		return OQS_ERROR;
 	}
-	// init the alg
-	picnic_params_t params;
-	char *name;
-	switch (algid) {
-	case OQS_SIG_picnic_42_14_FS:
-		params = LowMC_256_256_42_14_FS;
-		name = Picnic_42_14_FS;
-		break;
-	case OQS_SIG_picnic_42_14_UR:
-		params = LowMC_256_256_42_14_UR;
-		name = Picnic_42_14_UR;
-		break;
-	case OQS_SIG_picnic_1_316_FS:
-		params = LowMC_256_256_1_316_FS;
-		name = Picnic_1_316_FS;
-		break;
-	case OQS_SIG_picnic_1_316_UR:
-		params = LowMC_256_256_1_316_UR;
-		name = Picnic_1_316_UR;
-		break;
-	case OQS_SIG_picnic_default:
-	case OQS_SIG_picnic_10_38_FS:
-		params = LowMC_256_256_10_38_FS;
-		name = Picnic_10_38_FS;
-		break;
-	case OQS_SIG_picnic_10_38_UR:
-		params = LowMC_256_256_10_38_UR;
-		name = Picnic_10_38_UR;
-		break;
-	default:
-		return OQS_ERROR;
-	}
+
 	PICNIC_CTX *pctx = malloc(sizeof(PICNIC_CTX));
 	if (pctx == NULL) {
 		return OQS_ERROR;
 	}
-	pctx->params = params;
-	// read the path to the picnic params (if undefined, NULL is
-	// returned and passed to picnic_init, and the default is used).
-	const char *params_path = getenv("PICNIC_PARAMS_PATH");
-	if (picnic_init(params, params_path, USES_OPENSSL) != 0) {
-		free(pctx);
+
+	// set the scheme-specific alg values
+	// NOTE: the key and sig len values use macros, so we can't
+	//       parametrized with pctx->params to shorten the code.
+	switch (algid) {
+	case OQS_SIG_picnic_default:
+	case OQS_SIG_picnic_L1_FS:
+		pctx->params = Picnic_L1_FS;
+		s->method_name = Picnic_L1_FS_name;
+		s->estimated_classical_security = 128;
+		s->estimated_quantum_security = 64;
+		break;
+	case OQS_SIG_picnic_L1_UR:
+		pctx->params = Picnic_L1_UR;
+		s->method_name = Picnic_L1_UR_name;
+		s->estimated_classical_security = 128;
+		s->estimated_quantum_security = 64;
+		break;
+	case OQS_SIG_picnic_L3_FS:
+		pctx->params = Picnic_L3_FS;
+		s->method_name = Picnic_L3_FS_name;
+		s->estimated_classical_security = 192;
+		s->estimated_quantum_security = 96;
+		break;
+	case OQS_SIG_picnic_L3_UR:
+		pctx->params = Picnic_L3_UR;
+		s->method_name = Picnic_L3_UR_name;
+		s->estimated_classical_security = 192;
+		s->estimated_quantum_security = 96;
+		break;
+	case OQS_SIG_picnic_L5_FS:
+		pctx->params = Picnic_L5_FS;
+		s->method_name = Picnic_L5_FS_name;
+		s->estimated_classical_security = 256;
+		s->estimated_quantum_security = 128;
+		break;
+	case OQS_SIG_picnic_L5_UR:
+		pctx->params = Picnic_L5_UR;
+		s->method_name = Picnic_L5_UR_name;
+		s->estimated_classical_security = 256;
+		s->estimated_quantum_security = 128;
+		break;
+	default:
 		return OQS_ERROR;
 	}
-
-	// set the scheme values
-	s->method_name = name;
-	s->estimated_classical_security = 256;
-	s->estimated_quantum_security = 128;
-	s->priv_key_len = SERIALIZED_PRIV_KEY_LEN;
-	s->pub_key_len = SERIALIZED_PUB_KEY_LEN;
-	s->max_sig_len = PICNIC_MAX_SIGNATURE_SIZE;
+	// set the ctx, sizes, and API functions
+	s->ctx = pctx;
+	s->priv_key_len = PRIV_KEY_LEN[pctx->params] + PUB_KEY_LEN[pctx->params]; // priv key also contains pub key
+	s->pub_key_len = PUB_KEY_LEN[pctx->params];
+	s->max_sig_len = SIG_LEN[pctx->params];
 	s->keygen = &OQS_SIG_picnic_keygen;
 	s->sign = &OQS_SIG_picnic_sign;
 	s->verify = &OQS_SIG_picnic_verify;
-	s->shutdown = &OQS_SIG_picnic_shutdown;
-	s->ctx = pctx;
+
 	return OQS_SUCCESS;
 }
 
@@ -112,14 +125,14 @@ int OQS_SIG_picnic_keygen(const OQS_SIG *s, uint8_t *priv, uint8_t *pub) {
 		return OQS_ERROR;
 	}
 	// serialize the public key
-	if (picnic_write_public_key(&pk, pub, SERIALIZED_PUB_KEY_LEN) != SERIALIZED_PUB_KEY_LEN) {
+	int pk_len = picnic_write_public_key(&pk, pub, PUB_KEY_LEN[parameters]);
+	if ((size_t) pk_len != PUB_KEY_LEN[parameters]) {
 		return OQS_ERROR;
 	}
+
 	// serialize the private key
-	// 1. prepend the public key
-	memcpy(priv, pub, SERIALIZED_PUB_KEY_LEN);
-	// 2. write the private key
-	if (picnic_write_private_key(&sk, priv + SERIALIZED_PUB_KEY_LEN, SERIALIZED_PRIV_KEY_LEN) != (PICNIC_MAX_PRIVATEKEY_SIZE + 1)) {
+	int sk_len = picnic_write_private_key(&sk, priv, PRIV_KEY_LEN[parameters]);
+	if ((size_t) sk_len != PRIV_KEY_LEN[parameters]) {
 		return OQS_ERROR;
 	}
 	// wipe the private key
@@ -132,14 +145,9 @@ int OQS_SIG_picnic_sign(const OQS_SIG *s, const uint8_t *priv, const uint8_t *ms
 		return OQS_ERROR;
 	}
 	picnic_privatekey_t sk;
-	picnic_publickey_t pk;
+	picnic_params_t parameters = ((PICNIC_CTX *) s->ctx)->params;
 	// deserialize the private key
-	// 1. read the prepended public key
-	if (picnic_read_public_key(&pk, priv, SERIALIZED_PUB_KEY_LEN) != 0) {
-		return OQS_ERROR;
-	}
-	// 2. read the private key
-	if (picnic_read_private_key(&sk, priv + SERIALIZED_PUB_KEY_LEN, SERIALIZED_PRIV_KEY_LEN, &pk) != 0) {
+	if (picnic_read_private_key(&sk, priv, PRIV_KEY_LEN[parameters]) != 0) {
 		return OQS_ERROR;
 	}
 	if (picnic_sign(&sk, msg, msg_len, sig, sig_len) != 0) {
@@ -153,8 +161,9 @@ int OQS_SIG_picnic_verify(UNUSED const OQS_SIG *s, const uint8_t *pub, const uin
 		return OQS_ERROR;
 	}
 	picnic_publickey_t pk;
-	// deserialize the private key
-	if (picnic_read_public_key(&pk, pub, SERIALIZED_PUB_KEY_LEN) != 0) {
+	// deserialize the public key
+	picnic_params_t parameters = ((PICNIC_CTX *) s->ctx)->params;
+	if (picnic_read_public_key(&pk, pub, PUB_KEY_LEN[parameters]) != 0) {
 		return OQS_ERROR;
 	}
 	if (picnic_verify(&pk, msg, msg_len, sig, sig_len) != 0) {
@@ -162,13 +171,4 @@ int OQS_SIG_picnic_verify(UNUSED const OQS_SIG *s, const uint8_t *pub, const uin
 	}
 	return OQS_SUCCESS;
 }
-
-int OQS_SIG_picnic_shutdown(OQS_SIG *s) {
-	if (s == NULL) {
-		return OQS_ERROR;
-	}
-	picnic_shutdown(USES_OPENSSL);
-	free(s->ctx);
-	return OQS_SUCCESS;
-}
-#endif // ENABLE_PICNIC
+#endif
