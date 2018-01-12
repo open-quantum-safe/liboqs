0b285e0ae0351827c8a755527d5a3eb539894e71
Christian Paquin <cpaquin@microsoft.com>
2018-01-11 23:19:54 -0500

    Updated the picnic library, and removed it as a patched submodule and instead committed the code direclty.

--- src/sig_picnic/build_picnic.sh
+++ src/sig_picnic/build_picnic.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
 mkdir -p external/build
-# check if patch has already been applied
-patch -p1 -N -f --dry-run --silent -d external < oqs_sig_picnic.patch 2>/dev/null 1>/dev/null
-if [ $? -eq 0 ];
-then
-    # apply the patch
-    echo Patching Picnic external
-    patch -p1 -N -f -d external < oqs_sig_picnic.patch
-fi
 cd external/build
 # make picnic
 cmake -UWITH_LTO -DWITH_LTO:BOOL=OFF ..
