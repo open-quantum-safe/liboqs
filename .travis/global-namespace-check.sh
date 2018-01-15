176a25f3b1a78bdde523bad8d24ba31c2aa069c9
Shravan Mishra <s24mishra@uwaterloo.ca>
2018-01-15 13:10:35 -0500

    Further updates to gloabalnamespace script

--- .travis/global-namespace-check.sh
+++ .travis/global-namespace-check.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [[ $(nm -g liboqs.a | grep ' T ' | grep -E -v -i ' T [_]?(OQS|ntru|picnic|Keccak|.*SIKEp503|.*SIDHp503|.*SIKEp751|.*SIDHp751|.*shake128|.*shake256|rand_bytes|print_hex|cpu_supports|uint64_from_char_array|uint64_to_char_array)') ]];
+if [[ $(nm -g liboqs.a | grep ' T ' | grep -E -v -i ' T [_]?(OQS|ntru|picnic|Keccak|.*SIKEp503|.*SIDHp503|.*SIKEp751|.*SIDHp751|.*shake128|.*shake256|rand_bytes|print_hex|cpu_supports|uint64_from_char_array|uint64_to_char_array|_ntt_double|_rec)') ]];
 then
 	tput setaf 1;
 	echo "Code contains the following non-namespaced global symbols; see https://github.com/open-quantum-safe/liboqs/wiki/Coding-conventions for function naming conventions.";
 	tput sgr 0
-  nm -g liboqs.a | grep ' T ' | grep -E -v -i ' T [_]?(OQS|ntru|picnic|Keccak|.*SIKEp503|.*SIDHp503|.*SIKEp751|.*SIDHp751|.*shake128|.*shake256|rand_bytes|print_hex|cpu_supports|uint64_from_char_array|uint64_to_char_array)'
+  nm -g liboqs.a | grep ' T ' | grep -E -v -i ' T [_]?(OQS|ntru|picnic|Keccak|.*SIKEp503|.*SIDHp503|.*SIKEp751|.*SIDHp751|.*shake128|.*shake256|rand_bytes|print_hex|cpu_supports|uint64_from_char_array|uint64_to_char_array|_ntt_double|_rec)'
 	exit 1;
 else
 	tput setaf 2;
