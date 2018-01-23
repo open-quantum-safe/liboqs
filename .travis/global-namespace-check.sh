60d0bdcc2c6e49cdf8298770beec59d567c62ab8
Vlad Gheorghiu <vsoftco@gmail.com>
2018-01-22 20:52:52 -0500

    commit

--- .travis/global-namespace-check.sh
+++ .travis/global-namespace-check.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-if [[ $(nm -g liboqs.a | grep ' T ' | grep -E -v -i ' T [_]?(OQS|ntru|picnic|Keccak|.*SIKEp503|.*SIDHp503|.*SIKEp751|.*SIDHp751|.*shake128|.*shake256|rand_bytes|cpu_supports|uint64_from_char_array|uint64_to_char_array|_aligned_alloc|_aligned_free)') ]];
+REGEX=' T [_]?(OQS|ntru|picnic|Keccak|.*SIKEp503|.*SIDHp503|.*SIKEp751|.*SIDHp751|.*shake128|.*shake256|rand_bytes|cpu_supports|uint64_from_char_array|uint64_to_char_array|_print_hex|_aligned_alloc|_aligned_free)'
+
+if [[ $(nm -g liboqs.a | grep ' T ' | grep -E -v -i "$REGEX") ]];
 then
-	tput setaf 1;
-	echo "Code contains the following non-namespaced global symbols; see https://github.com/open-quantum-safe/liboqs/wiki/Coding-conventions for function naming conventions.";
-	tput sgr 0
-  nm -g liboqs.a | grep ' T ' | grep -E -v -i ' T [_]?(OQS|ntru|picnic|Keccak|.*SIKEp503|.*SIDHp503|.*SIKEp751|.*SIDHp751|.*shake128|.*shake256|rand_bytes|cpu_supports|uint64_from_char_array|uint64_to_char_array|_aligned_alloc|_aligned_free)'
-	exit 1;
+    tput setaf 1;
+    echo "Code contains the following non-namespaced global symbols; see https://github.com/open-quantum-safe/liboqs/wiki/Coding-conventions for function naming conventions.";
+    tput sgr 0
+    nm -g liboqs.a | grep ' T ' | grep -E -v -i "$REGEX"
+    exit 1;
 else
-	tput setaf 2;
-	echo "Code adheres to the project standards (global namespace).";
-	tput sgr 0
-	exit 0;
+    tput setaf 2;
+    echo "Code adheres to the project standards (global namespace).";
+    tput sgr 0
+    exit 0;
 fi;
