10009e3ab124575f03d5c8e8a8ba4cc0893e2cc2
Vlad Gheorghiu <vsoftco@gmail.com>
2018-01-28 19:20:32 -0500

    commit

--- src/common/oqs.h
+++ src/common/oqs.h
@@ -1,10 +1,12 @@
 #ifndef __OQS_H
 #define __OQS_H
 
+#include <oqs/aes.h>
 #include <oqs/common.h>
 #include <oqs/config.h>
 #include <oqs/kex.h>
 #include <oqs/rand.h>
+#include <oqs/sha3.h>
 #include <oqs/sig.h>
 
 #endif
