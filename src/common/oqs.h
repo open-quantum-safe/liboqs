5638788c52c80dc6a02649490648c25c6f7b41ff
Vlad Gheorghiu <vsoftco@gmail.com>
2018-01-29 13:52:59 -0500

    fixed VS build

--- src/common/oqs.h
+++ src/common/oqs.h
@@ -3,10 +3,13 @@
 
 #include <oqs/aes.h>
 #include <oqs/common.h>
-#include <oqs/config.h>
 #include <oqs/kex.h>
 #include <oqs/rand.h>
 #include <oqs/sha3.h>
 #include <oqs/sig.h>
+
+#if !defined(WINDOWS)
+#include <oqs/config.h>
+#endif
 
 #endif
