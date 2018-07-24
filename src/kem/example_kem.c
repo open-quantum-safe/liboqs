d5f96561dbb1f0d79d58800304c16b40e0e38f23
s24mishra <s24mishra@uwaterloo.ca>
2018-07-24 07:50:21 -0400

    Fix errors because of disabling frodokem

--- src/kem/example_kem.c
+++ src/kem/example_kem.c
@@ -43,6 +43,7 @@ OQS_STATUS example_stack() {
 	       "compile-time.\n");
 	return OQS_ERROR;
 #endif
+#ifdef OQS_ENABLE_KEM_frodokem_640_aes
 	uint8_t public_key[OQS_KEM_frodokem_640_aes_length_public_key];
 	uint8_t secret_key[OQS_KEM_frodokem_640_aes_length_secret_key];
 	uint8_t ciphertext[OQS_KEM_frodokem_640_aes_length_ciphertext];
@@ -79,6 +80,7 @@ OQS_STATUS example_stack() {
 	printf("[example_stack] OQS_KEM_frodokem_640_aes operations completed.\n");
 
 	return OQS_SUCCESS; // success!
+#endif
 }
 
 /* This function gives an example of the operations performed by both
