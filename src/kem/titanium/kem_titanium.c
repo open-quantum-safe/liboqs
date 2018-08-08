580de5e754863d6122d5786f4340020ae3b96701
Ben Davies <ben.davies@uwaterloo.ca>
2018-08-08 12:36:12 -0400

    Titanium: added med strength CCA algorithm.

--- src/kem/titanium/kem_titanium.c
+++ src/kem/titanium/kem_titanium.c
@@ -6,54 +6,81 @@
 
 OQS_KEM *OQS_KEM_titanium_cca_std_new() {
 
-	OQS_KEM *kem = malloc(sizeof(OQS_KEM));
-	if (kem == NULL) {
-		return NULL;
-	}
-	kem->method_name = OQS_KEM_alg_titanium_cca_std_kem;
+    OQS_KEM *kem = malloc(sizeof(OQS_KEM));
+    if (kem == NULL) {
+        return NULL;
+    }
+    kem->method_name = OQS_KEM_alg_titanium_cca_std_kem;
 
-	kem->claimed_nist_level = 1;
-	kem->ind_cca = true;
+    kem->claimed_nist_level = 1;
+    kem->ind_cca = true;
 
-	// NB: sets length of vars
-	kem->length_public_key = OQS_KEM_titanium_cca_std_length_public_key;
-	kem->length_secret_key = OQS_KEM_titanium_cca_std_length_secret_key;
-	kem->length_ciphertext = OQS_KEM_titanium_cca_std_length_ciphertext;
-	kem->length_shared_secret = OQS_KEM_titanium_cca_std_length_shared_secret;
+    // NB: sets length of vars
+    kem->length_public_key = OQS_KEM_titanium_cca_std_length_public_key;
+    kem->length_secret_key = OQS_KEM_titanium_cca_std_length_secret_key;
+    kem->length_ciphertext = OQS_KEM_titanium_cca_std_length_ciphertext;
+    kem->length_shared_secret = OQS_KEM_titanium_cca_std_length_shared_secret;
 
-	// redirection: link up externally defined API functions to KEM struct
-	kem->keypair = OQS_KEM_titanium_cca_std_keypair;
-	kem->encaps = OQS_KEM_titanium_cca_std_encaps;
-	kem->decaps = OQS_KEM_titanium_cca_std_decaps;
+    // redirection: link up externally defined API functions to KEM struct
+    kem->keypair = OQS_KEM_titanium_cca_std_keypair;
+    kem->encaps = OQS_KEM_titanium_cca_std_encaps;
+    kem->decaps = OQS_KEM_titanium_cca_std_decaps;
 
-	return kem;
+    return kem;
 }
 
-#endif
+#endif /* OQS_ENABLE_KEM_titanium_cca_std_kem */
 
 #ifdef OQS_ENABLE_KEM_titanium_cca_hi_kem
 
 OQS_KEM *OQS_KEM_titanium_cca_hi_new() {
 
-	OQS_KEM *kem = malloc(sizeof(OQS_KEM));
-	if (kem == NULL) {
-		return NULL;
-	}
-	kem->method_name = OQS_KEM_alg_titanium_cca_hi_kem;
+    OQS_KEM *kem = malloc(sizeof(OQS_KEM));
+    if (kem == NULL) {
+        return NULL;
+    }
+    kem->method_name = OQS_KEM_alg_titanium_cca_hi_kem;
 
-	kem->claimed_nist_level = 3;
-	kem->ind_cca = true;
+    kem->claimed_nist_level = 3;
+    kem->ind_cca = true;
 
-	kem->length_public_key = OQS_KEM_titanium_cca_hi_length_public_key;
-	kem->length_secret_key = OQS_KEM_titanium_cca_hi_length_secret_key;
-	kem->length_ciphertext = OQS_KEM_titanium_cca_hi_length_ciphertext;
-	kem->length_shared_secret = OQS_KEM_titanium_cca_hi_length_shared_secret;
+    kem->length_public_key = OQS_KEM_titanium_cca_hi_length_public_key;
+    kem->length_secret_key = OQS_KEM_titanium_cca_hi_length_secret_key;
+    kem->length_ciphertext = OQS_KEM_titanium_cca_hi_length_ciphertext;
+    kem->length_shared_secret = OQS_KEM_titanium_cca_hi_length_shared_secret;
 
-	kem->keypair = OQS_KEM_titanium_cca_hi_keypair;
-	kem->encaps = OQS_KEM_titanium_cca_hi_encaps;
-	kem->decaps = OQS_KEM_titanium_cca_hi_decaps;
+    kem->keypair = OQS_KEM_titanium_cca_hi_keypair;
+    kem->encaps = OQS_KEM_titanium_cca_hi_encaps;
+    kem->decaps = OQS_KEM_titanium_cca_hi_decaps;
 
-	return kem;
+    return kem;
 }
 
-#endif /* OQS_ENABLE_KEM_titanium_cca_hi_kem */
+#endif /* OQS_ENABLE_KEM_titanium_cca_med_kem */
+
+#ifdef OQS_ENABLE_KEM_titanium_cca_med_kem
+
+OQS_KEM *OQS_KEM_titanium_cca_med_new() {
+
+    OQS_KEM *kem = malloc(sizeof(OQS_KEM));
+    if (kem == NULL) {
+        return NULL;
+    }
+    kem->method_name = OQS_KEM_alg_titanium_cca_med_kem;
+
+    kem->claimed_nist_level = 0; // from https://github.com/raykzhao/liboqs
+    kem->ind_cca = true;
+
+    kem->length_public_key = OQS_KEM_titanium_cca_med_length_public_key;
+    kem->length_secret_key = OQS_KEM_titanium_cca_med_length_secret_key;
+    kem->length_ciphertext = OQS_KEM_titanium_cca_med_length_ciphertext;
+    kem->length_shared_secret = OQS_KEM_titanium_cca_med_length_shared_secret;
+
+    kem->keypair = OQS_KEM_titanium_cca_med_keypair;
+    kem->encaps = OQS_KEM_titanium_cca_med_encaps;
+    kem->decaps = OQS_KEM_titanium_cca_med_decaps;
+
+    return kem;
+}
+
+#endif /* OQS_ENABLE_KEM_titanium_cca_med_kem */
