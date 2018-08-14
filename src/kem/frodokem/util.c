8ea24629c33264f91519f39a2e7311f5871eb49a
Douglas Stebila <dstebila@uwaterloo.ca>
2018-08-14 12:14:13 -0400

    Prettyprint

--- src/kem/frodokem/util.c
+++ src/kem/frodokem/util.c
@@ -10,7 +10,7 @@
 #define min(x, y) (((x) < (y)) ? (x) : (y))
 
 void oqs_kem_frodokem_pack(unsigned char *out, const size_t outlen, const uint16_t *in, const size_t inlen, const unsigned char lsb) { // Pack the input uint16 vector into a char output vector, copying lsb bits from each input element.
-	                                                                                                                                // If inlen * lsb / 8 > outlen, only outlen * 8 bits are copied.
+	                                                                                                                                   // If inlen * lsb / 8 > outlen, only outlen * 8 bits are copied.
 	memset(out, 0, outlen);
 
 	size_t i = 0;           // whole bytes already filled in
@@ -57,7 +57,7 @@ void oqs_kem_frodokem_pack(unsigned char *out, const size_t outlen, const uint16
 }
 
 void oqs_kem_frodokem_unpack(uint16_t *out, const size_t outlen, const unsigned char *in, const size_t inlen, const unsigned char lsb) { // Unpack the input char vector into a uint16_t output vector, copying lsb bits
-	                                                                                                                                  // for each output element from input. outlen must be at least ceil(inlen * 8 / lsb).
+	                                                                                                                                     // for each output element from input. outlen must be at least ceil(inlen * 8 / lsb).
 	memset(out, 0, outlen * sizeof(uint16_t));
 
 	size_t i = 0;           // whole uint16_t already filled in
