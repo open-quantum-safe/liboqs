4767e157ebf3310eb8bb575d6b53b901271275c8
Douglas Stebila <stebilad@mcmaster.ca>
2018-09-24 21:06:02 -0400

    Point global-namespace-check to .libs/liboqs.a

--- .travis/global-namespace-check.sh
+++ .travis/global-namespace-check.sh
@@ -6,14 +6,14 @@ source $(dirname $0)/defs.sh
 
 REGEX=' T [_]?(OQS|ntru|picnic|Keccak|.*SIKEp503|.*SIDHp503|.*SIKEp751|.*SIDHp751|.*shake128|.*shake256|rand_bytes|cpu_supports|uint64_from_char_array|uint64_to_char_array|print_hex|ntt_double|rec|aligned_alloc|aligned_free)'
 
-NON_NAMESPACED=`nm -g liboqs.a | grep ' T ' | grep -E -v -i "$REGEX"`
+NON_NAMESPACED=`nm -g .libs/liboqs.a | grep ' T ' | grep -E -v -i "$REGEX"`
 
 if [[ ${NON_NAMESPACED} ]];
 then
     ${PRINT_RED}
     echo "Code contains the following non-namespaced global symbols; see https://github.com/open-quantum-safe/liboqs/wiki/Coding-conventions for function naming conventions.";
     ${PRINT_RESET}
-    nm -g liboqs.a | grep ' T ' | grep -E -v -i "$REGEX"
+    echo ${NON_NAMESPACED}
     exit 1;
 else
     ${PRINT_GREEN}
