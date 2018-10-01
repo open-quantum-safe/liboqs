76538fde13cd660c2a9f38a2b10843d8d7502b29
Christian Paquin <cpaquin@microsoft.com>
2018-10-01 13:22:23 -0700

    Update to Visual Studio (Sept 2018) (#395)
    * Added example_kem/sig and speed_kem/sig to Visual Studio.
    
    * Added kat_kem to VS
    
    * Re-added SIDH/SIKE to Visual Studio

--- VisualStudio/winconfig.h
+++ VisualStudio/winconfig.h
@@ -2,11 +2,14 @@
 #define __WINCONFIG_H
 
 /* Enable schemes supported on Windows */
-#define ENABLE_SIG_PICNIC
 #define OQS_ENABLE_KEM_frodokem_640_aes
 #define OQS_ENABLE_KEM_frodokem_976_aes
 #define OQS_ENABLE_KEM_frodokem_640_cshake
 #define OQS_ENABLE_KEM_frodokem_976_cshake
+#define OQS_ENABLE_KEM_sike_p503
+#define OQS_ENABLE_KEM_sike_p751
+#define OQS_ENABLE_KEM_sidh_p503
+#define OQS_ENABLE_KEM_sidh_p751
 #define OQS_ENABLE_SIG_picnic_L1_FS
 #define OQS_ENABLE_SIG_picnic_L1_UR
 #define OQS_ENABLE_SIG_picnic_L3_FS
