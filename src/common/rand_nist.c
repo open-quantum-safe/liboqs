aecd44d3413580a50aad38d268cba93040694300
Douglas Stebila <dstebila@uwaterloo.ca>
2018-10-03 12:47:29 -0400

    Sync more common files on nist-branch with master

--- src/common/rand_nist.c
+++ src/common/rand_nist.c
@@ -9,6 +9,8 @@
 #include <assert.h>
 #include <string.h>
 
+#include <oqs/common.h>
+
 #include <openssl/conf.h>
 #include <openssl/evp.h>
 #include <openssl/err.h>
@@ -50,7 +52,7 @@ static void AES256_ECB(unsigned char *key, unsigned char *ctr, unsigned char *bu
 	EVP_CIPHER_CTX_free(ctx);
 }
 
-void OQS_randombytes_nist_kat_init(unsigned char *entropy_input, unsigned char *personalization_string, int security_strength) {
+OQS_API void OQS_randombytes_nist_kat_init(unsigned char *entropy_input, unsigned char *personalization_string, int security_strength) {
 	unsigned char seed_material[48];
 
 	assert(security_strength == 256);
