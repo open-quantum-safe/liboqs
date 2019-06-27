7af6d72c79a8711978ef43ea17009527a10bcb78
Douglas Stebila <dstebila@uwaterloo.ca>
2019-06-27 14:51:28 -0400

    Fix merge artifacts

--- src/common/pqclean_shims/aes.h
+++ src/common/pqclean_shims/aes.h
@@ -16,20 +16,12 @@ typedef void * aes128ctx;
 
 #define aes128_keyexp(r, key) OQS_AES128_load_schedule((key), (r), 1);
 #define aes128_ecb(out, in, nblocks, ctx) OQS_AES128_ECB_enc_sch((in), (nblocks) * AES_BLOCKBYTES, *(ctx), (out));
-<<<<<<< HEAD
 #define aes128_ctx_release(ctx) OQS_AES128_free_schedule(*(ctx));
-=======
-#define aes128_freectx(ctx) OQS_AES128_free_schedule(*(ctx));
->>>>>>> c1ceab9943633961543802e1c9e763e650f51e78
 
 typedef void * aes256ctx;
 
 #define aes256_keyexp(r, key) OQS_AES256_load_schedule((key), (r), 1);
 #define aes256_ecb(out, in, nblocks, ctx) OQS_AES256_ECB_enc_sch((in), (nblocks) * AES_BLOCKBYTES, *(ctx), (out));
-<<<<<<< HEAD
 #define aes256_ctx_release(ctx) OQS_AES256_free_schedule(*(ctx));
-=======
-#define aes256_freectx(ctx) OQS_AES256_free_schedule(*(ctx));
->>>>>>> c1ceab9943633961543802e1c9e763e650f51e78
 
 #endif
