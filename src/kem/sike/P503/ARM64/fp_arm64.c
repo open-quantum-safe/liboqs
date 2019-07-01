7094c7a37c47fd7d6718022f3cc81b4cf9ec25b5
Christian Paquin <cpaquin@microsoft.com>
2019-07-01 15:32:11 -0400

    prettyprint

--- src/kem/sike/P503/ARM64/fp_arm64.c
+++ src/kem/sike/P503/ARM64/fp_arm64.c
@@ -8,86 +8,71 @@
 
 // Global constants
 extern const uint64_t p503[NWORDS_FIELD];
-extern const uint64_t p503x2[NWORDS_FIELD]; 
+extern const uint64_t p503x2[NWORDS_FIELD];
 
+__inline void fpadd503(const digit_t *a, const digit_t *b, digit_t *c) { // Modular addition, c = a+b mod p503.
+	                                                                     // Inputs: a, b in [0, 2*p503-1]
+	                                                                     // Output: c in [0, 2*p503-1]
 
-__inline void fpadd503(const digit_t* a, const digit_t* b, digit_t* c)
-{ // Modular addition, c = a+b mod p503.
-  // Inputs: a, b in [0, 2*p503-1] 
-  // Output: c in [0, 2*p503-1]
-
-    fpadd503_asm(a, b, c);
-} 
-
-
-__inline void fpsub503(const digit_t* a, const digit_t* b, digit_t* c)
-{ // Modular subtraction, c = a-b mod p503.
-  // Inputs: a, b in [0, 2*p503-1] 
-  // Output: c in [0, 2*p503-1] 
-
-    fpsub503_asm(a, b, c);
+	fpadd503_asm(a, b, c);
 }
 
+__inline void fpsub503(const digit_t *a, const digit_t *b, digit_t *c) { // Modular subtraction, c = a-b mod p503.
+	                                                                     // Inputs: a, b in [0, 2*p503-1]
+	                                                                     // Output: c in [0, 2*p503-1]
 
-__inline void fpneg503(digit_t* a)
-{ // Modular negation, a = -a mod p503.
-  // Input/output: a in [0, 2*p503-1] 
-    unsigned int i, borrow = 0;
-
-    for (i = 0; i < NWORDS_FIELD; i++) {
-        SUBC(borrow, ((digit_t*)p503x2)[i], a[i], borrow, a[i]); 
-    }
+	fpsub503_asm(a, b, c);
 }
 
+__inline void fpneg503(digit_t *a) { // Modular negation, a = -a mod p503.
+	                                 // Input/output: a in [0, 2*p503-1]
+	unsigned int i, borrow = 0;
 
-void fpdiv2_503(const digit_t* a, digit_t* c)
-{ // Modular division by two, c = a/2 mod p503.
-  // Input : a in [0, 2*p503-1] 
-  // Output: c in [0, 2*p503-1] 
-    unsigned int i, carry = 0;
-    digit_t mask;
-        
-    mask = 0 - (digit_t)(a[0] & 1);    // If a is odd compute a+p521
-    for (i = 0; i < NWORDS_FIELD; i++) {
-        ADDC(carry, a[i], ((digit_t*)p503)[i] & mask, carry, c[i]); 
-    }
-
-    mp_shiftr1(c, NWORDS_FIELD);
-} 
-
-
-void fpcorrection503(digit_t* a)
-{ // Modular correction to reduce field element a in [0, 2*p503-1] to [0, p503-1].
-    unsigned int i, borrow = 0;
-    digit_t mask;
-
-    for (i = 0; i < NWORDS_FIELD; i++) {
-        SUBC(borrow, a[i], ((digit_t*)p503)[i], borrow, a[i]);
-    }
-    mask = 0 - (digit_t)borrow;
-
-    borrow = 0;
-    for (i = 0; i < NWORDS_FIELD; i++) {
-        ADDC(borrow, a[i], ((digit_t*)p503)[i] & mask, borrow, a[i]);
-    }
+	for (i = 0; i < NWORDS_FIELD; i++) {
+		SUBC(borrow, ((digit_t *) p503x2)[i], a[i], borrow, a[i]);
+	}
 }
 
+void fpdiv2_503(const digit_t *a, digit_t *c) { // Modular division by two, c = a/2 mod p503.
+	                                            // Input : a in [0, 2*p503-1]
+	                                            // Output: c in [0, 2*p503-1]
+	unsigned int i, carry = 0;
+	digit_t mask;
 
-void mp_mul(const digit_t* a, const digit_t* b, digit_t* c, const unsigned int nwords)
-{ // Multiprecision multiply, c = a*b, where lng(a) = lng(b) = nwords.
+	mask = 0 - (digit_t)(a[0] & 1); // If a is odd compute a+p521
+	for (i = 0; i < NWORDS_FIELD; i++) {
+		ADDC(carry, a[i], ((digit_t *) p503)[i] & mask, carry, c[i]);
+	}
 
-    UNREFERENCED_PARAMETER(nwords);
-
-    mul503_asm(a, b, c);
+	mp_shiftr1(c, NWORDS_FIELD);
 }
 
+void fpcorrection503(digit_t *a) { // Modular correction to reduce field element a in [0, 2*p503-1] to [0, p503-1].
+	unsigned int i, borrow = 0;
+	digit_t mask;
 
+	for (i = 0; i < NWORDS_FIELD; i++) {
+		SUBC(borrow, a[i], ((digit_t *) p503)[i], borrow, a[i]);
+	}
+	mask = 0 - (digit_t) borrow;
 
-void rdc_mont(const digit_t* ma, digit_t* mc)
-{ // Montgomery reduction exploiting special form of the prime.
-  // mc = ma*R^-1 mod p503x2, where R = 2^512.
-  // If ma < 2^512*p503, the output mc is in the range [0, 2*p503-1].
-  // ma is assumed to be in Montgomery representation.
-  
-    rdc503_asm(ma, mc);
+	borrow = 0;
+	for (i = 0; i < NWORDS_FIELD; i++) {
+		ADDC(borrow, a[i], ((digit_t *) p503)[i] & mask, borrow, a[i]);
+	}
+}
+
+void mp_mul(const digit_t *a, const digit_t *b, digit_t *c, const unsigned int nwords) { // Multiprecision multiply, c = a*b, where lng(a) = lng(b) = nwords.
+
+	UNREFERENCED_PARAMETER(nwords);
+
+	mul503_asm(a, b, c);
+}
+
+void rdc_mont(const digit_t *ma, digit_t *mc) { // Montgomery reduction exploiting special form of the prime.
+	                                            // mc = ma*R^-1 mod p503x2, where R = 2^512.
+	                                            // If ma < 2^512*p503, the output mc is in the range [0, 2*p503-1].
+	                                            // ma is assumed to be in Montgomery representation.
+
+	rdc503_asm(ma, mc);
 }
--- src/kem/sike/P751/ARM64/fp_arm64.c
+++ src/kem/sike/P751/ARM64/fp_arm64.c
@@ -8,86 +8,71 @@
 
 // Global constants
 extern const uint64_t p751[NWORDS_FIELD];
-extern const uint64_t p751x2[NWORDS_FIELD]; 
+extern const uint64_t p751x2[NWORDS_FIELD];
 
+__inline void fpadd751(const digit_t *a, const digit_t *b, digit_t *c) { // Modular addition, c = a+b mod p751.
+	                                                                     // Inputs: a, b in [0, 2*p751-1]
+	                                                                     // Output: c in [0, 2*p751-1]
 
-__inline void fpadd751(const digit_t* a, const digit_t* b, digit_t* c)
-{ // Modular addition, c = a+b mod p751.
-  // Inputs: a, b in [0, 2*p751-1] 
-  // Output: c in [0, 2*p751-1]
-
-    fpadd751_asm(a, b, c);
-} 
-
-
-__inline void fpsub751(const digit_t* a, const digit_t* b, digit_t* c)
-{ // Modular subtraction, c = a-b mod p751.
-  // Inputs: a, b in [0, 2*p751-1] 
-  // Output: c in [0, 2*p751-1] 
-
-    fpsub751_asm(a, b, c);
+	fpadd751_asm(a, b, c);
 }
 
+__inline void fpsub751(const digit_t *a, const digit_t *b, digit_t *c) { // Modular subtraction, c = a-b mod p751.
+	                                                                     // Inputs: a, b in [0, 2*p751-1]
+	                                                                     // Output: c in [0, 2*p751-1]
 
-__inline void fpneg751(digit_t* a)
-{ // Modular negation, a = -a mod p751.
-  // Input/output: a in [0, 2*p751-1] 
-    unsigned int i, borrow = 0;
-
-    for (i = 0; i < NWORDS_FIELD; i++) {
-        SUBC(borrow, ((digit_t*)p751x2)[i], a[i], borrow, a[i]); 
-    }
+	fpsub751_asm(a, b, c);
 }
 
+__inline void fpneg751(digit_t *a) { // Modular negation, a = -a mod p751.
+	                                 // Input/output: a in [0, 2*p751-1]
+	unsigned int i, borrow = 0;
 
-void fpdiv2_751(const digit_t* a, digit_t* c)
-{ // Modular division by two, c = a/2 mod p751.
-  // Input : a in [0, 2*p751-1] 
-  // Output: c in [0, 2*p751-1] 
-    unsigned int i, carry = 0;
-    digit_t mask;
-        
-    mask = 0 - (digit_t)(a[0] & 1);    // If a is odd compute a+p521
-    for (i = 0; i < NWORDS_FIELD; i++) {
-        ADDC(carry, a[i], ((digit_t*)p751)[i] & mask, carry, c[i]); 
-    }
-
-    mp_shiftr1(c, NWORDS_FIELD);
-} 
-
-
-void fpcorrection751(digit_t* a)
-{ // Modular correction to reduce field element a in [0, 2*p751-1] to [0, p751-1].
-    unsigned int i, borrow = 0;
-    digit_t mask;
-
-    for (i = 0; i < NWORDS_FIELD; i++) {
-        SUBC(borrow, a[i], ((digit_t*)p751)[i], borrow, a[i]);
-    }
-    mask = 0 - (digit_t)borrow;
-
-    borrow = 0;
-    for (i = 0; i < NWORDS_FIELD; i++) {
-        ADDC(borrow, a[i], ((digit_t*)p751)[i] & mask, borrow, a[i]);
-    }
+	for (i = 0; i < NWORDS_FIELD; i++) {
+		SUBC(borrow, ((digit_t *) p751x2)[i], a[i], borrow, a[i]);
+	}
 }
 
+void fpdiv2_751(const digit_t *a, digit_t *c) { // Modular division by two, c = a/2 mod p751.
+	                                            // Input : a in [0, 2*p751-1]
+	                                            // Output: c in [0, 2*p751-1]
+	unsigned int i, carry = 0;
+	digit_t mask;
 
-void mp_mul(const digit_t* a, const digit_t* b, digit_t* c, const unsigned int nwords)
-{ // Multiprecision multiply, c = a*b, where lng(a) = lng(b) = nwords.
+	mask = 0 - (digit_t)(a[0] & 1); // If a is odd compute a+p521
+	for (i = 0; i < NWORDS_FIELD; i++) {
+		ADDC(carry, a[i], ((digit_t *) p751)[i] & mask, carry, c[i]);
+	}
 
-    UNREFERENCED_PARAMETER(nwords);
-
-    mul751_asm(a, b, c);
+	mp_shiftr1(c, NWORDS_FIELD);
 }
 
+void fpcorrection751(digit_t *a) { // Modular correction to reduce field element a in [0, 2*p751-1] to [0, p751-1].
+	unsigned int i, borrow = 0;
+	digit_t mask;
 
+	for (i = 0; i < NWORDS_FIELD; i++) {
+		SUBC(borrow, a[i], ((digit_t *) p751)[i], borrow, a[i]);
+	}
+	mask = 0 - (digit_t) borrow;
 
-void rdc_mont(const digit_t* ma, digit_t* mc)
-{ // Montgomery reduction exploiting special form of the prime.
-  // mc = ma*R^-1 mod p751x2, where R = 2^768.
-  // If ma < 2^768*p751, the output mc is in the range [0, 2*p751-1].
-  // ma is assumed to be in Montgomery representation.
-  
-    rdc751_asm(ma, mc);
+	borrow = 0;
+	for (i = 0; i < NWORDS_FIELD; i++) {
+		ADDC(borrow, a[i], ((digit_t *) p751)[i] & mask, borrow, a[i]);
+	}
+}
+
+void mp_mul(const digit_t *a, const digit_t *b, digit_t *c, const unsigned int nwords) { // Multiprecision multiply, c = a*b, where lng(a) = lng(b) = nwords.
+
+	UNREFERENCED_PARAMETER(nwords);
+
+	mul751_asm(a, b, c);
+}
+
+void rdc_mont(const digit_t *ma, digit_t *mc) { // Montgomery reduction exploiting special form of the prime.
+	                                            // mc = ma*R^-1 mod p751x2, where R = 2^768.
+	                                            // If ma < 2^768*p751, the output mc is in the range [0, 2*p751-1].
+	                                            // ma is assumed to be in Montgomery representation.
+
+	rdc751_asm(ma, mc);
 }
