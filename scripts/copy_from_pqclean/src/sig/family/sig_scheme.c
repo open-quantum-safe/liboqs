3231c929bed3682be9a142c54ebcd839402e1b51
Douglas Stebila <dstebila@uwaterloo.ca>
2019-07-04 22:06:19 -0400

    Update templates to use arbitrary implementation, not just _clean

--- scripts/copy_from_pqclean/src/sig/family/sig_scheme.c
+++ scripts/copy_from_pqclean/src/sig/family/sig_scheme.c
@@ -28,18 +28,18 @@ OQS_SIG *OQS_SIG_{{ family }}_{{ scheme['scheme'] }}_new() {
 	return sig;
 }
 
-int PQCLEAN_{{ scheme['pqclean_scheme_c']|upper }}_CLEAN_crypto_sign_keypair(uint8_t *pk, uint8_t *sk);
-int PQCLEAN_{{ scheme['pqclean_scheme_c']|upper }}_CLEAN_crypto_sign_signature(uint8_t *sig, size_t *siglen, const uint8_t *m, size_t mlen, const uint8_t *sk);
-int PQCLEAN_{{ scheme['pqclean_scheme_c']|upper }}_CLEAN_crypto_sign_verify(const uint8_t *sig, size_t siglen, const uint8_t *m, size_t mlen, const uint8_t *pk);
+int PQCLEAN_{{ scheme['pqclean_scheme_c']|upper }}_{{ scheme['implementation']|upper }}_crypto_sign_keypair(uint8_t *pk, uint8_t *sk);
+int PQCLEAN_{{ scheme['pqclean_scheme_c']|upper }}_{{ scheme['implementation']|upper }}_crypto_sign_signature(uint8_t *sig, size_t *siglen, const uint8_t *m, size_t mlen, const uint8_t *sk);
+int PQCLEAN_{{ scheme['pqclean_scheme_c']|upper }}_{{ scheme['implementation']|upper }}_crypto_sign_verify(const uint8_t *sig, size_t siglen, const uint8_t *m, size_t mlen, const uint8_t *pk);
 
 OQS_API OQS_STATUS OQS_SIG_{{ family }}_{{ scheme['scheme'] }}_keypair(uint8_t *public_key, uint8_t *secret_key) {
-	return (OQS_STATUS) PQCLEAN_{{ scheme['pqclean_scheme_c']|upper }}_CLEAN_crypto_sign_keypair(public_key, secret_key);
+	return (OQS_STATUS) PQCLEAN_{{ scheme['pqclean_scheme_c']|upper }}_{{ scheme['implementation']|upper }}_crypto_sign_keypair(public_key, secret_key);
 }
 OQS_API OQS_STATUS OQS_SIG_{{ family }}_{{ scheme['scheme'] }}_sign(uint8_t *signature, size_t *signature_len, const uint8_t *message, size_t message_len, const uint8_t *secret_key) {
-	return (OQS_STATUS) PQCLEAN_{{ scheme['pqclean_scheme_c']|upper }}_CLEAN_crypto_sign_signature(signature, signature_len, message, message_len, secret_key);
+	return (OQS_STATUS) PQCLEAN_{{ scheme['pqclean_scheme_c']|upper }}_{{ scheme['implementation']|upper }}_crypto_sign_signature(signature, signature_len, message, message_len, secret_key);
 }
 OQS_API OQS_STATUS OQS_SIG_{{ family }}_{{ scheme['scheme'] }}_verify(const uint8_t *message, size_t message_len, const uint8_t *signature, size_t signature_len, const uint8_t *public_key) {
-	return (OQS_STATUS) PQCLEAN_{{ scheme['pqclean_scheme_c']|upper }}_CLEAN_crypto_sign_verify(signature, signature_len, message, message_len, public_key);
+	return (OQS_STATUS) PQCLEAN_{{ scheme['pqclean_scheme_c']|upper }}_{{ scheme['implementation']|upper }}_crypto_sign_verify(signature, signature_len, message, message_len, public_key);
 }
 
 #endif
