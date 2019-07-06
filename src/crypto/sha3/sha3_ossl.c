9c0bbbfa17fec5f97d59124acd0a10afafe2f82b
Douglas Stebila <dstebila@uwaterloo.ca>
2019-07-05 21:28:50 -0400

    OpenSSL wrapper for SHA3 and SHAKE non-incremental functions

--- src/crypto/sha3/sha3_ossl.c
+++ src/crypto/sha3/sha3_ossl.c
@@ -21,10 +21,43 @@ static void do_hash(uint8_t *output, const uint8_t *input, size_t inplen, const 
 	EVP_MD_CTX_destroy(mdctx);
 }
 
+static void do_xof(uint8_t *output, size_t outlen, const uint8_t *input, size_t inplen, const EVP_MD *md) {
+	EVP_MD_CTX *mdctx;
+	mdctx = EVP_MD_CTX_create();
+	EVP_DigestInit_ex(mdctx, md, NULL);
+	EVP_DigestUpdate(mdctx, input, inplen);
+	EVP_DigestFinalXOF(mdctx, output, outlen);
+	EVP_MD_CTX_destroy(mdctx);
+}
+
 void OQS_SHA3_sha3_256(uint8_t *output, const uint8_t *input, size_t inplen) {
 	const EVP_MD *md;
 	md = EVP_sha3_256();
 	do_hash(output, input, inplen, md);
+}
+
+void OQS_SHA3_sha3_384(uint8_t *output, const uint8_t *input, size_t inplen) {
+	const EVP_MD *md;
+	md = EVP_sha3_384();
+	do_hash(output, input, inplen, md);
+}
+
+void OQS_SHA3_sha3_512(uint8_t *output, const uint8_t *input, size_t inplen) {
+	const EVP_MD *md;
+	md = EVP_sha3_512();
+	do_hash(output, input, inplen, md);
+}
+
+void OQS_SHA3_shake128(uint8_t *output, size_t outlen, const uint8_t *input, size_t inplen) {
+	const EVP_MD *md;
+	md = EVP_shake128();
+	do_xof(output, outlen, input, inplen, md);
+}
+
+void OQS_SHA3_shake256(uint8_t *output, size_t outlen, const uint8_t *input, size_t inplen) {
+	const EVP_MD *md;
+	md = EVP_shake256();
+	do_xof(output, outlen, input, inplen, md);
 }
 
 #endif
