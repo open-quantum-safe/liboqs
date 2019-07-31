e7ecb4263a742139f7dd9d2645d5672aa184e456
Douglas Stebila <dstebila@uwaterloo.ca>
2019-07-30 22:41:20 -0400

    Don't use subprocess.run

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,6 +1,28 @@
 import os
 import subprocess
 
+# subprocess.run is not defined on older versions of Python that are present on our test platform
+# so we need to supply our own backport
+# see https://stackoverflow.com/a/40590445
+def run(*popenargs, input=None, check=False, **kwargs):
+    if input is not None:
+        if 'stdin' in kwargs:
+            raise ValueError('stdin and input arguments may not both be used.')
+        kwargs['stdin'] = subprocess.PIPE
+
+    process = subprocess.Popen(*popenargs, **kwargs)
+    try:
+        stdout, stderr = process.communicate(input)
+    except:
+        process.kill()
+        process.wait()
+        raise
+    retcode = process.poll()
+    if check and retcode:
+        raise subprocess.CalledProcessError(
+            retcode, process.args, output=stdout, stderr=stderr)
+    return retcode, stdout, stderr
+
 def run_subprocess(command, working_dir='.', env=None, expected_returncode=0, input=None):
     """
     Helper function to run a shell command and report success/failure
@@ -15,7 +37,7 @@ def run_subprocess(command, working_dir='.', env=None, expected_returncode=0, in
     # then print it, which nose/unittest will then capture and
     # buffer appropriately
     print(working_dir + " > " + " ".join(command))
-    result = subprocess.run(
+    retcode, stdout, stderr = run(
         command,
         input=input,
         stdout=subprocess.PIPE,
@@ -23,10 +45,6 @@ def run_subprocess(command, working_dir='.', env=None, expected_returncode=0, in
         cwd=working_dir,
         env=env,
     )
-    print(result.stdout.decode('utf-8'))
-    if expected_returncode is not None:
-        assert result.returncode == expected_returncode, \
-            "Got unexpected return code {}".format(result.returncode)
-    else:
-        return (result.returncode, result.stdout.decode('utf-8'))
-    return result.stdout.decode('utf-8')
+    print(stdout.decode('utf-8'))
+    assert retcode == expected_returncode, "Got unexpected return code {}".format(retcode)
+    return stdout.decode('utf-8')
