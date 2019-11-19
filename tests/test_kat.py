a3e086a1d3c06eefd41e9cba83722dafe3bbb5fe
Christian Paquin <cpaquin@microsoft.com>
2019-11-19 15:59:24 -0500

    Also check for AES instructions when branching for qtesla AVX2 KAT file; both are required for avx2 optimization.

--- tests/test_kat.py
+++ tests/test_kat.py
@@ -30,8 +30,8 @@ def test_sig(sig_name):
     kats = []
     for filename in os.listdir(os.path.join('tests', 'KATs', 'sig')):
         if filename.startswith(sig_name + '.') and filename.endswith('.kat'):
-            # qtesla's avx2 implementation uses an optimized sampling method that results in different KAT values; we use the correct one
-            if not sig_name.startswith('qTesla') or not (bool(helpers.is_use_option_enabled_by_name('AVX2_INSTRUCTION')) ^ bool('avx2' in filename)):
+            # qtesla's avx2 implementation uses an optimized sampling method that results in different KAT values; we use the correct one (if avx2/aes instructions are available)
+            if not sig_name.startswith('qTesla') or not ((bool(helpers.is_use_option_enabled_by_name('AVX2_INSTRUCTION')) and bool(helpers.is_use_option_enabled_by_name('AES_INSTRUCTIONS'))) ^ bool('avx2' in filename)):
                 with open(os.path.join('tests', 'KATs', 'sig', filename), 'r') as myfile:
                     kats.append(myfile.read())
     assert(output in kats)
