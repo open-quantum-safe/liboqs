69a3755040be0c79bc0b9d25a87e15dbb1a50a12
xvzcf <xvzcf@users.noreply.github.com>
2019-12-12 14:00:58 -0500

    First shot at adding cpu extension checks to CMake.

---
+++ config/detect-cpu-extensions.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void){
+#if defined(__GNUC__) && !defined(__clang__)
+    __builtin_cpu_init();
+#endif
+    if(__builtin_cpu_supports("avx")){
+        printf("AVX;");
+    }
+    if(__builtin_cpu_supports("avx2")){
+        printf("AVX2;");
+    }
+    if(__builtin_cpu_supports("avx512bw")){
+        printf("AVX512BW;");
+    }
+    if(__builtin_cpu_supports("avx512dq")){
+        printf("AVX512DQ;");
+    }
+    if(__builtin_cpu_supports("avx512f")){
+        printf("AVX512F");
+    }
+    if(__builtin_cpu_supports("aes")){
+        printf("AES");
+    }
+    return EXIT_SUCCESS;
+}
