c17c282c6da8bcda10909ba75c55ff411ebc598c
Douglas Stebila <dstebila@uwaterloo.ca>
2020-01-27 19:45:11 -0500

    OR not AND

--- scripts/git_no_checkin_in_last_day.sh
+++ scripts/git_no_checkin_in_last_day.sh
@@ -6,7 +6,7 @@
 r=`git log --name-only --since="1 day ago" -n 2`
 if [ "x$r" == "x" ]; then 
 	echo "No commit in the last day. No build/test required. Exiting."
-	exit 1
-else
 	exit 0
+else
+	exit 1
 fi
