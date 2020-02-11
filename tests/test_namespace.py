19131e377da2f0e9ce6d224692e9e1d7b06aad68
xvzcf <xvzcf@users.noreply.github.com>
2020-02-10 19:18:52 -0500

    Added back non-ARM CircleCI workflows and fixed associated bugs.

--- tests/test_namespace.py
+++ tests/test_namespace.py
@@ -7,10 +7,15 @@ import sys
 
 @helpers.filtered_test
 @pytest.mark.skipif(sys.platform.startswith("win"), reason="Not needed on Windows")
-def test_namespace():
-    out = helpers.run_subprocess(
-        ['nm', '-g', 'build/lib/liboqs.a']
-    )
+def test_namespace(use_liboqs_so):
+    if use_liboqs_so:
+        out = helpers.run_subprocess(
+            ['nm', '-D', 'build/lib/liboqs.so']
+        )
+    else:
+        out = helpers.run_subprocess(
+            ['nm', '-g', 'build/lib/liboqs.a']
+        )
 
     lines = out.strip().split("\n")
     symbols = []
