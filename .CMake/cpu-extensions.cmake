56eece81d13d0fdbe345ce7cef29185af588da0e
xvzcf <xvzcf@users.noreply.github.com>
2020-02-20 11:17:25 -0500

    Improved CPU extension handling and updated README. (#622)

---
+++ .CMake/cpu-extensions.cmake
@@ -0,0 +1,15 @@
+# TODO: Use cpu_features instead of list-cpu-extensions.c
+if(ARCH STREQUAL "x86" OR
+   ARCH STREQUAL "x86_64")
+    try_run(RUN_RESULT COMPILE_RESULT
+            "${CMAKE_BINARY_DIR}" "${PROJECT_SOURCE_DIR}/.CMake/list-cpu-extensions.c"
+            RUN_OUTPUT_VARIABLE RUN_OUTPUT)
+    foreach(CPU_EXTENSION ${RUN_OUTPUT})
+        set(USE_${CPU_EXTENSION}_INSTRUCTIONS TRUE)
+    endforeach()
+    if(USE_AVX512BW_INSTRUCTIONS AND
+       USE_AVX512DQ_INSTRUCTIONS AND
+       USE_AVX512F_INSTRUCTIONS)
+           set(USE_AVX512_INSTRUCTIONS ON)
+    endif()
+endif()
