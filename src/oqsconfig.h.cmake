952fd38f771aae8128aafac406141c1d31987a0a
xvzcf <xvzcf@users.noreply.github.com>
2020-02-24 16:34:02 -0500

    Adding OQS_ prefix to algorithm-enable macros.

--- src/oqsconfig.h.cmake
+++ src/oqsconfig.h.cmake
@@ -25,13 +25,13 @@
 #cmakedefine OQS_USE_SSE3_INSTRUCTIONS 1
 #cmakedefine OQS_USE_SSE4A_INSTRUCTIONS 1
 
-#cmakedefine ENABLE_KEM_BIKE 1
+#cmakedefine OQS_ENABLE_KEM_BIKE 1
 #cmakedefine OQS_ENABLE_KEM_bike1_l1_cpa 1
 #cmakedefine OQS_ENABLE_KEM_bike1_l3_cpa 1
 #cmakedefine OQS_ENABLE_KEM_bike1_l1_fo 1
 #cmakedefine OQS_ENABLE_KEM_bike1_l3_fo 1
 
-#cmakedefine ENABLE_KEM_FRODOKEM 1
+#cmakedefine OQS_ENABLE_KEM_FRODOKEM 1
 #cmakedefine OQS_ENABLE_KEM_frodokem_640_aes 1
 #cmakedefine OQS_ENABLE_KEM_frodokem_640_shake 1
 #cmakedefine OQS_ENABLE_KEM_frodokem_976_aes 1
@@ -39,7 +39,7 @@
 #cmakedefine OQS_ENABLE_KEM_frodokem_1344_aes 1
 #cmakedefine OQS_ENABLE_KEM_frodokem_1344_shake 1
 
-#cmakedefine ENABLE_KEM_SIDH 1
+#cmakedefine OQS_ENABLE_KEM_SIDH 1
 #cmakedefine OQS_ENABLE_KEM_sidh_p434 1
 #cmakedefine OQS_ENABLE_KEM_sidh_p434_compressed 1
 #cmakedefine OQS_ENABLE_KEM_sidh_p503 1
@@ -49,7 +49,7 @@
 #cmakedefine OQS_ENABLE_KEM_sidh_p751 1
 #cmakedefine OQS_ENABLE_KEM_sidh_p751_compressed 1
 
-#cmakedefine ENABLE_KEM_SIKE 1
+#cmakedefine OQS_ENABLE_KEM_SIKE 1
 #cmakedefine OQS_ENABLE_KEM_sike_p434 1
 #cmakedefine OQS_ENABLE_KEM_sike_p434_compressed 1
 #cmakedefine OQS_ENABLE_KEM_sike_p503 1
@@ -59,7 +59,7 @@
 #cmakedefine OQS_ENABLE_KEM_sike_p751 1
 #cmakedefine OQS_ENABLE_KEM_sike_p751_compressed 1
 
-#cmakedefine ENABLE_SIG_PICNIC 1
+#cmakedefine OQS_ENABLE_SIG_PICNIC 1
 #cmakedefine OQS_ENABLE_SIG_picnic_L1_UR 1
 #cmakedefine OQS_ENABLE_SIG_picnic_L1_FS 1
 #cmakedefine OQS_ENABLE_SIG_picnic_L3_UR 1
@@ -70,12 +70,12 @@
 #cmakedefine OQS_ENABLE_SIG_picnic2_L3_FS 1
 #cmakedefine OQS_ENABLE_SIG_picnic2_L5_FS 1
 
-#cmakedefine ENABLE_SIG_QTESLA 1
+#cmakedefine OQS_ENABLE_SIG_QTESLA 1
 #cmakedefine OQS_ENABLE_SIG_qTesla_p_I 1
 #cmakedefine OQS_ENABLE_SIG_qTesla_p_III 1
 
 ///// OQS_COPY_FROM_PQCLEAN_FRAGMENT_ADD_ALG_ENABLE_DEFINES_START
-#cmakedefine ENABLE_KEM_KYBER 1
+#cmakedefine OQS_ENABLE_KEM_KYBER 1
 #cmakedefine OQS_ENABLE_KEM_kyber_512 1
 #cmakedefine OQS_ENABLE_KEM_kyber_768 1
 #cmakedefine OQS_ENABLE_KEM_kyber_1024 1
@@ -83,27 +83,27 @@
 #cmakedefine OQS_ENABLE_KEM_kyber_768_90s 1
 #cmakedefine OQS_ENABLE_KEM_kyber_1024_90s 1
 
-#cmakedefine ENABLE_KEM_LEDACRYPT 1
+#cmakedefine OQS_ENABLE_KEM_LEDACRYPT 1
 #cmakedefine OQS_ENABLE_KEM_ledacrypt_ledakemlt12 1
 #cmakedefine OQS_ENABLE_KEM_ledacrypt_ledakemlt32 1
 #cmakedefine OQS_ENABLE_KEM_ledacrypt_ledakemlt52 1
 
-#cmakedefine ENABLE_KEM_NEWHOPE 1
+#cmakedefine OQS_ENABLE_KEM_NEWHOPE 1
 #cmakedefine OQS_ENABLE_KEM_newhope_512cca 1
 #cmakedefine OQS_ENABLE_KEM_newhope_1024cca 1
 
-#cmakedefine ENABLE_KEM_NTRU 1
+#cmakedefine OQS_ENABLE_KEM_NTRU 1
 #cmakedefine OQS_ENABLE_KEM_ntru_hps2048509 1
 #cmakedefine OQS_ENABLE_KEM_ntru_hps2048677 1
 #cmakedefine OQS_ENABLE_KEM_ntru_hps4096821 1
 #cmakedefine OQS_ENABLE_KEM_ntru_hrss701 1
 
-#cmakedefine ENABLE_KEM_SABER 1
+#cmakedefine OQS_ENABLE_KEM_SABER 1
 #cmakedefine OQS_ENABLE_KEM_saber_lightsaber 1
 #cmakedefine OQS_ENABLE_KEM_saber_saber 1
 #cmakedefine OQS_ENABLE_KEM_saber_firesaber 1
 
-#cmakedefine ENABLE_KEM_THREEBEARS 1
+#cmakedefine OQS_ENABLE_KEM_THREEBEARS 1
 #cmakedefine OQS_ENABLE_KEM_threebears_babybear 1
 #cmakedefine OQS_ENABLE_KEM_threebears_babybear_ephem 1
 #cmakedefine OQS_ENABLE_KEM_threebears_mamabear 1
@@ -111,16 +111,16 @@
 #cmakedefine OQS_ENABLE_KEM_threebears_papabear 1
 #cmakedefine OQS_ENABLE_KEM_threebears_papabear_ephem 1
 
-#cmakedefine ENABLE_SIG_DILITHIUM 1
+#cmakedefine OQS_ENABLE_SIG_DILITHIUM
 #cmakedefine OQS_ENABLE_SIG_dilithium_2 1
 #cmakedefine OQS_ENABLE_SIG_dilithium_3 1
 #cmakedefine OQS_ENABLE_SIG_dilithium_4 1
 
-#cmakedefine ENABLE_SIG_MQDSS 1
+#cmakedefine OQS_ENABLE_SIG_MQDSS
 #cmakedefine OQS_ENABLE_SIG_mqdss_31_48 1
 #cmakedefine OQS_ENABLE_SIG_mqdss_31_64 1
 
-#cmakedefine ENABLE_SIG_SPHINCS 1
+#cmakedefine OQS_ENABLE_SIG_SPHINCS
 #cmakedefine OQS_ENABLE_SIG_sphincs_haraka_128f_robust 1
 #cmakedefine OQS_ENABLE_SIG_sphincs_haraka_128f_simple 1
 #cmakedefine OQS_ENABLE_SIG_sphincs_haraka_128s_robust 1
