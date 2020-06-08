bca9544cfb4d8c449a440e4fc3dd9e985b3eab0d
pi <cpranaviyer@gmail.com>
2020-06-08 17:01:23 -0600

    removing errors

--- liboqs-python/piTest/test.py
+++ liboqs-python/piTest/test.py
@@ -6,8 +6,7 @@ print ("Custom test dilithium3")
 message = bytes(random.getrandbits(8) for _ in range (100))
 sig = oqs.Signature("DILITHIUM_3")
 signature = sig.generate_keypair()
-publicKey = signature.public_key
 
-##print (signature )
+print (signature )
 
 print (publicKey)
