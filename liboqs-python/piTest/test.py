fa2f2331bf8d813afc93f2e75ffc77d8e33cbdd5
pi <cpranaviyer@gmail.com>
2020-06-08 16:06:15 -0600

    printing public key

--- liboqs-python/piTest/test.py
+++ liboqs-python/piTest/test.py
@@ -5,6 +5,9 @@ print ("Custom test dilithium3")
 
 message = bytes(random.getrandbits(8) for _ in range (100))
 sig = oqs.Signature("DILITHIUM_3")
-public_key = sig.generate_keypair()
+signature = sig.generate_keypair()
+publicKey = signature.public_key
 
-print (public_key)
+##print (signature )
+
+print (publicKey)
