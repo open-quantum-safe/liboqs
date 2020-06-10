12d9a61f6f3ca304c06fbd208367c28d0d15997b
Pi Iyer <54725951+prchander@users.noreply.github.com>
2020-06-10 15:06:02 -0600

    changing variables

--- liboqs-python/piTest/test.py
+++ liboqs-python/piTest/test.py
@@ -5,9 +5,9 @@ print ("Custom test dilithium3")
 
 message = bytes(random.getrandbits(8) for _ in range (100))
 sig = oqs.Signature("DILITHIUM_3")
-signature = sig.generate_keypair()
+publicKey = sig.generate_keypair()
 privateKey = sig.export_secret_key()
 
-print (signature)
+print (publicKey)
 print ()
 print (privateKey)
