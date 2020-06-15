0ae37de731c97c62162bd10c30079cb1193442f1
Pi <pchandra@uccs.edu>
2020-06-15 09:54:44 -0600

    making it versitile

--- liboqs-python/piTest/test.py
+++ liboqs-python/piTest/test.py
@@ -4,8 +4,13 @@ import base64
 
 message = bytes(random.getrandbits(8) for _ in range (100))
 sig = oqs.Signature("DILITHIUM_2")
-publicKey = sig.generate_keypair()
-privateKey = sig.export_secret_key()
+dil2 = oqs.Signature("DILITHIUM_2")
+dil3 = oqs.Signature("DILITHIUM_3")
+
+dil2_publicKey = dil2.generate_keypair()
+dil2_privateKey = dil2.export_secret_key()
+##publicKey = sig.generate_keypair()
+##privateKey = sig.export_secret_key()
 
 ##print ("Printing algorithms")
 ##print (oqs._enabled_sigs)
@@ -17,5 +22,7 @@ privateKey = sig.export_secret_key()
 ##print ()
 ##print (privateKey)
 
-with open("Pkey.txt", 'wb') as out:
-    out.write(privateKey)
+print (dil2_privateKey)
+
+##with open("Pkey.txt", 'wb') as out:
+##    out.write(privateKey)
