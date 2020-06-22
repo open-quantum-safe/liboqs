4786fd2ef12307d63340e792b909086fb6bba4c0
Pi Iyer <54725951+prchander@users.noreply.github.com>
2020-06-22 17:29:27 -0600

    test with multiple functons

--- liboqs-python/oqs/test1.py
+++ liboqs-python/oqs/test1.py
@@ -1,2 +1,14 @@
 import oqs
 
+global alg = oqs.Signature("DILITHIUM_2")
+
+def pqPublic():
+    public=alg.generate_keypair()
+    return public
+
+def pqPrivate()
+    private = alg.export_secret_key()
+    return private
+
+print (pqPublic())
+print (pqPrivate())
