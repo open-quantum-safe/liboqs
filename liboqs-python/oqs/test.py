14c1e416d45eab91974ef0e1b17a748159b36b86
Pi Iyer <54725951+prchander@users.noreply.github.com>
2020-06-22 09:50:27 -0600

    testing self commands

--- liboqs-python/oqs/test.py
+++ liboqs-python/oqs/test.py
@@ -1,17 +1,34 @@
 import oqs
 
-public = bytearray()
+##public = bytearray()
 
-def gen_dili2_private_key(dil2):
-    alg = oqs.Signature(dil2)
-    public = alg.generate_keypair()
-    pKey = alg.export_secret_key()
+##def gen_dili2_private_key(dil2):
+##    alg = oqs.Signature(dil2)
+##    public = alg.generate_keypair()
+##    pKey = alg.export_secret_key()
     
-    return pKey
+##    return pKey
 
-gen_dili2_private_key("DILITHIUM_2")
-print (public)
+##gen_dili2_private_key("DILITHIUM_2")
+##print (public)
 
-def gen_dili2_public_key(public)
+##def gen_dili2_public_key(public)
 
-    return public
+##    return public
+
+
+# A Python program to return multiple  
+# values from a method using class 
+class Test: 
+    def __init__(self): 
+        self.private 
+        self.public   
+  
+# This function returns an object of Test 
+def fun(): 
+    return Test() 
+      
+# Driver code to test above method 
+t = fun()  
+print(t.str) 
+print(t.x) 
