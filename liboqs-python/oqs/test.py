e49eb0104e1bf73a045ce047c020a7bdf93babde
Pi Iyer <54725951+prchander@users.noreply.github.com>
2020-06-22 17:32:12 -0600

    swapping to original code

--- liboqs-python/oqs/test.py
+++ liboqs-python/oqs/test.py
@@ -1,37 +1,16 @@
 import oqs
 
-##public = bytearray()
 
-##def gen_dili2_private_key(dil2):
-##    alg = oqs.Signature(dil2)
-##    public = alg.generate_keypair()
-##    pKey = alg.export_secret_key()
-    
-##    return pKey
+global alg
+alg = oqs.Signature("DILITHIUM_2")
 
-##gen_dili2_private_key("DILITHIUM_2")
-##print (public)
+def pqPublic():
+    public=alg.generate_keypair()
+    return public
 
-##def gen_dili2_public_key(public)
+def pqPrivate():
+    private = alg.export_secret_key()
+    return private
 
-##    return public
-
-
-# A Python program to return multiple  
-# values from a method using class 
-class Test: 
-    def __init__(self): 
-       self.private = " "
-       self.public = " "
-
-    global t 
-
-# This function returns an object of Test 
-    def gen_dili2_private_key():
-        alg = oqs.Signature("DILITHIUM_2")
-        public = alg.generate_keypair()
-        private = alg.export_secret_key()
-        self.private = private
-        self.public = public
-
-# Driver code to test above method 
+print (pqPublic())
+print (pqPrivate())
