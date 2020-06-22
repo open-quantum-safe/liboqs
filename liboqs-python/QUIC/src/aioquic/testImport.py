227ac1fcdc187070d32c9bd8b6976a76f1c1d815
Pi Iyer <54725951+prchander@users.noreply.github.com>
2020-06-22 13:36:06 -0600

    changing variable names

--- liboqs-python/QUIC/src/aioquic/testImport.py
+++ liboqs-python/QUIC/src/aioquic/testImport.py
@@ -4,9 +4,9 @@ sys.path.insert(1,'/home/pi/PQQUIC/modLiboqs/liboqs/liboqs-python/oqs')
 import test
 
 print ("TEST PRINTING OQS KEYS\n")
-t = test.Test()
-t.gen_dili2_private_key("DILITHIUM_2")
+f = test.Test()
+f.gen_dili2_private_key("DILITHIUM_2")
 
 
-print (t.private)
-print (t.public)
+print (f.private)
+print (f.public)
