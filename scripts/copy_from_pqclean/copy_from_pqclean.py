b24a046495d67fe14477542e1ff6bf45ab22b140
Michael Baentsch <57787676+baentsch@users.noreply.github.com>
2020-07-16 15:36:58 +0200

    Enable automated upstream imports (#789)

--- scripts/copy_from_pqclean/copy_from_pqclean.py
+++ scripts/copy_from_pqclean/copy_from_pqclean.py
@@ -10,10 +10,25 @@ import shutil
 import subprocess
 import yaml
 from pathlib import Path
+import sys
+import json
+
+# kats of all algs
+kats = {}
+
+# The higher the value the more is output
+if len(sys.argv)>1:
+   DEBUG=int(sys.argv[1])
+else:
+   DEBUG=0
 
 if 'PQCLEAN_DIR' not in os.environ:
     print("Must set environment variable PQCLEAN_DIR")
     exit(1)
+
+for t in ["kem", "sig"]:
+   with open(os.path.join('tests', 'KATs', t, 'kats.json'), 'r') as fp:
+      kats[t] = json.load(fp)
 
 def file_get_contents(filename, encoding=None):
     with open(filename, mode='r', encoding=encoding) as fh:
@@ -50,6 +65,28 @@ def replacer(filename, instructions, delimiter):
         contents = preamble + identifier_start + jinja2.Template(template).render({'instructions': instructions}) + postamble
     file_put_contents(filename, contents)
 
+def upstream_check(scheme):
+    # check for presence of an upstream location and insert into scheme if found:
+    try:
+      if 'upstream_location' in scheme:
+        if os.environ.get(scheme['upstream_location']):
+          if os.path.isdir(os.environ.get(scheme['upstream_location'])):
+            if DEBUG>0:
+               print("Loading META-yml from %s" % (os.path.join(os.environ.get(scheme['upstream_location']), '{}_META.yml'.format(scheme['pretty_name_full']))))
+            scheme['metadata'] = yaml.safe_load(file_get_contents(os.path.join(os.environ.get(scheme['upstream_location']), '{}_META.yml'.format(scheme['pretty_name_full']))))
+          else:
+            if DEBUG>0:
+              print("upstream location '%s' set but not a directory in the local file system. Falling back to PQClean." % (scheme['upstream_location']))
+            scheme.pop('upstream_location', None)
+        else:
+          if DEBUG>0:
+             print("upstream location '%s' set but not found in environment. Falling back to PQClean." % (scheme['upstream_location']))
+    except FileNotFoundError as fnf:
+      if DEBUG>0:
+         print("%s. Falling back to PQClean." %(fnf))
+      scheme.pop('upstream_location', None)
+      pass
+
 def load_instructions():
     instructions = file_get_contents(os.path.join('scripts', 'copy_from_pqclean', 'copy_from_pqclean.yml'), encoding='utf-8')
     instructions = yaml.safe_load(instructions)
@@ -58,7 +95,9 @@ def load_instructions():
         family['pqclean_type'] = 'kem'
         family['family'] = family['name']
         for scheme in family['schemes']:
-            scheme['metadata'] = yaml.safe_load(file_get_contents(os.path.join(os.environ['PQCLEAN_DIR'], 'crypto_kem', scheme['pqclean_scheme'], 'META.yml')))
+            upstream_check(scheme)
+            if not 'metadata' in scheme:
+                scheme['metadata'] = yaml.safe_load(file_get_contents(os.path.join(os.environ['PQCLEAN_DIR'], 'crypto_kem', scheme['pqclean_scheme'], 'META.yml')))
             scheme['metadata']['ind_cca'] = 'true' if (scheme['metadata']['claimed-security'] == "IND-CCA2") else 'false'
             scheme['pqclean_scheme_c'] = scheme['pqclean_scheme'].replace('-', '')
             scheme['scheme_c'] = scheme['scheme'].replace('-', '')
@@ -68,13 +107,15 @@ def load_instructions():
         family['pqclean_type'] = 'sign'
         family['family'] = family['name']
         for scheme in family['schemes']:
-            scheme['metadata'] = yaml.safe_load(file_get_contents(os.path.join(os.environ['PQCLEAN_DIR'], 'crypto_sign', scheme['pqclean_scheme'], 'META.yml')))
-            # This is a temporary hack to work around the fact that
-            # the PQClean's META.ymls for the Dilithium AVX2 variants
-            # are not properly specified.
-            if scheme['pretty_name_full'].startswith('DILITHIUM_'):
-                scheme['metadata']['implementations'][1]['supported_platforms'][0]['operating_systems'] = ['Linux']
-                scheme['metadata']['implementations'][1]['supported_platforms'][0]['required_flags'] = ['avx2', 'bmi', 'popcnt']
+            upstream_check(scheme)
+            if not 'metadata' in scheme:
+               scheme['metadata'] = yaml.safe_load(file_get_contents(os.path.join(os.environ['PQCLEAN_DIR'], 'crypto_sign', scheme['pqclean_scheme'], 'META.yml')))
+               # This is a temporary hack to work around the fact that
+               # the PQClean's META.ymls for the Dilithium AVX2 variants
+               # are not properly specified.
+               if scheme['pretty_name_full'].startswith('DILITHIUM_'):
+                   scheme['metadata']['implementations'][1]['supported_platforms'][0]['operating_systems'] = ['Linux']
+                   scheme['metadata']['implementations'][1]['supported_platforms'][0]['required_flags'] = ['avx2', 'bmi', 'popcnt']
 
             scheme['metadata']['euf_cma'] = 'true'
             scheme['pqclean_scheme_c'] = scheme['pqclean_scheme'].replace('-', '')
@@ -85,21 +126,65 @@ def load_instructions():
 # Copy over all files for a given impl in a family using scheme
 # Returns list of all relative source files
 def handle_implementation(impl, family, scheme):
+        # Obtain current implementation array in i
+        for imp in scheme['metadata']['implementations']:
+           if imp['name'] == impl:
+             i = imp
+        if DEBUG>2:
+           print("IMP = %s" %(i))
+        #if 'upstream_location' in scheme and os.environ.get(scheme['upstream_location']):
+        if DEBUG>3:
+           print("Obtain files for implementation %s" % (impl))
+           print("Obtain files for %s" % (scheme))
         shutil.rmtree(os.path.join('src', family['type'], family['name'], 'pqclean_{}_{}'.format(scheme['pqclean_scheme'], impl)), ignore_errors=True)
         srcfolder = os.path.join('src', family['type'], family['name'], 'pqclean_{}_{}'.format(scheme['pqclean_scheme'], impl))
-        subprocess.run([
-            'cp',
-            '-pr',
-            os.path.join(os.environ['PQCLEAN_DIR'], 'crypto_' + family['pqclean_type'], scheme['pqclean_scheme'], impl),
-            srcfolder
-        ])
+        if 'upstream_location' in scheme and os.environ.get(scheme['upstream_location']):
+           # Don't copy from PQClean straight but check for origfile list
+           try:
+              os.mkdir(srcfolder)
+           except FileExistsError as fee:
+              print(fee)
+              pass
+           # determine origin folder of (may be renamed via 'folder_name'):
+           if 'folder_name' in i:
+              of = i['folder_name']
+           else:
+              of = impl
+           origfolder = os.path.join(os.environ[scheme['upstream_location']], of)
+           # determine list of files to copy:
+           if 'sources' in i:
+              srcs = i['sources'].split(" ")
+              for s in srcs:
+                 # Copy recursively only in case of directories not with plain files to avoid copying over symbolic links
+                 if os.path.isfile(os.path.join(origfolder, s)):
+                    subprocess.run([ 'cp', os.path.join(origfolder, s), os.path.join(srcfolder, s) ])
+                 else:
+                    subprocess.run([ 'cp', '-r', os.path.join(origfolder, s), os.path.join(srcfolder, s) ])
+           else:
+              raise Exception("Malformed YML file: No sources listed to copy. Check upstream YML file." )
+           
+        else: # simple case: PQClean sourcefolder copy
+           origfolder = os.path.join(os.environ['PQCLEAN_DIR'], 'crypto_' + family['pqclean_type'], scheme['pqclean_scheme'], impl)
+           subprocess.run([ 'cp', '-pr', origfolder, srcfolder ])
+
         try:
             os.remove(os.path.join('src', family['type'], family['name'], 'pqclean_{}_{}'.format(scheme['pqclean_scheme'], impl), 'Makefile'))
             os.remove(os.path.join('src', family['type'], family['name'], 'pqclean_{}_{}'.format(scheme['pqclean_scheme'], impl), 'Makefile.Microsoft_nmake'))
         except FileNotFoundError:
            pass
         extensions = [ '.c', '.s' ]
-        return [str(x.relative_to(srcfolder)) for x in Path(srcfolder).iterdir() if x.suffix.lower() in extensions]
+        ffs = []
+        for subdir, dirs, files in os.walk(srcfolder):
+          for x in files:
+           for i in extensions:
+              if x.lower().endswith(i):
+                 fname = subdir + os.sep + x
+                 if DEBUG>2:
+                    print("srcfolder: %s - File: %s" % (srcfolder, fname))
+                 ffs.append(fname)
+        if DEBUG>2:
+          print(ffs)
+        return [x[len(srcfolder)+1:] for x in ffs]
 
 
 instructions = load_instructions()
@@ -113,6 +198,8 @@ for family in instructions['kems'] + instructions['sigs']:
         if 'implementation' in scheme:
             impl = scheme['implementation']
             srcs = handle_implementation(impl, family, scheme)
+            if DEBUG>3: 
+               print("SRCs found: %s" % (srcs))
             if (scheme['sources']):
                assert(len(scheme['sources']) == len(srcs))
             # in any case: add 'sources' to implementation(s)
@@ -123,6 +210,8 @@ for family in instructions['kems'] + instructions['sigs']:
             # If no scheme['implementation'] given, get the list from META.yml and add all implementations
            for impl in scheme['metadata']['implementations']:
                srcs = handle_implementation(impl['name'], family, scheme)
+               if DEBUG>2:
+                 print("SRCs found: %s" % (srcs))
                # in any case: add 'sources' to implementation(s)
                impl['sources'] = srcs
                # also add suitable defines:
@@ -134,6 +223,23 @@ for family in instructions['kems'] + instructions['sigs']:
                       if (impl['name'] != family['default_implementation']):
                           print("No required flags found for %s (KeyError %s on impl %s)\n" % (scheme['scheme'], str(ke), impl['name']))
                       pass
+
+        if family in instructions['kems']:
+          try:
+            if kats['kem'][scheme['pretty_name_full']] != scheme['metadata']['nistkat-sha256']:
+                print("Info: Updating KAT for %s" % (scheme['pretty_name_full']))
+          except KeyError: # new key
+            print("Adding new KAT for %s" % (scheme['pretty_name_full']))
+            pass
+          kats['kem'][scheme['pretty_name_full']] = scheme['metadata']['nistkat-sha256'] 
+        else:
+          try:
+            if kats['sig'][scheme['pretty_name_full']] != scheme['metadata']['nistkat-sha256']:
+                print("Info: Updating KAT for %s" % (scheme['pretty_name_full']))
+          except KeyError: # new key
+            print("Adding new KAT for %s" % (scheme['pretty_name_full']))
+            pass
+          kats['sig'][scheme['pretty_name_full']] = scheme['metadata']['nistkat-sha256'] 
 
     generator(
         os.path.join('src', family['type'], family['name'], family['type'] + '_{}.h'.format(family['name'])),
@@ -166,3 +272,10 @@ replacer('src/kem/kem.h', instructions, '/////')
 replacer('src/sig/sig.c', instructions, '/////')
 replacer('src/sig/sig.h', instructions, '/////')
 replacer('tests/kat_sig.c', instructions, '/////')
+
+# Finally store KATS away again
+for t in ["kem", "sig"]:
+   with open(os.path.join('tests', 'KATs', t, 'kats.json'), "w") as f:
+      json.dump(kats[t], f)
+
+
--- scripts/copy_from_pqclean/src/kem/family/kem_scheme.c
+++ scripts/copy_from_pqclean/src/kem/family/kem_scheme.c
@@ -31,15 +31,52 @@ OQS_KEM *OQS_KEM_{{ family }}_{{ scheme['scheme'] }}_new() {
 	return kem;
 }
 
-extern int PQCLEAN_{{ scheme['pqclean_scheme_c']|upper }}_{{ scheme['default_implementation']|upper }}_crypto_kem_keypair(unsigned char *pk, unsigned char *sk);
-extern int PQCLEAN_{{ scheme['pqclean_scheme_c']|upper }}_{{ scheme['default_implementation']|upper }}_crypto_kem_enc(unsigned char *ct, unsigned char *ss, const unsigned char *pk);
-extern int PQCLEAN_{{ scheme['pqclean_scheme_c']|upper }}_{{ scheme['default_implementation']|upper }}_crypto_kem_dec(unsigned char *ss, const unsigned char *ct, const unsigned char *sk);
+    {%- for impl in scheme['metadata']['implementations'] if impl['name'] == scheme['default_implementation'] %}
+
+        {%- if impl['signature_keypair'] %}
+           {%- set cleankeypair = scheme['metadata'].update({'default_keypair_signature': impl['signature_keypair']}) -%}
+        {%- else %}
+           {%- set cleankeypair = scheme['metadata'].update({'default_keypair_signature': "PQCLEAN_"+scheme['pqclean_scheme_c']|upper+"_"+scheme['default_implementation']|upper+"_crypto_kem_keypair"}) -%}
+        {%- endif %}
+
+extern int {{ scheme['metadata']['default_keypair_signature'] }}(unsigned char *pk, unsigned char *sk);
+
+        {%- if impl['signature_enc'] %}
+           {%- set cleanenc = scheme['metadata'].update({'default_enc_signature': impl['signature_enc']}) -%}
+        {%- else %}
+           {%- set cleanenc = scheme['metadata'].update({'default_enc_signature': "PQCLEAN_"+scheme['pqclean_scheme_c']|upper+"_"+scheme['default_implementation']|upper+"_crypto_kem_enc"}) -%}
+        {%- endif %}
+extern int {{ scheme['metadata']['default_enc_signature'] }}(unsigned char *ct, unsigned char *ss, const unsigned char *pk);
+
+        {%- if impl['signature_dec'] %}
+           {%- set cleandec = scheme['metadata'].update({'default_dec_signature': impl['signature_dec']}) -%}
+        {%- else %}
+           {%- set cleandec = scheme['metadata'].update({'default_dec_signature': "PQCLEAN_"+scheme['pqclean_scheme_c']|upper+"_"+scheme['default_implementation']|upper+"_crypto_kem_dec"}) -%}
+        {%- endif %}
+extern int {{ scheme['metadata']['default_dec_signature']  }}(unsigned char *ss, const unsigned char *ct, const unsigned char *sk);
+
+    {%- endfor %}
+
     {%- for impl in scheme['metadata']['implementations'] if impl['name'] != scheme['default_implementation'] %}
 
 #if defined(OQS_ENABLE_KEM_{{ family }}_{{ scheme['scheme'] }}_{{ impl['name'] }})
+        {%- if impl['signature_keypair'] %}
+extern int {{ impl['signature_keypair'] }}(unsigned char *pk, unsigned char *sk);
+        {%- else %}
 extern int PQCLEAN_{{ scheme['pqclean_scheme_c']|upper }}_{{ impl['name']|upper }}_crypto_kem_keypair(unsigned char *pk, unsigned char *sk);
+        {%- endif %}
+
+        {%- if impl['signature_enc'] %}
+extern int {{ impl['signature_enc'] }}(unsigned char *ct, unsigned char *ss, const unsigned char *pk);
+        {%- else %}
 extern int PQCLEAN_{{ scheme['pqclean_scheme_c']|upper }}_{{ impl['name']|upper }}_crypto_kem_enc(unsigned char *ct, unsigned char *ss, const unsigned char *pk);
+        {%- endif %}
+
+        {%- if impl['signature_dec'] %}
+extern int {{ impl['signature_dec'] }}(unsigned char *ss, const unsigned char *ct, const unsigned char *sk);
+        {%- else %}
 extern int PQCLEAN_{{ scheme['pqclean_scheme_c']|upper }}_{{ impl['name']|upper }}_crypto_kem_dec(unsigned char *ss, const unsigned char *ct, const unsigned char *sk);
+        {%- endif %}
 #endif
     {%- endfor %}
 
@@ -50,15 +87,19 @@ OQS_API OQS_STATUS OQS_KEM_{{ family }}_{{ scheme['scheme'] }}_keypair(uint8_t *
 	OQS_CPU_EXTENSIONS available_cpu_extensions = OQS_get_available_CPU_extensions();
 	if ({%- for flag in impl['required_flags'] -%}available_cpu_extensions.{{ flag|upper }}_ENABLED{%- if not loop.last %} && {% endif -%}{%- endfor -%}) {
 #endif /* OQS_PORTABLE_BUILD */
+           {%- if impl['signature_keypair'] %}
+		return (OQS_STATUS) {{ impl['signature_keypair'] }}(public_key, secret_key);
+           {%- else %}
 		return (OQS_STATUS) PQCLEAN_{{ scheme['pqclean_scheme_c']|upper }}_{{ impl['name']|upper }}_crypto_kem_keypair(public_key, secret_key);
+           {%- endif %}
 #if defined(OQS_PORTABLE_BUILD)
 	} else {
-		return (OQS_STATUS) PQCLEAN_{{ scheme['pqclean_scheme_c']|upper }}_{{ scheme['default_implementation']|upper }}_crypto_kem_keypair(public_key, secret_key);
+		return (OQS_STATUS) {{ scheme['metadata']['default_keypair_signature'] }}(public_key, secret_key);
 	}
 #endif /* OQS_PORTABLE_BUILD */
 #else
     {%- endfor %}
-	return (OQS_STATUS) PQCLEAN_{{ scheme['pqclean_scheme_c']|upper }}_{{ scheme['default_implementation']|upper }}_crypto_kem_keypair(public_key, secret_key);
+	return (OQS_STATUS) {{ scheme['metadata']['default_keypair_signature'] }}(public_key, secret_key);
     {%- if scheme['metadata']['implementations']|rejectattr('name', 'equalto', scheme['default_implementation'])|list %}
 #endif
     {%- endif %}
@@ -71,15 +112,19 @@ OQS_API OQS_STATUS OQS_KEM_{{ family }}_{{ scheme['scheme'] }}_encaps(uint8_t *c
 	OQS_CPU_EXTENSIONS available_cpu_extensions = OQS_get_available_CPU_extensions();
 	if ({%- for flag in impl['required_flags'] -%}available_cpu_extensions.{{ flag|upper }}_ENABLED{%- if not loop.last %} && {% endif -%}{%- endfor -%}) {
 #endif /* OQS_PORTABLE_BUILD */
+           {%- if impl['signature_enc'] %}
+		return (OQS_STATUS) {{ impl['signature_enc'] }}(ciphertext, shared_secret, public_key);
+           {%- else %}
 		return (OQS_STATUS) PQCLEAN_{{ scheme['pqclean_scheme_c']|upper }}_{{ impl['name']|upper }}_crypto_kem_enc(ciphertext, shared_secret, public_key);
+           {%- endif %}
 #if defined(OQS_PORTABLE_BUILD)
 	} else {
-		return (OQS_STATUS) PQCLEAN_{{ scheme['pqclean_scheme_c']|upper }}_{{ scheme['default_implementation']|upper }}_crypto_kem_enc(ciphertext, shared_secret, public_key);
+		return (OQS_STATUS) {{ scheme['metadata']['default_enc_signature'] }}(ciphertext, shared_secret, public_key);
 	}
 #endif /* OQS_PORTABLE_BUILD */
 #else
     {%- endfor %}
-	return (OQS_STATUS) PQCLEAN_{{ scheme['pqclean_scheme_c']|upper }}_{{ scheme['default_implementation']|upper }}_crypto_kem_enc(ciphertext, shared_secret, public_key);
+	return (OQS_STATUS) {{ scheme['metadata']['default_enc_signature'] }}(ciphertext, shared_secret, public_key);
     {%- if scheme['metadata']['implementations']|rejectattr('name', 'equalto', scheme['default_implementation'])|list %}
 #endif
     {%- endif %}
@@ -92,15 +137,19 @@ OQS_API OQS_STATUS OQS_KEM_{{ family }}_{{ scheme['scheme'] }}_decaps(uint8_t *s
 	OQS_CPU_EXTENSIONS available_cpu_extensions = OQS_get_available_CPU_extensions();
 	if ({%- for flag in impl['required_flags'] -%}available_cpu_extensions.{{ flag|upper }}_ENABLED{%- if not loop.last %} && {% endif -%}{%- endfor -%}) {
 #endif /* OQS_PORTABLE_BUILD */
+           {%- if impl['signature_dec'] %}
+		return (OQS_STATUS) {{ impl['signature_dec'] }}(shared_secret, ciphertext, secret_key);
+           {%- else %}
 		return (OQS_STATUS) PQCLEAN_{{ scheme['pqclean_scheme_c']|upper }}_{{ impl['name']|upper }}_crypto_kem_dec(shared_secret, ciphertext, secret_key);
+           {%- endif %}
 #if defined(OQS_PORTABLE_BUILD)
 	} else {
-		return (OQS_STATUS) PQCLEAN_{{ scheme['pqclean_scheme_c']|upper }}_{{ scheme['default_implementation']|upper }}_crypto_kem_dec(shared_secret, ciphertext, secret_key);
+		return (OQS_STATUS) {{ scheme['metadata']['default_dec_signature'] }}(shared_secret, ciphertext, secret_key);
 	}
 #endif /* OQS_PORTABLE_BUILD */
 #else
     {%- endfor %}
-	return (OQS_STATUS) PQCLEAN_{{ scheme['pqclean_scheme_c']|upper }}_{{ scheme['default_implementation']|upper }}_crypto_kem_dec(shared_secret, ciphertext, secret_key);
+	return (OQS_STATUS) {{ scheme['metadata']['default_dec_signature'] }}(shared_secret, ciphertext, secret_key);
     {%- if scheme['metadata']['implementations']|rejectattr('name', 'equalto', scheme['default_implementation'])|list %}
 #endif
     {%- endif %}
