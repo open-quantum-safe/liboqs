f201ff35169240aa75d55ef22f80b844d06539db
Michael <57787676+baentsch@users.noreply.github.com>
2020-12-02 17:56:37 +0100

    more defensive read file

--- tests/tmp_store.c
+++ tests/tmp_store.c
@@ -22,16 +22,23 @@ static OQS_STATUS oqs_fstore(const char *fname, const char *mname, uint8_t *data
 }
 
 static OQS_STATUS oqs_fload(const char *fname, const char *mname, uint8_t *data, size_t len, size_t *rcvd) {
+	size_t len_read = 0, r = 0;
+	uint8_t *dr = NULL;
 	char fpath[MAXPATHLEN];
 	strcpy(fpath, STORE_PREFIX);
 	strcat(fpath, mname);
-	FILE *fp = fopen(strcat(fpath, fname), "r");
+	FILE *fp = fopen(strcat(fpath, fname), "rb");
 	if (!fp) {
 		fprintf(stderr, "Couldn't open %s for reading.\n", fpath);
 		return OQS_ERROR;
 	}
-	*rcvd = fread(data, 1, len, fp);
-	if (*rcvd < 1) {
+	do { // assume some OSs don't deliver all data in one go...
+		dr = (uint8_t *)(data + len_read);
+		r = fread(dr, 1, len - len_read, fp);
+		len_read += r;
+	} while (r > 0);
+	*rcvd = len_read;
+	if (len_read <= 0) {
 		fprintf(stderr, "Error reading data (operations called in proper sequence?). Expecting %zu. Exiting.\n", len);
 		return OQS_ERROR;
 	}
