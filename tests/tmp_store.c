3ca64c35aeeabaaf1c32f4638bed66107dd3ba5b
Michael Baentsch <57787676+baentsch@users.noreply.github.com>
2020-12-02 20:45:30 +0000

    enable Valgrind memory use testing (#854)

---
+++ tests/tmp_store.c
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define STORE_PREFIX "tmp/oqs-temp-file-"
+#define MAXPATHLEN 128
+
+static OQS_STATUS oqs_fstore(const char *fname, const char *mname, uint8_t *data, size_t len) {
+	char fpath[MAXPATHLEN];
+	strcpy(fpath, STORE_PREFIX);
+	strcat(fpath, mname);
+	FILE *fp = fopen(strcat(fpath, fname), "wb");
+	if (!fp) {
+		fprintf(stderr, "Couldn't open %s for writing.\n", fpath);
+		return OQS_ERROR;
+	}
+	fwrite(data, len, 1, fp);
+	fclose(fp);
+	return OQS_SUCCESS;
+}
+
+static OQS_STATUS oqs_fload(const char *fname, const char *mname, uint8_t *data, size_t len, size_t *rcvd) {
+	size_t len_read = 0, r = 0;
+	uint8_t *dr = NULL;
+	char fpath[MAXPATHLEN];
+	strcpy(fpath, STORE_PREFIX);
+	strcat(fpath, mname);
+	FILE *fp = fopen(strcat(fpath, fname), "rb");
+	if (!fp) {
+		fprintf(stderr, "Couldn't open %s for reading.\n", fpath);
+		return OQS_ERROR;
+	}
+	do { // assume some OSs don't deliver all data in one go...
+		dr = (uint8_t *)(data + len_read);
+		r = fread(dr, 1, len - len_read, fp);
+		len_read += r;
+	} while (r > 0);
+	*rcvd = len_read;
+	if (len_read <= 0) {
+		fprintf(stderr, "Error reading data (operations called in proper sequence?). Expecting %zu. Exiting.\n", len);
+		return OQS_ERROR;
+	}
+	fclose(fp);
+	return OQS_SUCCESS;
+}
+
