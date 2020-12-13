aca336ed2373ba22512d19dce8025b166d14b6e3
Michael <57787676+baentsch@users.noreply.github.com>
2020-12-13 08:35:05 +0100

    added portability tests

---
+++ tests/test_portability.py
@@ -0,0 +1,39 @@
+# SPDX-License-Identifier: MIT
+
+import helpers
+import pytest
+import platform
+from pathlib import Path
+
+@helpers.filtered_test
+@pytest.mark.parametrize('kem_name', helpers.available_kems_by_name())
+def test_kem(kem_name):
+    if not(helpers.is_build_portable()):
+        pytest.skip("Portability not enabled")
+
+    if not(helpers.is_kem_enabled_by_name(kem_name)):
+        pytest.skip('Not enabled')
+
+    Path('build/mem-benchmark').mkdir(parents=True, exist_ok=True)
+
+    for i in range(3):
+       helpers.run_subprocess(["qemu-"+platform.machine(), helpers.path_to_executable('test_kem_mem'), kem_name, str(i)])
+
+@helpers.filtered_test
+@pytest.mark.parametrize('sig_name', helpers.available_sigs_by_name())
+def test_sig(sig_name):
+    if not(helpers.is_build_portable()):
+        pytest.skip("Portability not enabled")
+
+    if not(helpers.is_sig_enabled_by_name(sig_name)):
+        pytest.skip('Not enabled')
+
+    Path('build/mem-benchmark').mkdir(parents=True, exist_ok=True)
+
+    for i in range(3):
+       helpers.run_subprocess(["qemu-"+platform.machine(), helpers.path_to_executable('test_sig_mem'), sig_name, str(i)])
+
+if __name__ == "__main__":
+    import sys
+    pytest.main(sys.argv)
+
