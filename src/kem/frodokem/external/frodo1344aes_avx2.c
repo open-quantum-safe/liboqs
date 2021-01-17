c77d688e886451145fe8098ba3757ca832d70c5c
Douglas Stebila <dstebila@uwaterloo.ca>
2021-01-17 15:12:34 -0500

    Move as+e so compiler can apply AVX2 optimizations
    There isn't an explicit separate AVX2 implementation since Patrick 
    observed that the compiler can optimize this code well enough without 
    hand-written usage of intrinsics.  This commit reorganizes the code so 
    that the compiler can separately compile a version with -mavx2 options 
    provided.

--- src/kem/frodokem/external/frodo1344aes_avx2.c
+++ src/kem/frodokem/external/frodo1344aes_avx2.c
@@ -7,3 +7,6 @@
 #include "frodo1344aes_params.h"
 
 #include "frodo_macrify_aes_avx2.c"
+
+#define frodo_mul_add_as_plus_e_actual frodo_mul_add_as_plus_e_avx2
+#include "frodo_macrify_as_plus_e.c"
--- src/kem/frodokem/external/frodo1344shake_avx2.c
+++ src/kem/frodokem/external/frodo1344shake_avx2.c
@@ -7,3 +7,6 @@
 #include "frodo1344shake_params.h"
 
 #include "frodo_macrify_shake_avx2.c"
+
+#define frodo_mul_add_as_plus_e_actual frodo_mul_add_as_plus_e_avx2
+#include "frodo_macrify_as_plus_e.c"
