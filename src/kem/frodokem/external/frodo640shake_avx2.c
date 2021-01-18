fb5484cda68dac115dc563593c68388f89444b95
Douglas Stebila <dstebila@users.noreply.github.com>
2021-01-18 10:13:11 -0500

    FrodoKEM portable build with CPU feature detection (#878)
    * Make `OQS_SHA3_shake128_4x` always available
    
    * Simplify FrodoKEM SHA3
    
    * Fix prettyprint
    
    * Make SHAKE128 x4 always available and use CPU feature detection
    
    Move contents of sha3x4.c into sha3x4_avx2.c
    
    * CPU feature detection for FrodoKEM sa+e operation
    
    Separate out the 4 versions (C, AVX2) x (AES, SHAKE) and then select 
    appropriate one
    
    * Missing braces for single-line for statement
    
    * Reorganize FrodoKEM AVX2 code into separate compilation units
    
    * Move as+e so compiler can apply AVX2 optimizations
    
    There isn't an explicit separate AVX2 implementation since Patrick 
    observed that the compiler can optimize this code well enough without 
    hand-written usage of intrinsics.  This commit reorganizes the code so 
    that the compiler can separately compile a version with -mavx2 options 
    provided.

---
+++ src/kem/frodokem/external/frodo640shake_avx2.c
@@ -0,0 +1,12 @@
+#include <stddef.h>
+#include <stdint.h>
+
+#include <oqs/kem_frodokem.h>
+
+#include "frodo_internal.h"
+#include "frodo640shake_params.h"
+
+#include "frodo_macrify_shake_avx2.c"
+
+#define frodo_mul_add_as_plus_e_actual frodo_mul_add_as_plus_e_avx2
+#include "frodo_macrify_as_plus_e.c"
