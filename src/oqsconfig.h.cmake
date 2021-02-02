4c53588948c3a53a0d4717a1e8ba7034576e7c26
John M. Schanck <jschanck@uwaterloo.ca>
2021-02-02 11:31:06 -0500

    Add OQS_ENABLE_TEST_CONSTANT_TIME to oqsconfig.h

--- src/oqsconfig.h.cmake
+++ src/oqsconfig.h.cmake
@@ -28,6 +28,8 @@
 #cmakedefine OQS_USE_SSE2_INSTRUCTIONS 1
 #cmakedefine OQS_USE_SSE3_INSTRUCTIONS 1
 
+#cmakedefine OQS_ENABLE_TEST_CONSTANT_TIME 1
+
 #cmakedefine OQS_ENABLE_KEM_BIKE 1
 #cmakedefine OQS_ENABLE_KEM_bike1_l1_cpa 1
 #cmakedefine OQS_ENABLE_KEM_bike1_l3_cpa 1
