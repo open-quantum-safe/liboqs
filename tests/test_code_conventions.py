f45991307ddb8209b074e47842ce2dddb3ac0aa6
John M. Schanck <jschanck@uwaterloo.ca>
2021-02-11 12:58:57 -0500

    test_free: replace find|grep|xargs with python solution

--- tests/test_code_conventions.py
+++ tests/test_code_conventions.py
@@ -1,7 +1,9 @@
 # SPDX-License-Identifier: MIT
 
 import helpers
+import os
 import pytest
+import re
 import sys
 
 # Ensure every key-exchange algorithm in the code
@@ -50,16 +52,21 @@ def test_spdx():
 @helpers.filtered_test
 @pytest.mark.skipif(sys.platform.startswith("win"), reason="Not needed on Windows")
 def test_free():
-    files = helpers.run_subprocess(['find', 'src', '-name', '*.c'])
-    files = helpers.run_subprocess(['grep', '-v', 'picnic/external'], input=files.encode('utf8'))
-    lines = helpers.run_subprocess(['xargs', 'grep', '[^_]free('], input=files.encode('utf8'))
-    lines = lines.split("\n")
+    c_files = []
+    for path, _, files in os.walk('src'):
+        if os.path.join('picnic','external') in path: continue
+        c_files += [os.path.join(path,f) for f in files if f[-2:] == '.c']
     okay = True
-    for line in lines:
-        if line == "": continue
-        if not('IGNORE free-check' in line):
-            okay = False
-            print("Suspicious `free` in " + line)
+    for fn in c_files:
+        with open(fn) as f:
+            # Find all lines that contain 'free(' but not '_free('
+            for no, line in enumerate(f,1):
+                if not re.match(r'^.*[^_]free\(.*$', line):
+                    continue
+                if 'IGNORE free-check' in line:
+                    continue
+                okay = False
+                print("Suspicious `free` in {}:{}:{}".format(fn,no,line))
     assert okay, "'free' is used in some files.  These should be changed to 'OQS_MEM_secure_free' or 'OQS_MEM_insecure_free' as appropriate. If you are sure you want to use 'free' in a particular spot, add the comment '// IGNORE free-check' on the line where 'free' occurs."
 
 if __name__ == "__main__":
