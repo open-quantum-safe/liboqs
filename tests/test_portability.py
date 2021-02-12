95c9c5bdf8b0e3e32a3c8dad5538f5ed27026708
Michael Baentsch <info@baentsch.ch>
2021-02-12 10:25:32 +0100

    Changing python platform test (#902)
    Fixes #901

--- tests/test_portability.py
+++ tests/test_portability.py
@@ -9,9 +9,20 @@ MIN_CPUS = {}
 # set other CPU types for other architectures; Westmere supports cpuid but not avx2
 MIN_CPUS["x86_64"] = "Westmere"
 
+def check_ubuntu():
+   try:
+      with open("/etc/os-release") as f:
+         for line in f.readlines():
+            if "Ubuntu" in line:
+               return True
+   # if file /etc/os-release doesn't exist, we're definitely not on Ubuntu:
+   except:
+      return False
+   return False
+
 @helpers.filtered_test
 @pytest.mark.parametrize('kem_name', helpers.available_kems_by_name())
-@pytest.mark.skipif(not "Ubuntu" in platform.platform(), reason="Only supported on Ubuntu")
+@pytest.mark.skipif(not check_ubuntu(), reason="Only supported on Ubuntu")
 def test_kem(kem_name):
     if not(helpers.is_build_portable()):
         pytest.skip("Portability not enabled")
@@ -19,12 +30,12 @@ def test_kem(kem_name):
     if not(helpers.is_kem_enabled_by_name(kem_name)):
         pytest.skip('Not enabled')
 
-    helpers.run_subprocess(["qemu-"+platform.machine(), "-cpu", MIN_CPUS[platform.machine()],
+    helpers.run_subprocess(["qemu-"+platform.machine()+"-static", "-cpu", MIN_CPUS[platform.machine()],
                             helpers.path_to_executable('test_kem'), kem_name])
 
 @helpers.filtered_test
 @pytest.mark.parametrize('sig_name', helpers.available_sigs_by_name())
-@pytest.mark.skipif(not "Ubuntu" in platform.platform(), reason="Only supported on Ubuntu")
+@pytest.mark.skipif(not check_ubuntu(), reason="Only supported on Ubuntu")
 def test_sig(sig_name):
     if not(helpers.is_build_portable()):
         pytest.skip("Portability not enabled")
@@ -35,7 +46,7 @@ def test_sig(sig_name):
     if (sig_name.startswith("picnic")):
         pytest.skip("Picnic portability known to not be given.")
 
-    helpers.run_subprocess(["qemu-"+platform.machine(), "-cpu", MIN_CPUS[platform.machine()],
+    helpers.run_subprocess(["qemu-"+platform.machine()+"-static", "-cpu", MIN_CPUS[platform.machine()],
                              helpers.path_to_executable('test_sig'), sig_name])
 
 if __name__ == "__main__":
