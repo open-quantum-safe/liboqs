211aa22f9414b298da69385d905657a2b406afd1
John Schanck <jschanck@uwaterloo.ca>
2021-04-06 08:42:20 -0400

    Fix shake context leak in Dilithium (#960)
    * Reduce concurrency in CircleCI AddressSanitizer builds
    
    * Fix shake context leak in Dilithium
    
    Co-authored-by: Douglas Stebila <dstebila@uwaterloo.ca>

--- src/sig/dilithium/pqcrystals-dilithium_dilithium3_avx2/symmetric.h
+++ src/sig/dilithium/pqcrystals-dilithium_dilithium3_avx2/symmetric.h
@@ -17,8 +17,10 @@ typedef aes256ctr_ctx stream256_state;
 
 #define stream128_init(STATE, SEED, NONCE) aes256ctr_init(STATE, SEED, NONCE)
 #define stream128_squeezeblocks(OUT, OUTBLOCKS, STATE) aes256ctr_squeezeblocks(OUT, OUTBLOCKS, STATE)
+#define stream128_release(STATE)
 #define stream256_init(STATE, SEED, NONCE) aes256ctr_init(STATE, SEED, NONCE)
 #define stream256_squeezeblocks(OUT, OUTBLOCKS, STATE) aes256ctr_squeezeblocks(OUT, OUTBLOCKS, STATE)
+#define stream256_release(STATE)
 
 #else
 
@@ -38,8 +40,10 @@ void dilithium_shake256_stream_init(shake256incctx *state, const uint8_t seed[CR
 
 #define stream128_init(STATE, SEED, NONCE) dilithium_shake128_stream_init(STATE, SEED, NONCE)
 #define stream128_squeezeblocks(OUT, OUTBLOCKS, STATE) shake128_squeezeblocks(OUT, OUTBLOCKS, STATE)
+#define stream128_release(STATE) shake128_inc_ctx_release(STATE)
 #define stream256_init(STATE, SEED, NONCE) dilithium_shake256_stream_init(STATE, SEED, NONCE)
 #define stream256_squeezeblocks(OUT, OUTBLOCKS, STATE) shake256_squeezeblocks(OUT, OUTBLOCKS, STATE)
+#define stream256_release(STATE) shake256_inc_ctx_release(STATE)
 
 #endif
 
