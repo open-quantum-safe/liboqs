7e9921ffd4f89d927658ceae324f1691e3b08364
Michael <57787676+baentsch@users.noreply.github.com>
2021-05-15 16:52:57 +0200

    another test

--- appveyor_build.bat
+++ appveyor_build.bat
@@ -2,7 +2,7 @@
 IF %COMPILER%==cygwin (
     @echo on
     SET "PATH=C:\cywin64\bin;c:\cygwin64;%PATH%"
-    c:\cygwin64\bin\bash.exe -lc "echo $PATH && setup-x86_64.exe -qnNdO -R C:/cygwin64 -s http://cygwin.mirror.constant.com -l C:/cygwin/var/cache/setup -P openssl-devel -P ninja -P cmake -P gcc && cd ${APPVEYOR_BUILD_FOLDER} && pwd && mkdir build && cd build && whoami && touch CMakeDoxyfile.tpl && ls -la && cmake .. -GNinja -DCMAKE_C_COMPILER=gcc -DOQS_DIST_BUILD=ON -DOQS_ENABLE_SIG_SPHINCS=OFF -DOQS_ENABLE_SIG_RAINBOW=OFF -DBUILD_SHARED_LIBS=%BUILD_SHARED% -DOQS_USE_OPENSSL=%OQS_USE_OPENSSL% && ninja && ls -laR"
+    c:\cygwin64\bin\bash.exe -lc "echo $PATH && setup-x86_64.exe -qnNdO -R C:/cygwin64 -s http://cygwin.mirror.constant.com -l C:/cygwin/var/cache/setup -P openssl-devel -P ninja -P cmake -P gcc && cd ${APPVEYOR_BUILD_FOLDER} && pwd && mkdir build && cd build && whoami && touch CMakeDoxyfile.tpl && ls -la && cmake .. -GNinja -DCMAKE_C_COMPILER=gcc -DOQS_DIST_BUILD=ON -DOQS_ENABLE_SIG_SPHINCS=OFF -DOQS_ENABLE_SIG_RAINBOW=OFF -DBUILD_SHARED_LIBS=%BUILD_SHARED% -DOQS_USE_OPENSSL=%OQS_USE_OPENSSL% && ninja && ls -laR && ./tests/example_kem.exe && ./tests/example_sig.exe"
 )
 IF %COMPILER%==msys2 (
     @echo on
