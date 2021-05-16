0e137546ceafc53353d30c3f24dea7c9a03652bc
Michael <57787676+baentsch@users.noreply.github.com>
2021-05-16 06:57:45 +0200

    setting cygwin path

--- appveyor_build.bat
+++ appveyor_build.bat
@@ -2,9 +2,12 @@
 IF %COMPILER%==cygwin (
     @echo on
     SET "PATH=C:\cywin64\bin;c:\cygwin64;%PATH%"
-    c:\cygwin64\bin\bash.exe -lc "echo $PATH && setup-x86_64.exe -qnNdO -R C:/cygwin64 -s http://cygwin.mirror.constant.com -l C:/cygwin/var/cache/setup -P openssl-devel -P ninja -P cmake -P gcc && cd ${APPVEYOR_BUILD_FOLDER} && pwd && mkdir build && cd build && whoami && touch CMakeDoxyfile.tpl && ls -la && cmake .. -GNinja -DCMAKE_C_COMPILER=gcc -DOQS_BUILD_MINIMAL=ON -DOQS_DIST_BUILD=ON -DOQS_ENABLE_SIG_SPHINCS=OFF -DOQS_ENABLE_SIG_RAINBOW=OFF -DBUILD_SHARED_LIBS=%BUILD_SHARED% -DOQS_USE_OPENSSL=%OQS_USE_OPENSSL% && ninja && ls -laR && ./tests/example_kem.exe && ./tests/example_sig.exe"
+    c:\cygwin64\bin\bash.exe -lc "echo $PATH && setup-x86_64.exe -qnNdO -R C:/cygwin64 -s http://cygwin.mirror.constant.com -l C:/cygwin/var/cache/setup -P openssl-devel -P ninja -P cmake -P gcc && cd ${APPVEYOR_BUILD_FOLDER} && pwd && mkdir build && cd build && whoami && touch CMakeDoxyfile.tpl && ls -la && cmake .. -GNinja -DCMAKE_C_COMPILER=gcc -DOQS_MINIMAL_BUILD=ON -DOQS_DIST_BUILD=ON -DOQS_ENABLE_SIG_SPHINCS=OFF -DOQS_ENABLE_SIG_RAINBOW=OFF -DBUILD_SHARED_LIBS=%BUILD_SHARED% -DOQS_USE_OPENSSL=%OQS_USE_OPENSSL% && ninja && ls -laR && ./tests/example_kem.exe && ./tests/example_sig.exe"
     echo "ABF: %APPVEYOR_BUILD_FOLDER%"
+    echo "%PATH%"
     %APPVEYOR_BUILD_FOLDER%\build\tests\example_kem.exe
+    "%APPVEYOR_BUILD_FOLDER%\build\tests\example_kem.exe"
+    echo "done"
 )
 IF %COMPILER%==msys2 (
     @echo on
