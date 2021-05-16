71803ffb10358a26aaf549e8d4c51eb0cf169aa3
Michael <57787676+baentsch@users.noreply.github.com>
2021-05-16 12:01:20 +0200

    unset OPENSSL_ROOT_DIR in bash

--- appveyor_build.bat
+++ appveyor_build.bat
@@ -5,7 +5,7 @@ IF %COMPILER%==cygwin (
     set "OPENSSL_ROOT_DIR="
     echo "OSSL: %OPENSSL_ROOT_DIR%"
     SET "PATH=C:\cywin64\bin;c:\cygwin64;%PATH%"
-    c:\cygwin64\bin\bash.exe -lc "echo $PATH && setup-x86_64.exe -qnNdO -R C:/cygwin64 -l C:/cygwin/var/cache/setup -P openssl-devel -P openssl -P ninja -P cmake -P gcc && cd ${APPVEYOR_BUILD_FOLDER} && openssl version && cygcheck -c && cygcheck -l openssl && cygcheck -l openssl-devel && find /usr -name opensslv.h -print && pwd && mkdir build && cd build && whoami && touch CMakeDoxyfile.tpl && ls -la && cmake .. -GNinja -DCMAKE_C_COMPILER=gcc -DOQS_MINIMAL_BUILD=ON -DOQS_DIST_BUILD=ON -DOQS_ENABLE_SIG_SPHINCS=OFF -DOQS_ENABLE_SIG_RAINBOW=OFF -DBUILD_SHARED_LIBS=%BUILD_SHARED% -DOQS_USE_OPENSSL=%OQS_USE_OPENSSL% && ninja "
+    c:\cygwin64\bin\bash.exe -lc "echo $OPENSSL_ROOT_DIR && unset OPENSSL_ROOT_DIR && echo $PATH && setup-x86_64.exe -qnNdO -R C:/cygwin64 -l C:/cygwin/var/cache/setup -P openssl-devel -P openssl -P ninja -P cmake -P gcc && cd ${APPVEYOR_BUILD_FOLDER} && openssl version && cygcheck -c && cygcheck -l openssl && cygcheck -l openssl-devel && find /usr -name opensslv.h -print && pwd && mkdir build && cd build && whoami && touch CMakeDoxyfile.tpl && ls -la && cmake .. -GNinja -DCMAKE_C_COMPILER=gcc -DOQS_MINIMAL_BUILD=ON -DOQS_DIST_BUILD=ON -DOQS_ENABLE_SIG_SPHINCS=OFF -DOQS_ENABLE_SIG_RAINBOW=OFF -DBUILD_SHARED_LIBS=%BUILD_SHARED% -DOQS_USE_OPENSSL=%OQS_USE_OPENSSL% && ninja "
 )
 IF %COMPILER%==msys2 (
     @echo on
