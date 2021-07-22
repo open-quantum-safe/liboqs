8b798a5e0c92df7e6968a78b535940417ad6ded0
xvzcf <xvzcf@users.noreply.github.com>
2021-07-22 18:12:27 -0400

    Updated signature YAML docsheets.

--- scripts/copy_from_upstream/update_pqclean_alg_docs.py
+++ scripts/copy_from_upstream/update_pqclean_alg_docs.py
@@ -37,9 +37,10 @@ def store_yaml(filename, contents, encoding='utf-8'):
         yaml.dump(contents, fh, sort_keys=False, allow_unicode=True)
 
 def fetch_upstream(upstream_info):
-    os.makedirs('repos', exist_ok=True)
+    work_dir_root = os.path.join(args.liboqs_root, 'scripts', 'copy_from_upstream', 'repos')
+    os.makedirs(work_dir_root, exist_ok=True)
 
-    work_dir = os.path.join('repos', upstream_info['name'])
+    work_dir = os.path.join(work_dir_root, upstream_info['name'])
     work_dotgit = os.path.join(work_dir, '.git')
     if not os.path.exists(work_dotgit):
         shell(['git', 'init', work_dir])
@@ -117,6 +118,64 @@ def update_pqclean_kem_alg_docs(kems, upstream_info, pqclean_root, meta_yaml_pat
             if args.write_changes:
                 store_yaml(oqs_yaml_path, oqs_yaml)
 
+def update_pqclean_sig_alg_docs(sigs, upstream_info, pqclean_root, meta_yaml_path_template):
+    for sig in sigs:
+        if sig['upstream_location'] == 'pqclean':
+            oqs_yaml_path = os.path.join(args.liboqs_root, 'docs', 'algorithms', 'sig', '{}.yml'.format(sig['name']))
+            if os.path.isfile(oqs_yaml_path):
+               oqs_yaml = load_yaml(oqs_yaml_path)
+            else:
+                continue
+
+            # We assume that the ordering of "parameter-sets"
+            # in the OQS YAML files matches that of copy_from_upstream.yml
+            for index, scheme in enumerate(sig['schemes']):
+                if DEBUG > 0:
+                    print("Examining {}'s META.yml.".format(scheme['pretty_name_full']))
+                pqclean_meta_path = os.path.join(pqclean_root, meta_yaml_path_template.format_map(scheme))
+                pqclean_yaml = load_yaml(pqclean_meta_path)
+
+                oqs_yaml['type'] = rhs_if_not_equal(oqs_yaml['type'], pqclean_yaml['type'], "type")
+                oqs_yaml['principal-submitters'] = rhs_if_not_equal(oqs_yaml['principal-submitters'], pqclean_yaml['principal-submitters'], "principal-submitters")
+
+                upstream_base_url = upstream_info['git_url'][:-len(".git")]
+                oqs_yaml['upstream'] = rhs_if_not_equal(oqs_yaml['upstream'], "{}/commit/{}".format(upstream_base_url, upstream_info['git_commit']), "upstream")
+
+                if 'auxiliary-submitters' in pqclean_yaml:
+                        oqs_yaml['auxiliary-submitters'] = rhs_if_not_equal(oqs_yaml['auxiliary-submitters'] if 'auxiliary-submitters' in oqs_yaml else '', pqclean_yaml['auxiliary-submitters'], "auxiliary-submitters")
+
+                oqs_scheme_yaml = oqs_yaml['parameter-sets'][index]
+
+                # TODO: PQClean and liboqs pretty-naming conventions for the
+                # following algorithms are out of sync.
+                if sig['name'] == 'sphincs' or sig['name'] == 'rainbow':
+                    oqs_scheme_yaml['name'] = rhs_if_not_equal(oqs_scheme_yaml['name'], scheme['pretty_name_full'], "scheme pretty name")
+                else:
+                    oqs_scheme_yaml['name'] = rhs_if_not_equal(oqs_scheme_yaml['name'], pqclean_yaml['name'], "scheme pretty name")
+
+                oqs_scheme_yaml['claimed-nist-level'] = rhs_if_not_equal(oqs_scheme_yaml['claimed-nist-level'], pqclean_yaml['claimed-nist-level'], "claimed-nist-level")
+                oqs_scheme_yaml['claimed-security'] = rhs_if_not_equal(oqs_scheme_yaml['claimed-security'], 'EUF-CMA', "claimed-security")
+                oqs_scheme_yaml['length-public-key'] = rhs_if_not_equal(oqs_scheme_yaml['length-public-key'], pqclean_yaml['length-public-key'], "length-public-key")
+                oqs_scheme_yaml['length-secret-key'] = rhs_if_not_equal(oqs_scheme_yaml['length-secret-key'], pqclean_yaml['length-secret-key'], "legnth-secret-key")
+                oqs_scheme_yaml['length-signature'] = rhs_if_not_equal(oqs_scheme_yaml['length-signature'], pqclean_yaml['length-signature'], "length-signature")
+
+                for impl_index, impl in enumerate(oqs_scheme_yaml['implementations']):
+                    for pqclean_impl in pqclean_yaml['implementations']:
+                        if impl['upstream-id'] == pqclean_impl['name']:
+                            break
+
+                    if 'supported_platforms' in pqclean_impl:
+                        impl['supported-platforms'] = rhs_if_not_equal(impl['supported-platforms'], pqclean_impl['supported_platforms'], "supported-platforms")
+                    else:
+                        impl['supported-platforms'] = rhs_if_not_equal(impl['supported-platforms'], "all", "supported-platforms")
+
+                    oqs_scheme_yaml['implementations'][impl_index] = impl
+
+                oqs_yaml['parameter-sets'][index] = oqs_scheme_yaml
+
+            if args.write_changes:
+                store_yaml(oqs_yaml_path, oqs_yaml)
+
 instructions = load_yaml(
     os.path.join(args.liboqs_root, 'scripts', 'copy_from_upstream', 'copy_from_upstream.yml'),
     encoding='utf-8')
@@ -128,3 +187,4 @@ for upstream in instructions['upstreams']:
 pqclean_root = fetch_upstream(pqclean_info)
 
 update_pqclean_kem_alg_docs(instructions['kems'], pqclean_info, pqclean_root, pqclean_info['kem_meta_path'])
+update_pqclean_sig_alg_docs(instructions['sigs'], pqclean_info, pqclean_root, pqclean_info['sig_meta_path'])
