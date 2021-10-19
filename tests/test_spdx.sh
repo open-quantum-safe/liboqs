2c0fd7854e29b376511e11de1c27eda399665574
Jason Goertzen <Martyrshot@gmail.com>
2021-10-19 18:43:25 -0400

    No longer checking spdx on files copied from upstream

--- tests/test_spdx.sh
+++ tests/test_spdx.sh
@@ -2,4 +2,4 @@
 # SPDX-License-Identifier: MIT
 
 BD=${OQS_BUILD_DIR:-build}
-find . \( -name '*.[chsS]' -or -name '*.cmake' -or -name '*.py' -or -name '*.sh' -or -name 'CMakeLists.txt' \) -and -type f | grep -v '/external/' | grep -v 'kem/.*/.*/.*' | grep -v 'sig/.*/.*/.*' | grep -v 'xkcp_low' | grep -v 'patches' | grep -v `basename $BD` | xargs grep -L 'SPDX-License-Identifier' | sort
+find . \( -name '*.[chsS]' -or -name '*.cmake' -or -name '*.py' -or -name '*.sh' -or -name 'CMakeLists.txt' \) -and -type f | grep -v '/external/' | grep -v 'kem/.*/.*/.*' | grep -v 'sig/.*/.*/.*' | grep -v 'xkcp_low' | grep -v 'patches' | grep -v 'scripts/copy_from_upstream/repos' | grep -v `basename $BD` | xargs grep -L 'SPDX-License-Identifier' | sort
