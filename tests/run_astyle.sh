fc3ed12e8e2c0af1373908939460ae846fe86e67
Michael <57787676+baentsch@users.noreply.github.com>
2021-11-23 16:15:03 +0100

    doing all style checks in one run

--- tests/run_astyle.sh
+++ tests/run_astyle.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 # SPDX-License-Identifier: MIT
 
+rv=0
+
 # check style of non-external code:
 find src tests -name '*.[ch]' | grep -v '/external/' | grep -v 'kem/.*/.*/.*' | grep -v 'sig/.*/.*/.*' | xargs astyle --dry-run --options=.astylerc | grep Format
 if [ $? -ne 1 ]; then
-   echo "Error: Some files need reformatting. Check output above. Exiting."
-   exit -1
+   echo "Error: Some files need reformatting. Check output above."
+   rv=-1
 fi
 
 # check _all_ source files for CRLF line endings:
 find . -name '*.[chS]' -exec file "{}" ";" | grep CRLF
 if [ $? -ne 1 ]; then
-   echo "Error: Files found with non-UNIX line endings. Exiting."
+   echo "Error: Files found with non-UNIX line endings."
    echo "To fix, consider running \"find src tests -name '*.[chS]' | xargs sed -i 's/\r//' \"."
-   exit -1
+   rv=-1
 fi
+
+exit $rv
