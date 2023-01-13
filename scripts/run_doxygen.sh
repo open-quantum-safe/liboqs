aed3b4965f9c30d0ba6a3269dc86b2fcd4c8ec57
Douglas Stebila <dstebila@users.noreply.github.com>
2023-01-13 16:58:03 -0500

    Build Doxygen docs in whatever the CMake build directory is (#1357)
    * Build Doxygen docs in whatever the CMake build directory is
    
    Fixes #1341.
    
    * Missing Doxygen build directory in CI

--- scripts/run_doxygen.sh
+++ scripts/run_doxygen.sh
@@ -2,8 +2,8 @@
 
 # SPDX-License-Identifier: MIT
 
-if [ "$#" -ne 2 ]; then
-   echo "Wrong number of arguments: Expecting path to doxygen binary and doxygen file. Exiting." 
+if [ "$#" -ne 3 ]; then
+   echo "Wrong number of arguments: Expecting path to doxygen binary, path to doxygen file, and destination directory. Exiting." 
    exit 1
 fi
 
@@ -14,7 +14,8 @@ cp CONFIGURE.md CONFIGURE.md-orig
 python3 scripts/doxyfy.py CONFIGURE.md-orig CONFIGURE.md
 
 # run doxygen:
-$1 $2
+mkdir -p "$3/docs"
+env DOXYGEN_DESTIONATION_DIR="$3/docs" "$1" "$2"
 EXITCODE=$?
 
 # undo the Github Markdown reformatting
