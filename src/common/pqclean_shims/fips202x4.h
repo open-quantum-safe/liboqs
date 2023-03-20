fb26fb44493af23ab7b7b70a5425e9c4f6fb34cd
Michael Baentsch <57787676+baentsch@users.noreply.github.com>
2023-03-20 13:24:47 +0100

    patching SPHINCS again

--- src/common/pqclean_shims/fips202x4.h
+++ src/common/pqclean_shims/fips202x4.h
@@ -5,6 +5,13 @@
 
 #include <oqs/sha3x4.h>
 
+/* Additions "just" for SPHINCS: */
+#include <immintrin.h>
+/* Provide a reference to the common AVX2 KeccakP1600times4_PermuteAll_24rounds */
+#define KECCAKTIMES4_IMPL_NAMESPACE(x) x##_avx2
+#define KeccakP1600times4_PermuteAll_24rounds KECCAKTIMES4_IMPL_NAMESPACE(KeccakP1600times4_PermuteAll_24rounds)
+
+#define shake256x4 OQS_SHA3_shake256_x4
 #define shake128x4incctx OQS_SHA3_shake128_x4_inc_ctx
 #define shake128x4_inc_init OQS_SHA3_shake128_x4_inc_init
 #define shake128x4_inc_absorb OQS_SHA3_shake128_x4_inc_absorb
@@ -22,19 +29,6 @@
 #define shake256x4_inc_ctx_release OQS_SHA3_shake256_x4_inc_ctx_release
 #define shake256x4_inc_ctx_clone OQS_SHA3_shake256_x4_inc_ctx_clone
 #define shake256x4_inc_ctx_reset OQS_SHA3_shake256_x4_inc_ctx_reset
-
-#define PQCLEAN_SPHINCSSHAKE256128FROBUST_AVX2_shake256x4 OQS_SHA3_shake256_x4
-#define PQCLEAN_SPHINCSSHAKE256128FSIMPLE_AVX2_shake256x4 OQS_SHA3_shake256_x4
-#define PQCLEAN_SPHINCSSHAKE256128SROBUST_AVX2_shake256x4 OQS_SHA3_shake256_x4
-#define PQCLEAN_SPHINCSSHAKE256128SSIMPLE_AVX2_shake256x4 OQS_SHA3_shake256_x4
-#define PQCLEAN_SPHINCSSHAKE256192FROBUST_AVX2_shake256x4 OQS_SHA3_shake256_x4
-#define PQCLEAN_SPHINCSSHAKE256192FSIMPLE_AVX2_shake256x4 OQS_SHA3_shake256_x4
-#define PQCLEAN_SPHINCSSHAKE256192SROBUST_AVX2_shake256x4 OQS_SHA3_shake256_x4
-#define PQCLEAN_SPHINCSSHAKE256192SSIMPLE_AVX2_shake256x4 OQS_SHA3_shake256_x4
-#define PQCLEAN_SPHINCSSHAKE256256FROBUST_AVX2_shake256x4 OQS_SHA3_shake256_x4
-#define PQCLEAN_SPHINCSSHAKE256256FSIMPLE_AVX2_shake256x4 OQS_SHA3_shake256_x4
-#define PQCLEAN_SPHINCSSHAKE256256SROBUST_AVX2_shake256x4 OQS_SHA3_shake256_x4
-#define PQCLEAN_SPHINCSSHAKE256256SSIMPLE_AVX2_shake256x4 OQS_SHA3_shake256_x4
 
 #define shake128x4_absorb_once OQS_SHA3_shake128_x4_absorb_once
 void OQS_SHA3_shake128_x4_absorb_once(shake128x4incctx *state, const uint8_t *in0, const uint8_t *in1, const uint8_t *in2, const uint8_t *in3, size_t inlen);
