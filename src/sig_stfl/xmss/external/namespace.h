2a890a6be53b1bae464e469b881b125c56a29291
Duc Nguyen <106774416+ducnguyen-sb@users.noreply.github.com>
2023-07-23 22:10:34 -0400

    Add 12 XMSS and 16 XMSSMT parameters. (#1489)
    * populate all 28 XMSS parameters
    
    * clean up
    
    * remove wanrings in scanbuild
    
    * change free to OQS_MEM_insecure_free
    
    * fix build warning
    
    * fix integer in i386 platforms
    
    * proper type for sigs_remain and sig_maximum
    
    * remove size_t in signature remain and total
    
    * make scan-build happy

--- src/sig_stfl/xmss/external/namespace.h
+++ src/sig_stfl/xmss/external/namespace.h
@@ -1,14 +1,18 @@
 #ifndef XMSS_NAMESPACE_H
 #define XMSS_NAMESPACE_H
 
+#define XMSS__(prefix, funcname) prefix##_##funcname
+#define XMSS_(prefix, funcname) XMSS__(prefix, funcname)
+#define __XMSS(funcname, postfix) funcname##_##postfix
+#define _XMSS(funcname, postfix) __XMSS(funcname, postfix)
 
-#define XMSS_PARAMS oqs_sig_stfl_xmss
-#define XMSS_PARAMS_INNER oqs_sig_stfl_xmss_inner
+#define XMSS_PARAMS _XMSS(oqs_sig_stfl, XMSS_PARAMS_NAMESPACE)
+#define XMSS_PARAMS_INNER _XMSS(_XMSS(oqs_sig_stfl, XMSS_PARAMS_NAMESPACE), inner)
+#define XMSS_PARAMS_INNER_CORE _XMSS(_XMSS(oqs_sig_stfl, XMSS_PARAMS_NAMESPACE), inner)
+
+#define XMSS_PARAMS_INNER_CORE_HASH(funcname) XMSS_(XMSS_PARAMS_INNER_CORE, funcname)
 
 #define XMSS_NAMESPACE(funcname) XMSS_(XMSS_PARAMS, funcname)
 #define XMSS_INNER_NAMESPACE(funcname) XMSS_(XMSS_PARAMS_INNER, funcname)
-#define XMSS_(prefix, funcname) XMSS__(prefix, funcname)
-#define XMSS__(prefix, funcname) prefix ## _ ## funcname
-
 
 #endif
