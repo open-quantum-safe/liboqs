8fd667d05fd466ce00b9ab779ef831923656431f
Spencer Wilson <spencer.wilson@uwaterloo.ca>
2023-09-12 11:28:15 -0400

    Clean things up to run copy_from_upstream fresh

--- src/sig/falcon/pqclean_falcon-1024_aarch64/util.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTIL_H
-#define UTIL_H
-
-#define poly_small_to_fp PQCLEAN_FALCON1024_AARCH64_smallints_to_fpr
-
-void PQCLEAN_FALCON1024_AARCH64_smallints_to_fpr(fpr *r, const int8_t *t, unsigned logn);
-
-#endif
