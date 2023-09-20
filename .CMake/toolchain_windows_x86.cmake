122d838a64ae8057f928edac0a02cb20cb0b0ebe
res0nance <raihaanhimself@gmail.com>
2023-09-20 20:54:15 +0800

    ci: add windows x86 builds

---
+++ .CMake/toolchain_windows_x86.cmake
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: MIT
+
+set(CMAKE_SYSTEM_NAME Windows)
+
+set(CMAKE_SYSTEM_PROCESSOR x86)
+
+set(CMAKE_CROSSCOMPILING OFF)
+
+set(CMAKE_GENERATOR_PLATFORM
+    Win32
+    CACHE STRING "Platform" FORCE
+)
