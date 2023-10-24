cf8697522b09ba20673dbe5675f6aa8cb37da4f3
Spencer Wilson <spencer.wilson@uwaterloo.ca>
2023-10-24 11:20:05 -0400

    Fix CI build errors

--- src/common/rand/rand_hqc.c
+++ src/common/rand/rand_hqc.c
@@ -1,3 +1,4 @@
+#include "oqs/oqs.h"
 #include "oqs/sha3.h"
 
 #define PRNG_DOMAIN 1
@@ -10,7 +11,7 @@ OQS_SHA3_shake256_inc_ctx shake_prng_state = { NULL };
 
 void OQS_randombytes_hqc_kat(uint8_t *buf, size_t n);
 
-void OQS_randombytes_hqc_kat_init(const uint8_t *entropy_input, const uint8_t *personalization_string) {
+OQS_API void OQS_randombytes_hqc_kat_init(const uint8_t *entropy_input, const uint8_t *personalization_string) {
 	uint8_t domain = PRNG_DOMAIN;
 	if (shake_prng_state.ctx != NULL) {
 		OQS_SHA3_shake256_inc_ctx_reset(&shake_prng_state);
@@ -29,7 +30,7 @@ void OQS_randombytes_hqc_kat(uint8_t *buf, size_t n) {
 	OQS_SHA3_shake256_inc_squeeze(buf, n, &shake_prng_state);
 }
 
-void OQS_randombytes_hqc_kat_free() {
+OQS_API void OQS_randombytes_hqc_kat_free(void) {
 	if (shake_prng_state.ctx != NULL) {
 		OQS_SHA3_shake256_inc_ctx_release(&shake_prng_state);
 		shake_prng_state.ctx = NULL;
