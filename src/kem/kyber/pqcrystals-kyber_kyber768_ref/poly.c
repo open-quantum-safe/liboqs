cba3e40c3ab9923b087d46f47174e03b0b9f5102
Basil Hess <bhe@zurich.ibm.com>
2023-12-15 15:46:27 +0100

    pull kyber from upstream: dda29cc63af721981ee2c831cf00822e69be3220

--- src/kem/kyber/pqcrystals-kyber_kyber768_ref/poly.c
+++ src/kem/kyber/pqcrystals-kyber_kyber768_ref/poly.c
@@ -180,14 +180,19 @@ void poly_frommsg(poly *r, const uint8_t msg[KYBER_INDCPA_MSGBYTES])
 void poly_tomsg(uint8_t msg[KYBER_INDCPA_MSGBYTES], const poly *a)
 {
   unsigned int i,j;
-  uint16_t t;
+  uint32_t t;
 
   for(i=0;i<KYBER_N/8;i++) {
     msg[i] = 0;
     for(j=0;j<8;j++) {
       t  = a->coeffs[8*i+j];
-      t += ((int16_t)t >> 15) & KYBER_Q;
-      t  = (((t << 1) + KYBER_Q/2)/KYBER_Q) & 1;
+      // t += ((int16_t)t >> 15) & KYBER_Q;
+      // t  = (((t << 1) + KYBER_Q/2)/KYBER_Q) & 1;
+      t <<= 1;
+      t += 1665;
+      t *= 80635;
+      t >>= 28;
+      t &= 1;
       msg[i] |= t << j;
     }
   }
