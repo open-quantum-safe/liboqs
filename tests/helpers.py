e37e2f0b9809f68730abe4986dbe204377a59f06
Duc Nguyen <ductri.nguyen@sandboxquantum.com>
2023-12-21 11:01:12 -0500

    remove debug information

--- tests/helpers.py
+++ tests/helpers.py
@@ -180,7 +180,6 @@ def path_to_executable(program_name):
         os.path.join(path, program_name + ".EXE"),
         os.path.join(path, program_name + ".exe"),
         os.path.join(path, "Debug", program_name + ".exe"),]:
-            print(f"executable = {executable}")
             if os.path.isfile(executable):
                 return executable
     assert False, "Unable to find executable file {}".format(program_name)
