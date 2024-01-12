aca5e15a9f3d6b99d293156d5a2d3aff976d0744
Spencer Wilson <spencer.wilson@uwaterloo.ca>
2024-01-12 11:56:54 -0500

    Separate public and internal headers

--- src/oqs.h
+++ src/oqs.h
@@ -14,10 +14,6 @@
 #include <oqs/oqsconfig.h>
 
 #include <oqs/common.h>
-#include <oqs/aes.h>
-#include <oqs/sha2.h>
-#include <oqs/sha3.h>
-#include <oqs/sha3x4.h>
 #include <oqs/rand.h>
 #include <oqs/kem.h>
 #include <oqs/sig.h>
