0883606ba63ca954fb1cf1da9b0d2cceb316b033
Duc Nguyen <106774416+ducnguyen-sb@users.noreply.github.com>
2024-01-19 14:37:07 -0500

    Add Apache 2.0 and MIT License to XMSS (#1662)
    * Add Apache 2.0 and MIT License to XMSS

--- src/sig_stfl/xmss/external/xmss_core.h
+++ src/sig_stfl/xmss/external/xmss_core.h
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: Apache-2.0 AND MIT
 #ifndef XMSS_CORE_H
 #define XMSS_CORE_H
 
--- src/sig_stfl/xmss/sig_stfl_xmssmt_shake128_h20_2.c
+++ src/sig_stfl/xmss/sig_stfl_xmssmt_shake128_h20_2.c
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: Apache-2.0 AND MIT
 
 #include <string.h>
 #include <stdlib.h>
