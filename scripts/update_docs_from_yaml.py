8ebbcb918e028d7f9e1dca598c0fe4a3fcf3028a
Basil Hess <bhe@zurich.ibm.com>
2024-01-30 17:31:40 +0100

    update readme with aliases

--- scripts/update_docs_from_yaml.py
+++ scripts/update_docs_from_yaml.py
@@ -295,13 +295,21 @@ def do_it(liboqs_root):
             parameter_sets = kem_yaml['parameter-sets']
             if any(impl['large-stack-usage'] for impl in parameter_sets[0]['implementations']):
                 readme.write('- **{}**: {}†'.format(kem_yaml['name'], parameter_sets[0]['name']))
+                if 'alias' in parameter_sets[0]:
+                    readme.write(' (alias: {})'.format(parameter_sets[0]['alias']))
             else:
                 readme.write('- **{}**: {}'.format(kem_yaml['name'], parameter_sets[0]['name']))
+                if 'alias' in parameter_sets[0]:
+                    readme.write(' (alias: {})'.format(parameter_sets[0]['alias']))
             for parameter_set in parameter_sets[1:]:
                 if any(impl['large-stack-usage'] for impl in parameter_set['implementations']):
                     readme.write(', {}†'.format(parameter_set['name']))
+                    if 'alias' in parameter_set:
+                        readme.write(' (alias: {})'.format(parameter_set['alias']))
                 else:
                     readme.write(', {}'.format(parameter_set['name']))
+                    if 'alias' in parameter_set:
+                        readme.write(' (alias: {})'.format(parameter_set['alias']))
             readme.write('\n')
 
         readme.write(postamble)
@@ -322,13 +330,21 @@ def do_it(liboqs_root):
             parameter_sets = sig_yaml['parameter-sets']
             if any(impl['large-stack-usage'] for impl in parameter_sets[0]['implementations']):
                 readme.write('- **{}**: {}†'.format(sig_yaml['name'], parameter_sets[0]['name'].replace('_','\_')))
+                if 'alias' in parameter_sets[0]:
+                    readme.write(' (alias: {})'.format(parameter_sets[0]['alias']).replace('_','\_'))
             else:
                 readme.write('- **{}**: {}'.format(sig_yaml['name'], parameter_sets[0]['name'].replace('_','\_')))
+                if 'alias' in parameter_sets[0]:
+                    readme.write(' (alias: {})'.format(parameter_sets[0]['alias']).replace('_','\_'))
             for parameter_set in parameter_sets[1:]:
                 if any(impl['large-stack-usage'] for impl in parameter_set['implementations']):
                     readme.write(', {}†'.format(parameter_set['name'].replace('_', '\_')))
+                    if 'alias' in parameter_set:
+                        readme.write(' (alias: {})'.format(parameter_set['alias']).replace('_','\_'))
                 else:
                     readme.write(', {}'.format(parameter_set['name'].replace('_', '\_')))
+                    if 'alias' in parameter_set:
+                        readme.write(' (alias: {})'.format(parameter_set['alias']).replace('_','\_'))
             readme.write('\n')
 
         sphincs_yml = sig_yamls[-1]
