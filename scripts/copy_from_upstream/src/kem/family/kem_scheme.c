8b8a7ef2d7672643968b3e3878eb102d349a4cfc
Pravek Sharma <sharmapravek@gmail.com>
2024-04-02 22:31:10 +0200

    Update KEM family templates
    Signed-off-by: Pravek Sharma <sharmapravek@gmail.com>

--- scripts/copy_from_upstream/src/kem/family/kem_scheme.c
+++ scripts/copy_from_upstream/src/kem/family/kem_scheme.c
@@ -115,7 +115,20 @@ extern int PQCLEAN_{{ scheme['pqclean_scheme_c']|upper }}_{{ impl['name']|upper 
 #endif
     {%- endfor %}
 
+{%- if libjade_implementation is defined and scheme['libjade_implementation'] %}
+///// OQS_COPY_FROM_LIBJADE_FRAGMENT_KEM_SCHEME_START
+
+///// OQS_COPY_FROM_LIBJADE_FRAGMENT_KEM_SCHEME_END
+{%- endif %}
+
 OQS_API OQS_STATUS OQS_KEM_{{ family }}_{{ scheme['scheme'] }}_keypair(uint8_t *public_key, uint8_t *secret_key) {
+{%- if libjade_implementation is defined and scheme['libjade_implementation'] %}
+#if OQS_LIBJADE_BUILD
+///// OQS_COPY_FROM_LIBJADE_FRAGMENT_KEM_SCHEME_KEYPAIR_START
+
+///// OQS_COPY_FROM_LIBJADE_FRAGMENT_KEM_SCHEME_KEYPAIR_END
+#else
+{%- endif %}
     {%- for impl in scheme['metadata']['implementations'] if impl['name'] != scheme['default_implementation'] %}
     {%- if loop.first %}
 #if defined(OQS_ENABLE_KEM_{{ family }}_{{ scheme['scheme'] }}_{{ impl['name'] }}) {%- if 'alias_scheme' in scheme %} || defined(OQS_ENABLE_KEM_{{ family }}_{{ scheme['alias_scheme'] }}_{{ impl['name'] }}){%- endif %}
@@ -150,6 +163,13 @@ OQS_API OQS_STATUS OQS_KEM_{{ family }}_{{ scheme['scheme'] }}_keypair(uint8_t *
 }
 
 OQS_API OQS_STATUS OQS_KEM_{{ family }}_{{ scheme['scheme'] }}_encaps(uint8_t *ciphertext, uint8_t *shared_secret, const uint8_t *public_key) {
+{%- if libjade_implementation is defined and scheme['libjade_implementation'] %}
+#if OQS_LIBJADE_BUILD
+///// OQS_COPY_FROM_LIBJADE_FRAGMENT_KEM_SCHEME_ENCAPS_START
+
+///// OQS_COPY_FROM_LIBJADE_FRAGMENT_KEM_SCHEME_ENCAPS_END
+#else
+{%- endif %}
     {%- for impl in scheme['metadata']['implementations'] if impl['name'] != scheme['default_implementation'] %}
     {%- if loop.first %}
 #if defined(OQS_ENABLE_KEM_{{ family }}_{{ scheme['scheme'] }}_{{ impl['name'] }}) {%- if 'alias_scheme' in scheme %} || defined(OQS_ENABLE_KEM_{{ family }}_{{ scheme['alias_scheme'] }}_{{ impl['name'] }}){%- endif %}
@@ -184,6 +204,11 @@ OQS_API OQS_STATUS OQS_KEM_{{ family }}_{{ scheme['scheme'] }}_encaps(uint8_t *c
 }
 
 OQS_API OQS_STATUS OQS_KEM_{{ family }}_{{ scheme['scheme'] }}_decaps(uint8_t *shared_secret, const uint8_t *ciphertext, const uint8_t *secret_key) {
+#if OQS_LIBJADE_BUILD
+///// OQS_COPY_FROM_LIBJADE_FRAGMENT_KEM_SCHEME_DECAPS_START
+
+///// OQS_COPY_FROM_LIBJADE_FRAGMENT_KEM_SCHEME_DECAPS_END
+#else
     {%- for impl in scheme['metadata']['implementations'] if impl['name'] != scheme['default_implementation'] %}
     {%- if loop.first %}
 #if defined(OQS_ENABLE_KEM_{{ family }}_{{ scheme['scheme'] }}_{{ impl['name'] }}) {%- if 'alias_scheme' in scheme %} || defined(OQS_ENABLE_KEM_{{ family }}_{{ scheme['alias_scheme'] }}_{{ impl['name'] }}){%- endif %}
