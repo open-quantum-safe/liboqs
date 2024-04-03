06b1036be5f357bfe6f31109eae252a40c02932b
Pravek Sharma <sharmapravek@gmail.com>
2024-04-03 17:52:58 +0200

    Run copy_from_upstream.py
    Signed-off-by: Pravek Sharma <sharmapravek@gmail.com>

--- src/kem/kyber/kem_kyber_768.c
+++ src/kem/kyber/kem_kyber_768.c
@@ -45,29 +45,46 @@ extern int PQCLEAN_KYBER768_AARCH64_crypto_kem_keypair(uint8_t *pk, uint8_t *sk)
 extern int PQCLEAN_KYBER768_AARCH64_crypto_kem_enc(uint8_t *ct, uint8_t *ss, const uint8_t *pk);
 extern int PQCLEAN_KYBER768_AARCH64_crypto_kem_dec(uint8_t *ss, const uint8_t *ct, const uint8_t *sk);
 #endif
-///// OQS_COPY_FROM_LIBJADE_FRAGMENT_KEM_SCHEME_START
-
 
 #if defined(OQS_ENABLE_LIBJADE_KEM_kyber_768)
 extern int libjade_kyber768_amd64_ref_keypair(uint8_t *pk, uint8_t *sk);
 extern int libjade_kyber768_amd64_ref_enc(uint8_t *ct, uint8_t *ss, const uint8_t *pk);
 extern int libjade_kyber768_amd64_ref_dec(uint8_t *ss, const uint8_t *ct, const uint8_t *sk);
 #endif
-
+    
 #if defined(OQS_ENABLE_LIBJADE_KEM_kyber_768_amd64_avx2)
 extern int libjade_kyber768_amd64_avx2_keypair(uint8_t *pk, uint8_t *sk);
 extern int libjade_kyber768_amd64_avx2_enc(uint8_t *ct, uint8_t *ss, const uint8_t *pk);
 extern int libjade_kyber768_amd64_avx2_dec(uint8_t *ss, const uint8_t *ct, const uint8_t *sk);
 #endif
-
-///// OQS_COPY_FROM_LIBJADE_FRAGMENT_KEM_SCHEME_END
+    
 
 OQS_API OQS_STATUS OQS_KEM_kyber_768_keypair(uint8_t *public_key, uint8_t *secret_key) {
 #if OQS_LIBJADE_BUILD
-///// OQS_COPY_FROM_LIBJADE_FRAGMENT_KEM_SCHEME_KEYPAIR_START
-
-///// OQS_COPY_FROM_LIBJADE_FRAGMENT_KEM_SCHEME_KEYPAIR_END
+#if defined(OQS_ENABLE_LIBJADE_KEM_kyber_768_amd64_avx2)
+#if defined(OQS_DIST_BUILD)
+	if (OQS_CPU_has_extension(OQS_CPU_EXT_AVX2) && OQS_CPU_has_extension(OQS_CPU_EXT_BMI2) && OQS_CPU_has_extension(OQS_CPU_EXT_POPCNT)) {
+#endif /* OQS_DIST_BUILD */
+		return (OQS_STATUS) libjade_kyber768_amd64_avx2_keypair(public_key, secret_key);
+#if defined(OQS_DIST_BUILD)
+	} else {
+		return (OQS_STATUS) libjade_kyber768_amd64_ref_keypair(public_key, secret_key);
+	}
+#endif /* OQS_DIST_BUILD */
+#elif defined(OQS_ENABLE_KEM_kyber_768_aarch64)
+#if defined(OQS_DIST_BUILD)
+	if (OQS_CPU_has_extension(OQS_CPU_EXT_ARM_NEON)) {
+#endif /* OQS_DIST_BUILD */
+		return (OQS_STATUS) PQCLEAN_KYBER768_AARCH64_crypto_kem_keypair(public_key, secret_key);
+#if defined(OQS_DIST_BUILD)
+	} else {
+		return (OQS_STATUS) libjade_kyber768_amd64_ref_keypair(public_key, secret_key);
+	}
+#endif /* OQS_DIST_BUILD */
 #else
+    return (OQS_STATUS) libjade_kyber768_amd64_ref_keypair(public_key, secret_key);
+#endif
+#else /*OQS_LIBJADE_BUILD*/
 #if defined(OQS_ENABLE_KEM_kyber_768_avx2)
 #if defined(OQS_DIST_BUILD)
 	if (OQS_CPU_has_extension(OQS_CPU_EXT_AVX2) && OQS_CPU_has_extension(OQS_CPU_EXT_BMI2) && OQS_CPU_has_extension(OQS_CPU_EXT_POPCNT)) {
@@ -91,14 +108,35 @@ OQS_API OQS_STATUS OQS_KEM_kyber_768_keypair(uint8_t *public_key, uint8_t *secre
 #else
 	return (OQS_STATUS) pqcrystals_kyber768_ref_keypair(public_key, secret_key);
 #endif
+#endif /* OQS_LIBJADE_BUILD */
 }
 
 OQS_API OQS_STATUS OQS_KEM_kyber_768_encaps(uint8_t *ciphertext, uint8_t *shared_secret, const uint8_t *public_key) {
 #if OQS_LIBJADE_BUILD
-///// OQS_COPY_FROM_LIBJADE_FRAGMENT_KEM_SCHEME_ENCAPS_START
-
-///// OQS_COPY_FROM_LIBJADE_FRAGMENT_KEM_SCHEME_ENCAPS_END
+#if defined(OQS_ENABLE_LIBJADE_KEM_kyber_768_amd64_avx2)
+#if defined(OQS_DIST_BUILD)
+	if (OQS_CPU_has_extension(OQS_CPU_EXT_AVX2) && OQS_CPU_has_extension(OQS_CPU_EXT_BMI2) && OQS_CPU_has_extension(OQS_CPU_EXT_POPCNT)) {
+#endif /* OQS_DIST_BUILD */
+		return (OQS_STATUS) libjade_kyber768_amd64_avx2_enc(public_key, secret_key);
+#if defined(OQS_DIST_BUILD)
+	} else {
+		return (OQS_STATUS) libjade_kyber768_amd64_ref_enc(public_key, secret_key);
+	}
+#endif /* OQS_DIST_BUILD */
+#elif defined(OQS_ENABLE_KEM_kyber_768_aarch64)
+#if defined(OQS_DIST_BUILD)
+	if (OQS_CPU_has_extension(OQS_CPU_EXT_ARM_NEON)) {
+#endif /* OQS_DIST_BUILD */
+		return (OQS_STATUS) PQCLEAN_KYBER768_AARCH64_crypto_kem_enc(public_key, secret_key);
+#if defined(OQS_DIST_BUILD)
+	} else {
+		return (OQS_STATUS) libjade_kyber768_amd64_ref_enc(public_key, secret_key);
+	}
+#endif /* OQS_DIST_BUILD */
 #else
+    return (OQS_STATUS) libjade_kyber768_amd64_ref_enc(public_key, secret_key);
+#endif
+#else /*OQS_LIBJADE_BUILD*/
 #if defined(OQS_ENABLE_KEM_kyber_768_avx2)
 #if defined(OQS_DIST_BUILD)
 	if (OQS_CPU_has_extension(OQS_CPU_EXT_AVX2) && OQS_CPU_has_extension(OQS_CPU_EXT_BMI2) && OQS_CPU_has_extension(OQS_CPU_EXT_POPCNT)) {
@@ -122,14 +160,35 @@ OQS_API OQS_STATUS OQS_KEM_kyber_768_encaps(uint8_t *ciphertext, uint8_t *shared
 #else
 	return (OQS_STATUS) pqcrystals_kyber768_ref_enc(ciphertext, shared_secret, public_key);
 #endif
+#endif /* OQS_LIBJADE_BUILD */
 }
 
 OQS_API OQS_STATUS OQS_KEM_kyber_768_decaps(uint8_t *shared_secret, const uint8_t *ciphertext, const uint8_t *secret_key) {
 #if OQS_LIBJADE_BUILD
-///// OQS_COPY_FROM_LIBJADE_FRAGMENT_KEM_SCHEME_DECAPS_START
-
-///// OQS_COPY_FROM_LIBJADE_FRAGMENT_KEM_SCHEME_DECAPS_END
+#if defined(OQS_ENABLE_LIBJADE_KEM_kyber_768_amd64_avx2)
+#if defined(OQS_DIST_BUILD)
+	if (OQS_CPU_has_extension(OQS_CPU_EXT_AVX2) && OQS_CPU_has_extension(OQS_CPU_EXT_BMI2) && OQS_CPU_has_extension(OQS_CPU_EXT_POPCNT)) {
+#endif /* OQS_DIST_BUILD */
+		return (OQS_STATUS) libjade_kyber768_amd64_avx2_dec(public_key, secret_key);
+#if defined(OQS_DIST_BUILD)
+	} else {
+		return (OQS_STATUS) libjade_kyber768_amd64_ref_dec(public_key, secret_key);
+	}
+#endif /* OQS_DIST_BUILD */
+#elif defined(OQS_ENABLE_KEM_kyber_768_aarch64)
+#if defined(OQS_DIST_BUILD)
+	if (OQS_CPU_has_extension(OQS_CPU_EXT_ARM_NEON)) {
+#endif /* OQS_DIST_BUILD */
+		return (OQS_STATUS) PQCLEAN_KYBER768_AARCH64_crypto_kem_dec(public_key, secret_key);
+#if defined(OQS_DIST_BUILD)
+	} else {
+		return (OQS_STATUS) libjade_kyber768_amd64_ref_dec(public_key, secret_key);
+	}
+#endif /* OQS_DIST_BUILD */
 #else
+    return (OQS_STATUS) libjade_kyber768_amd64_ref_dec(public_key, secret_key);
+#endif
+#else /*OQS_LIBJADE_BUILD*/
 #if defined(OQS_ENABLE_KEM_kyber_768_avx2)
 #if defined(OQS_DIST_BUILD)
 	if (OQS_CPU_has_extension(OQS_CPU_EXT_AVX2) && OQS_CPU_has_extension(OQS_CPU_EXT_BMI2) && OQS_CPU_has_extension(OQS_CPU_EXT_POPCNT)) {
@@ -153,6 +212,7 @@ OQS_API OQS_STATUS OQS_KEM_kyber_768_decaps(uint8_t *shared_secret, const uint8_
 #else
 	return (OQS_STATUS) pqcrystals_kyber768_ref_dec(shared_secret, ciphertext, secret_key);
 #endif
+#endif /* OQS_LIBJADE_BUILD */
 }
 
 #endif
