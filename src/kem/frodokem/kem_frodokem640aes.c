41f3240f31844b59c8ff0a8f976221c5ed80425b
Eddy Kim <Eddy.M.Kim@outlook.com>
2024-05-20 11:17:34 -0400

    Initial derive keypair commit
    Signed-off-by: Eddy Kim <Eddy.M.Kim@outlook.com>
    Signed-off-by: Eddy Kim <e84kim@uwaterloo.ca>
    
    Add pqcrystals-ml_kem_ipd.patch
    
    Signed-off-by: Eddy Kim <Eddy.M.Kim@outlook.com>
    Signed-off-by: Eddy Kim <e84kim@uwaterloo.ca>
    
    Fix encaps key in scheme and revert whitespace changes
    
    Signed-off-by: Eddy Kim <e84kim@uwaterloo.ca>
    
    Hopefully corrected patch file
    
    Signed-off-by: Eddy Kim <e84kim@uwaterloo.ca>
    
    Corrected missing derand in kem_scheme
    
    Signed-off-by: Eddy Kim <e84kim@uwaterloo.ca>
    
    Fix indentation
    
    Signed-off-by: Eddy Kim <e84kim@uwaterloo.ca>
    
    Run copy_from_upstream
    
    Signed-off-by: Eddy Kim <e84kim@uwaterloo.ca>
    
    derand testing tentative changes
    
    Signed-off-by: Eddy Kim <e84kim@uwaterloo.ca>
    
    Add missing function declarations
    
    Signed-off-by: Eddy Kim <e84kim@uwaterloo.ca>
    
    Add template for avx2 derand functions
    
    Signed-off-by: Eddy Kim <e84kim@uwaterloo.ca>
    
    Run copy_from_upstream
    
    Signed-off-by: Eddy Kim <e84kim@uwaterloo.ca>
    
    WIP: Add changes for coin length
    
    Signed-off-by: Eddy Kim <e84kim@uwaterloo.ca>
    
    Update patch to include coin lengths
    
    Signed-off-by: Eddy Kim <e84kim@uwaterloo.ca>
    Signed-off-by: Spencer Wilson <spencer.wilson@uwaterloo.ca>
    
    Bootstrap
    
    Signed-off-by: Eddy Kim <e84kim@uwaterloo.ca>
    Signed-off-by: Spencer Wilson <spencer.wilson@uwaterloo.ca>
    
    Conditional copy
    
    Signed-off-by: Eddy Kim <e84kim@uwaterloo.ca>
    Signed-off-by: Spencer Wilson <spencer.wilson@uwaterloo.ca>
    
    Run copy_from_upstream
    
    Signed-off-by: Eddy Kim <e84kim@uwaterloo.ca>
    Signed-off-by: Spencer Wilson <spencer.wilson@uwaterloo.ca>
    
    Separate coins variable into two distinct variables
    
    Signed-off-by: Eddy Kim <Eddy.M.Kim@outlook.com>
    Signed-off-by: Eddy Kim <e84kim@uwaterloo.ca>
    Signed-off-by: Spencer Wilson <spencer.wilson@uwaterloo.ca>
    
    Add derand fixes
    
    - Add support for BIKE, FrodoKEM, sntrup
    - Add hooks for testing
    - Add missing kem comment to documentation
    - Don't run decaps() in test_kem_derand if encaps_derand() fails
    - Add markdown documentation changes
    
    Signed-off-by: Eddy Kim <e84kim@uwaterloo.ca>
    Signed-off-by: Spencer Wilson <spencer.wilson@uwaterloo.ca>
    
    WIP trying to fix build errors
    
    Signed-off-by: Eddy Kim <e84kim@uwaterloo.ca>
    Signed-off-by: Spencer Wilson <spencer.wilson@uwaterloo.ca>
    
    Fix remaining build issues
    
    Signed-off-by: Eddy Kim <e84kim@uwaterloo.ca>
    Signed-off-by: Spencer Wilson <spencer.wilson@uwaterloo.ca>
    
    Resolve unused parameter issues for BIKE
    
    Signed-off-by: Eddy Kim <e84kim@uwaterloo.ca>
    Signed-off-by: Spencer Wilson <spencer.wilson@uwaterloo.ca>
    
    Resolve unused paramter issues for FrodoKEM
    
    Signed-off-by: Eddy Kim <e84kim@uwaterloo.ca>
    Signed-off-by: Spencer Wilson <spencer.wilson@uwaterloo.ca>
    
    Fix whitespace inconsistency
    
    Signed-off-by: Eddy Kim <e84kim@uwaterloo.ca>
    Signed-off-by: Spencer Wilson <spencer.wilson@uwaterloo.ca>
    
    Fix whitepace issue
    
    Signed-off-by: Eddy Kim <e84kim@uwaterloo.ca>
    Signed-off-by: Spencer Wilson <spencer.wilson@uwaterloo.ca>
    
    Insert unused attributes
    
    Signed-off-by: Eddy Kim <e84kim@uwaterloo.ca>
    Signed-off-by: Spencer Wilson <spencer.wilson@uwaterloo.ca>
    
    Void all unused parameters
    
    Signed-off-by: Eddy Kim <e84kim@uwaterloo.ca>
    Signed-off-by: Spencer Wilson <spencer.wilson@uwaterloo.ca>
    
    Use tab instead of spaces in kem_scheme
    
    Signed-off-by: Eddy Kim <e84kim@uwaterloo.ca>
    Signed-off-by: Spencer Wilson <spencer.wilson@uwaterloo.ca>
    
    Run copy_from_upstream
    
    Signed-off-by: Eddy Kim <e84kim@uwaterloo.ca>
    Signed-off-by: Spencer Wilson <spencer.wilson@uwaterloo.ca>
    
    Fix kem_derand python tests
    
    Signed-off-by: Eddy Kim <e84kim@uwaterloo.ca>
    Signed-off-by: Spencer Wilson <spencer.wilson@uwaterloo.ca>
    
    Initialize coins in test_kem_derand
    
    Signed-off-by: Eddy Kim <e84kim@uwaterloo.ca>
    Signed-off-by: Spencer Wilson <spencer.wilson@uwaterloo.ca>

--- src/kem/frodokem/kem_frodokem640aes.c
+++ src/kem/frodokem/kem_frodokem640aes.c
@@ -22,8 +22,12 @@ OQS_KEM *OQS_KEM_frodokem_640_aes_new(void) {
 	kem->length_secret_key = OQS_KEM_frodokem_640_aes_length_secret_key;
 	kem->length_ciphertext = OQS_KEM_frodokem_640_aes_length_ciphertext;
 	kem->length_shared_secret = OQS_KEM_frodokem_640_aes_length_shared_secret;
+	kem->length_keypair_coins = OQS_KEM_frodokem_640_aes_length_keypair_coins;
+	kem->length_encaps_coins = OQS_KEM_frodokem_640_aes_length_encaps_coins;
 
+	kem->keypair_derand = OQS_KEM_frodokem_640_aes_keypair_derand;
 	kem->keypair = OQS_KEM_frodokem_640_aes_keypair;
+	kem->encaps_derand = OQS_KEM_frodokem_640_aes_encaps_derand;
 	kem->encaps = OQS_KEM_frodokem_640_aes_encaps;
 	kem->decaps = OQS_KEM_frodokem_640_aes_decaps;
 
--- src/kem/ntruprime/kem_ntruprime.h
+++ src/kem/ntruprime/kem_ntruprime.h
@@ -10,8 +10,12 @@
 #define OQS_KEM_ntruprime_sntrup761_length_secret_key 1763
 #define OQS_KEM_ntruprime_sntrup761_length_ciphertext 1039
 #define OQS_KEM_ntruprime_sntrup761_length_shared_secret 32
+#define OQS_KEM_ntruprime_sntrup761_length_keypair_coins 0
+#define OQS_KEM_ntruprime_sntrup761_length_encaps_coins 0
 OQS_KEM *OQS_KEM_ntruprime_sntrup761_new(void);
+OQS_API OQS_STATUS OQS_KEM_ntruprime_sntrup761_keypair_derand(uint8_t *public_key, uint8_t *secret_key, const uint8_t *coins);
 OQS_API OQS_STATUS OQS_KEM_ntruprime_sntrup761_keypair(uint8_t *public_key, uint8_t *secret_key);
+OQS_API OQS_STATUS OQS_KEM_ntruprime_sntrup761_encaps_derand(uint8_t *ciphertext, uint8_t *shared_secret, const uint8_t *public_key, const uint8_t *coins);
 OQS_API OQS_STATUS OQS_KEM_ntruprime_sntrup761_encaps(uint8_t *ciphertext, uint8_t *shared_secret, const uint8_t *public_key);
 OQS_API OQS_STATUS OQS_KEM_ntruprime_sntrup761_decaps(uint8_t *shared_secret, const uint8_t *ciphertext, const uint8_t *secret_key);
 #endif
