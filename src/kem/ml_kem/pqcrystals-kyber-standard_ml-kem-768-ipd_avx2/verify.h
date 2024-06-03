982c762c242ef549c914891b47bf6e0ed6321f91
Basil Hess <bhe@zurich.ibm.com>
2024-06-03 15:26:57 +0200

    Pull Kyber/ML-KEM CT-Fix from upstream
    Signed-off-by: Basil Hess <bhe@zurich.ibm.com>

--- src/kem/ml_kem/pqcrystals-kyber-standard_ml-kem-768-ipd_avx2/verify.h
+++ src/kem/ml_kem/pqcrystals-kyber-standard_ml-kem-768-ipd_avx2/verify.h
@@ -11,4 +11,7 @@ int verify(const uint8_t *a, const uint8_t *b, size_t len);
 #define cmov KYBER_NAMESPACE(cmov)
 void cmov(uint8_t *r, const uint8_t *x, size_t len, uint8_t b);
 
+#define cmov_int16 KYBER_NAMESPACE(cmov_int16)
+void cmov_int16(int16_t *r, int16_t v, uint16_t b);
+
 #endif
