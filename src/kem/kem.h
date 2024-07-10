008161ffa552b11327de0836838b13b56a5887b9
Eddy Kim <Eddy.M.Kim@outlook.com>
2024-07-10 10:51:10 -0400

    Add missing function declarations
    Signed-off-by: Eddy Kim <e84kim@uwaterloo.ca>

--- src/kem/kem.h
+++ src/kem/kem.h
@@ -250,6 +250,20 @@ typedef struct OQS_KEM {
 OQS_API OQS_KEM *OQS_KEM_new(const char *method_name);
 
 /**
+ * Derandomized keypair generation algorithm.
+ *
+ * Caller is responsible for allocating sufficient memory for `public_key` and
+ * `secret_key`, based on the `length_*` members in this object or the per-scheme
+ * compile-time macros `OQS_KEM_*_length_*`.
+ *
+ * @param[out] public_key The public key represented as a byte string.
+ * @param[out] secret_key The secret key represented as a byte string.
+ * @param[in] coins The input randomness represented as a byte string.
+ * @return OQS_SUCCESS or OQS_ERROR
+ */
+OQS_API OQS_STATUS OQS_KEM_keypair_derand(const OQS_KEM *kem, uint8_t *public_key, uint8_t *secret_key, const uint8_t *coins);
+
+/**
  * Keypair generation algorithm.
  *
  * Caller is responsible for allocating sufficient memory for `public_key` and
@@ -262,6 +276,21 @@ OQS_API OQS_KEM *OQS_KEM_new(const char *method_name);
  * @return OQS_SUCCESS or OQS_ERROR
  */
 OQS_API OQS_STATUS OQS_KEM_keypair(const OQS_KEM *kem, uint8_t *public_key, uint8_t *secret_key);
+
+/**
+ * Derandomized encapsulation algorithm.
+ *
+ * Caller is responsible for allocating sufficient memory for `ciphertext` and
+ * `shared_secret`, based on the `length_*` members in this object or the per-scheme
+ * compile-time macros `OQS_KEM_*_length_*`.
+ *
+ * @param[out] ciphertext The ciphertext (encapsulation) represented as a byte string.
+ * @param[out] shared_secret The shared secret represented as a byte string.
+ * @param[in] public_key The public key represented as a byte string.
+ * @param[in] coins The input randomness represented as a byte string.
+ * @return OQS_SUCCESS or OQS_ERROR
+ */
+OQS_API OQS_STATUS OQS_KEM_encaps_derand(const OQS_KEM *kem, uint8_t *ciphertext, uint8_t *shared_secret, const uint8_t *public_key, const uint8_t *coins);
 
 /**
  * Encapsulation algorithm.
