26feef2e8a7863cc4f479e3ac387443beec06791
Daiki Ueno <dueno@redhat.com>
2024-07-12 00:53:46 +0900

    Expose callback API for replacing low-level cryptographic primitives (#1832)
    This makes the callback API to replace low-level cryptographic
    implementation public again after open-quantum-safe#1667.
    
    Signed-off-by: Daiki Ueno <dueno@redhat.com>

--- src/oqs.h
+++ src/oqs.h
@@ -18,5 +18,9 @@
 #include <oqs/kem.h>
 #include <oqs/sig.h>
 #include <oqs/sig_stfl.h>
+#include <oqs/aes_ops.h>
+#include <oqs/sha2_ops.h>
+#include <oqs/sha3_ops.h>
+#include <oqs/sha3x4_ops.h>
 
 #endif // OQS_H
