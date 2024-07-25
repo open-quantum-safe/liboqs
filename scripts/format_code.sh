e0af88ed9664862aff1b1574cbd7195bc2a626c6
Spencer Wilson <spencer.wilson@uwaterloo.ca>
2024-07-25 11:26:41 -0400

    Handle arm64
    Signed-off-by: Spencer Wilson <spencer.wilson@uwaterloo.ca>

--- scripts/format_code.sh
+++ scripts/format_code.sh
@@ -2,10 +2,18 @@
 
 # SPDX-License-Identifier: MIT
 
-if [ ! -d "$LIBOQS_DIR" ]
+arch=$(uname -m)
+
+if [ "$arch" != "x86_64" ] && [ "$arch" != "arm64" ]
 then
-	echo Must set environment variable LIBOQS_DIR
+	echo "This script does not currently support systems where \`uname -m\` returns $arch."
 	exit 1
 fi
 
-docker run --rm -v"$LIBOQS_DIR":/root/liboqs -w /root/liboqs openquantumsafe/ci-ubuntu-focal-x86_64:latest ./tests/run_astyle.sh --no-dry-run
+if [ ! -d "$LIBOQS_DIR" ]
+then
+	echo Please set the environment variable LIBOQS_DIR to point to your liboqs directory.
+	exit 1
+fi
+
+docker run --rm -v"$LIBOQS_DIR":/root/liboqs -w /root/liboqs openquantumsafe/ci-ubuntu-focal-$arch:latest ./tests/run_astyle.sh --no-dry-run
