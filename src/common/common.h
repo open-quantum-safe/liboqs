1a50b7c732bf5387f7425098e3de842175b1b365
Norman Ashley <nashley@cisco.com>
2024-10-25 00:19:49 +0000

    Update format
    Signed-off-by: Norman Ashley <nashley@cisco.com>

--- src/common/common.h
+++ src/common/common.h
@@ -170,39 +170,39 @@ extern "C" {
  *
  */
 typedef enum {
-  /** Used to indicate that some undefined error occurred. */
-  OQS_ERROR = -1,
-  /** Used to indicate successful return from function. */
-  OQS_SUCCESS = 0,
-  /** Used to indicate failures in external libraries (e.g., OpenSSL). */
-  OQS_EXTERNAL_LIB_ERROR_OPENSSL = 50,
+	/** Used to indicate that some undefined error occurred. */
+	OQS_ERROR = -1,
+	/** Used to indicate successful return from function. */
+	OQS_SUCCESS = 0,
+	/** Used to indicate failures in external libraries (e.g., OpenSSL). */
+	OQS_EXTERNAL_LIB_ERROR_OPENSSL = 50,
 } OQS_STATUS;
 
 /**
  * CPU runtime detection flags
  */
 typedef enum {
-  OQS_CPU_EXT_INIT, /* Must be first */
-  /* Start extension list */
-  OQS_CPU_EXT_ADX,
-  OQS_CPU_EXT_AES,
-  OQS_CPU_EXT_AVX,
-  OQS_CPU_EXT_AVX2,
-  OQS_CPU_EXT_AVX512,
-  OQS_CPU_EXT_BMI1,
-  OQS_CPU_EXT_BMI2,
-  OQS_CPU_EXT_PCLMULQDQ,
-  OQS_CPU_EXT_VPCLMULQDQ,
-  OQS_CPU_EXT_POPCNT,
-  OQS_CPU_EXT_SSE,
-  OQS_CPU_EXT_SSE2,
-  OQS_CPU_EXT_SSE3,
-  OQS_CPU_EXT_ARM_AES,
-  OQS_CPU_EXT_ARM_SHA2,
-  OQS_CPU_EXT_ARM_SHA3,
-  OQS_CPU_EXT_ARM_NEON,
-  /* End extension list */
-  OQS_CPU_EXT_COUNT, /* Must be last */
+	OQS_CPU_EXT_INIT, /* Must be first */
+	/* Start extension list */
+	OQS_CPU_EXT_ADX,
+	OQS_CPU_EXT_AES,
+	OQS_CPU_EXT_AVX,
+	OQS_CPU_EXT_AVX2,
+	OQS_CPU_EXT_AVX512,
+	OQS_CPU_EXT_BMI1,
+	OQS_CPU_EXT_BMI2,
+	OQS_CPU_EXT_PCLMULQDQ,
+	OQS_CPU_EXT_VPCLMULQDQ,
+	OQS_CPU_EXT_POPCNT,
+	OQS_CPU_EXT_SSE,
+	OQS_CPU_EXT_SSE2,
+	OQS_CPU_EXT_SSE3,
+	OQS_CPU_EXT_ARM_AES,
+	OQS_CPU_EXT_ARM_SHA2,
+	OQS_CPU_EXT_ARM_SHA3,
+	OQS_CPU_EXT_ARM_NEON,
+	/* End extension list */
+	OQS_CPU_EXT_COUNT, /* Must be last */
 } OQS_CPU_EXT;
 
 /**
