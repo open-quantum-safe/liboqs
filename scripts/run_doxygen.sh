2e218d8ad1a22737a954399d1bc9185174cbfdfb
Spencer Wilson <spencer.wilson@uwaterloo.ca>
2025-02-20 16:37:33 -0500

    Add SECURITY.md to Doxyfile
    Signed-off-by: Spencer Wilson <spencer.wilson@uwaterloo.ca>

--- scripts/run_doxygen.sh
+++ scripts/run_doxygen.sh
@@ -8,10 +8,11 @@ if [ "$#" -ne 3 ]; then
 fi
 
 # Doxygen can't handle Github Markdown so we temporarily reformat it
-cp README.md README.md-orig
-python3 scripts/doxyfy.py README.md-orig README.md
-cp CONFIGURE.md CONFIGURE.md-orig
-python3 scripts/doxyfy.py CONFIGURE.md-orig CONFIGURE.md
+for file in README.md CONFIGURE.md SECURITY.md
+do
+	cp $file $file-orig
+	python3 scripts/doxyfy.py $file-orig $file
+done
 
 # run doxygen:
 mkdir -p "$3/docs"
@@ -19,7 +20,9 @@ env DOXYGEN_DESTIONATION_DIR="$3/docs" "$1" "$2"
 EXITCODE=$?
 
 # undo the Github Markdown reformatting
-mv README.md-orig README.md
-mv CONFIGURE.md-orig CONFIGURE.md
+for file in README.md CONFIGURE.md SECURITY.md
+do
+	mv $file-orig $file
+done
 
 exit ${EXITCODE}
