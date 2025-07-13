a35c3b85765d5f41cede815c8fa92e171c05b967
nonam3e <timur@ingonyama.com>
2025-07-13 18:40:55 +0300

    build with local package
    Signed-off-by: nonam3e <timur@ingonyama.com>

--- src/kem/ml_kem/icicle_ml-kem-512_icicle_cuda/icicle_ml-kem.cpp
+++ src/kem/ml_kem/icicle_ml-kem-512_icicle_cuda/icicle_ml-kem.cpp
@@ -1,41 +1,31 @@
 #include <stdint.h>
 #include <oqs/rand.h>
+#include "icicle/pqc/ml_kem.h"
 #include "icicle/runtime.h"
-#include <icicle/pqc/ml_kem.h>
+#include "icicle/device.h"
+#include "icicle/errors.h"
 
 extern "C" {
+#if defined(OQS_ENABLE_KEM_ml_kem_512_icicle_cuda)
 int icicle_ml_kem_512_keypair(uint8_t *pk, uint8_t *sk) {
     uint8_t coins[icicle::pqc::ml_kem::ENTROPY_BYTES];
     OQS_randombytes(coins, icicle::pqc::ml_kem::ENTROPY_BYTES);
-#if defined(OQS_ENABLE_KEM_ml_kem_512_icicle_cuda)
-    icicle::Device dev = {"CUDA-PQC", 0};
-#endif
-    icicle_set_device(dev);
 
     icicle::pqc::ml_kem::MlKemConfig config;
-
     return (int)icicle::pqc::ml_kem::keygen<icicle::pqc::ml_kem::Kyber512Params>((std::byte*)coins, config, (std::byte*)pk, (std::byte*)sk);
 }
 
 int icicle_ml_kem_512_enc(uint8_t *ct, uint8_t *ss, const uint8_t *pk) {
     uint8_t coins[icicle::pqc::ml_kem::MESSAGE_BYTES];
     OQS_randombytes(coins, icicle::pqc::ml_kem::MESSAGE_BYTES);
-#if defined(OQS_ENABLE_KEM_ml_kem_512_icicle_cuda)
-    icicle::Device dev = {"CUDA-PQC", 0};
-#endif
-    icicle_set_device(dev);
 
     icicle::pqc::ml_kem::MlKemConfig config;
     return (int)icicle::pqc::ml_kem::encapsulate<icicle::pqc::ml_kem::Kyber512Params>((std::byte*)coins, (std::byte*)pk, config, (std::byte*)ct, (std::byte*)ss);
 }
 
 int icicle_ml_kem_512_dec(uint8_t *ss, const uint8_t *ct, const uint8_t *sk) {
-#if defined(OQS_ENABLE_KEM_ml_kem_512_icicle_cuda)
-    icicle::Device dev = {"CUDA-PQC", 0};
-#endif
-    icicle_set_device(dev);
-
     icicle::pqc::ml_kem::MlKemConfig config;
     return (int)icicle::pqc::ml_kem::decapsulate<icicle::pqc::ml_kem::Kyber512Params>((std::byte*)sk, (std::byte*)ct, config, (std::byte*)ss);
 }
+#endif
 }
