7bd434392c800ab73c215f6b63f557d6df8db0dd
h2parson <h2parson@uwaterloo.ca>
2025-08-21 16:42:00 -0400

    explicitly null terminate random string in vector_sig
    Signed-off-by: h2parson <h2parson@uwaterloo.ca>

--- tests/vectors_sig.c
+++ tests/vectors_sig.c
@@ -723,6 +723,8 @@ int main(int argc, char **argv) {
 				/* we actually want half the length of the public key but we double for a string. give extra byte for null char. */
 				prng_output_stream = OQS_MEM_malloc(sig->length_public_key + 1);
 				memcpy(prng_output_stream, sigGen_sk + 2 * sig->length_public_key, sig->length_public_key);
+				/* null terminate */
+				prng_output_stream[sig->length_public_key] = '\0';
 			}
 
 			if ( strlen(sigGen_msg) % 2 != 0 ||
@@ -787,6 +789,8 @@ int main(int argc, char **argv) {
 				/* we actually want half the length of the public key but we double for a string. give extra byte for null char. */
 				prng_output_stream = OQS_MEM_malloc(sig->length_public_key + 1);
 				memcpy(prng_output_stream, sigGen_sk + 2 * sig->length_public_key, sig->length_public_key);
+				/* null terminate */
+				prng_output_stream[sig->length_public_key] = '\0';
 			}
 
 			if ( strlen(sigGen_msg) % 2 != 0 ||
