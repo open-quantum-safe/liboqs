1adf20bb84004ea75aef4dbaeb5787cb265e016c
Ganyu (Bruce) Xu <g66xu@uwaterloo.ca>
2025-09-11 15:25:40 -0400

    Upgraded C++ sig linking test to ML-DSA-44; also added option to make the test fail hard if the algorithm is not enabled
    Signed-off-by: Ganyu (Bruce) Xu <g66xu@uwaterloo.ca>

--- cpp/sig_linking_test.cpp
+++ cpp/sig_linking_test.cpp
@@ -2,6 +2,18 @@
  * example_sig.cpp
  *
  * Minimal C++ example of using a post-quantum signature implemented in liboqs.
+ * To test linking, compile the program against libcrypto (from OpenSSL) and liboqs
+ *
+ * g++ -g -I${LIBOQS_DIR}/build/include \
+ *     -L${LIBOQS_DIR}/build/lib -loqs \
+ *     -lcrypto -std=c++11 \
+ *     -o ${LIBOQS_DIR}/build/tests/example_sig \
+ *     ${LIBOQS_DIR}/cpp/sig_linking_test.cpp \
+ * && ${LIBOQS_DIR}/build/tests/example_sig
+ *
+ * `-lcrypto` requires libcrypto to be in dynamic linker's path.
+ * If installed with Homebrew, it is likely located at:
+ * /opt/homebrew/Cellar/openssl@3/x.y.z/lib
  *
  * SPDX-License-Identifier: MIT
  */
@@ -13,6 +25,9 @@
 #include <memory>
 
 #include <oqs/oqs.h>
+
+/* TODO: I am not sure if "algo_xxx" not enabled should make the test succeed */
+#define CPP_LINKING_TEST_FAIL_HARD 0
 
 constexpr size_t MESSAGE_LEN = 50;
 
@@ -53,9 +68,9 @@ struct OQSSigDeleter {
  * statically on the stack, calling a specific algorithm's functions
  * directly.
  *
- * The macros OQS_SIG_dilithium_2_length_* and the functions OQS_SIG_dilithium_2_*
- * are only defined if the algorithm dilithium_2 was enabled at compile-time
- * which must be checked using the OQS_ENABLE_SIG_dilithium_2 macro.
+ * The macros OQS_SIG_ml_dsa_44_length_* and the functions OQS_SIG_ml_dsa_44_*
+ * are only defined if the algorithm ml_dsa_44 was enabled at compile-time
+ * which must be checked using the OQS_ENABLE_SIG_ml_dsa_44 macro.
  *
  * <oqs/oqsconfig.h>, which is included in <oqs/oqs.h>, contains macros
  * indicating which algorithms were enabled when this instance of liboqs
@@ -63,49 +78,51 @@ struct OQSSigDeleter {
  */
 static OQS_STATUS example_stack(void) {
 
-#ifdef OQS_ENABLE_SIG_dilithium_2
+#ifdef OQS_ENABLE_SIG_ml_dsa_44
 
 	OQS_STATUS rc;
 
-	uint8_t public_key[OQS_SIG_dilithium_2_length_public_key];
-	uint8_t secret_key[OQS_SIG_dilithium_2_length_secret_key];
+	uint8_t public_key[OQS_SIG_ml_dsa_44_length_public_key];
+	uint8_t secret_key[OQS_SIG_ml_dsa_44_length_secret_key];
 	uint8_t message[MESSAGE_LEN];
-	uint8_t signature[OQS_SIG_dilithium_2_length_signature];
+	uint8_t signature[OQS_SIG_ml_dsa_44_length_signature];
 	size_t message_len = MESSAGE_LEN;
 	size_t signature_len;
 
 	// let's create a random test message to sign
 	OQS_randombytes(message, message_len);
 
-	rc = OQS_SIG_dilithium_2_keypair(public_key, secret_key);
+	rc = OQS_SIG_ml_dsa_44_keypair(public_key, secret_key);
 	if (rc != OQS_SUCCESS) {
-		std::cerr << "ERROR: OQS_SIG_dilithium_2_keypair failed!" << std::endl;
-		cleanup_stack(secret_key, OQS_SIG_dilithium_2_length_secret_key);
+		std::cerr << "ERROR: OQS_SIG_ml_dsa_44_keypair failed!" << std::endl;
+		cleanup_stack(secret_key, OQS_SIG_ml_dsa_44_length_secret_key);
 		return OQS_ERROR;
 	}
-	rc = OQS_SIG_dilithium_2_sign(signature, &signature_len, message, message_len, secret_key);
+	rc = OQS_SIG_ml_dsa_44_sign(signature, &signature_len, message, message_len, secret_key);
 	if (rc != OQS_SUCCESS) {
-		std::cerr << "ERROR: OQS_SIG_dilithium_2_sign failed!" << std::endl;
-		cleanup_stack(secret_key, OQS_SIG_dilithium_2_length_secret_key);
+		std::cerr << "ERROR: OQS_SIG_ml_dsa_44_sign failed!" << std::endl;
+		cleanup_stack(secret_key, OQS_SIG_ml_dsa_44_length_secret_key);
 		return OQS_ERROR;
 	}
-	rc = OQS_SIG_dilithium_2_verify(message, message_len, signature, signature_len, public_key);
+	rc = OQS_SIG_ml_dsa_44_verify(message, message_len, signature, signature_len, public_key);
 	if (rc != OQS_SUCCESS) {
-		std::cerr << "ERROR: OQS_SIG_dilithium_2_verify failed!" << std::endl;
-		cleanup_stack(secret_key, OQS_SIG_dilithium_2_length_secret_key);
+		std::cerr << "ERROR: OQS_SIG_ml_dsa_44_verify failed!" << std::endl;
+		cleanup_stack(secret_key, OQS_SIG_ml_dsa_44_length_secret_key);
 		return OQS_ERROR;
 	}
 
-	std::cout << "[example_stack] OQS_SIG_dilithium_2 operations completed" << std::endl;
-	cleanup_stack(secret_key, OQS_SIG_dilithium_2_length_secret_key);
+	std::cout << "[example_stack] OQS_SIG_ml_dsa_44 operations completed" << std::endl;
+	cleanup_stack(secret_key, OQS_SIG_ml_dsa_44_length_secret_key);
 	return OQS_SUCCESS; // success!
 
 #else
-
-	std::cout << "[example_stack] OQS_SIG_dilithium_2 was not enabled at compile-time" << std::endl;
+	std::cout << "[example_stack] OQS_SIG_ml_dsa_44 was not enabled at compile-time" << std::endl;
+#if CPP_LINKING_TEST_FAIL_HARD
+    return OQS_ERROR;
+#else
 	return OQS_SUCCESS;
-
-#endif
+#endif /* CPP_LINKING_TEST_FAIL_HARD */
+#endif /* OQS_ENABLE_SIG_ml_dsa_44 */
 }
 
 /* This function gives an example of the signing operations,
@@ -118,15 +135,15 @@ static OQS_STATUS example_stack(void) {
  */
 static OQS_STATUS example_heap(void) {
 
-#ifdef OQS_ENABLE_SIG_dilithium_2
+#ifdef OQS_ENABLE_SIG_ml_dsa_44
 
 	size_t message_len = MESSAGE_LEN;
 	size_t signature_len;
 	OQS_STATUS rc;
 
-	std::unique_ptr<OQS_SIG, OQSSigDeleter> sig(OQS_SIG_new((OQS_SIG_alg_dilithium_2)));
+	std::unique_ptr<OQS_SIG, OQSSigDeleter> sig(OQS_SIG_new((OQS_SIG_alg_ml_dsa_44)));
 	if (sig == nullptr) {
-		throw std::runtime_error("[example_heap]  OQS_SIG_alg_dilithium_2 was not enabled at compile-time.");
+		throw std::runtime_error("[example_heap]  OQS_SIG_alg_ml_dsa_44 was not enabled at compile-time.");
 	}
 	std::unique_ptr<uint8_t[], OQSInsecureDeleter> public_key(static_cast<uint8_t*>(malloc(sig->length_public_key)));
 	std::unique_ptr<uint8_t[], OQSSecureDeleter> secret_key(static_cast<uint8_t*>(malloc(sig->length_secret_key)), OQSSecureDeleter(sig->length_secret_key));
@@ -152,14 +169,16 @@ static OQS_STATUS example_heap(void) {
 		throw std::runtime_error("ERROR: OQS_SIG_verify failed!");
 	}
 
-	std::cout << "[example_heap]  OQS_SIG_dilithium_2 operations completed." << std::endl;
+	std::cout << "[example_heap]  OQS_SIG_ml_dsa_44 operations completed." << std::endl;
 	return OQS_SUCCESS; // success
 #else
-
-	std::cout << "[example_heap] OQS_SIG_dilithium_2 was not enabled at compile-time." << std::endl;
+	std::cout << "[example_stack] OQS_SIG_ml_dsa_44 was not enabled at compile-time" << std::endl;
+#if CPP_LINKING_TEST_FAIL_HARD
+    return OQS_ERROR;
+#else
 	return OQS_SUCCESS;
-
-#endif
+#endif /* CPP_LINKING_TEST_FAIL_HARD */
+#endif /* OQS_ENABLE_SIG_ml_dsa_44 */
 }
 
 int main() {
