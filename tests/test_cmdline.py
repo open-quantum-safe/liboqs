14d50426e19ac2de59ba02f96ad6178b33e16f75
Norman Ashley <nashley@cisco.com>
2025-11-01 00:12:25 +0000

    Restrict slow LMS variants.
    Signed-off-by: Norman Ashley <nashley@cisco.com>

--- tests/test_cmdline.py
+++ tests/test_cmdline.py
@@ -31,6 +31,19 @@ def test_sig(sig_name):
 @helpers.filtered_test
 @pytest.mark.parametrize('sig_stfl_name', helpers.available_sig_stfls_by_name())
 def test_sig_stfl(sig_stfl_name):
+    # Define the list of LMS varients to allow
+    names_to_allow = ["LMS_SHA256_H5_W1", "LMS_SHA256_H5_W2", 
+                        "LMS_SHA256_H5_W4",
+                        "LMS_SHA256_H5_W8",
+                        "LMS_SHA256_H10_W1",
+                        "LMS_SHA256_H10_W2",
+                        "LMS_SHA256_H10_W4",
+                        "LMS_SHA256_H10_W8",
+                        "LMS_SHA256_H15_W1",
+                        "LMS_SHA256_H15_W2",
+                        "LMS_SHA256_H15_W4",
+                        "LMS_SHA256_H5_W8_H5_W8"
+                        "LMS_SHA256_H10_W4_H5_W8",]
     if not(helpers.is_sig_stfl_enabled_by_name(sig_stfl_name)): pytest.skip('Not enabled')
     # Test with KATs apply for XMSS
     if sig_stfl_name.startswith("XMSS"):
@@ -40,7 +53,7 @@ def test_sig_stfl(sig_stfl_name):
             [helpers.path_to_executable('test_sig_stfl'), sig_stfl_name, katfile],
         )
     else:
-        if sig_stfl_name.find("H20")==-1:
+        if sig_stfl_name not in names_to_allow:
             pytest.skip('Test skipped')
         else:
             helpers.run_subprocess(
@@ -49,5 +62,7 @@ def test_sig_stfl(sig_stfl_name):
 
 if __name__ == "__main__":
     import sys
+    print("NMA Debug In main\n")
+    print (sys.argv)
     pytest.main(sys.argv)
 
