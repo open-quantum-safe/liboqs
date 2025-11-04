320dfc4214ad6c8f66c0174fec51ce7da347d8a7
Bruce <g66xu@uwaterloo.ca>
2025-11-04 11:35:07 -0500

    OQS Builder: an attempt "copy from upstream 2.0" (#2315)
    * Getting started with oqsbuilder. Can now clone some upstream remote repos
    
    Signed-off-by: Ganyu (Bruce) Xu <g66xu@uwaterloo.ca>
    
    * Fixed typos. Implemented dryrun for clone_remote_repo. Start tracking OQS Builder progress in oqsbuilder/README.md. [skip ci]
    
    Signed-off-by: Ganyu (Bruce) Xu <g66xu@uwaterloo.ca>
    
    ---------
    
    Signed-off-by: Ganyu (Bruce) Xu <g66xu@uwaterloo.ca>

---
+++ oqsbuilder/__init__.py
@@ -0,0 +1,12 @@
+"""OQS Builder"""
+
+import os
+
+# TODO: consider using a logger instead of print for debugging
+
+_liboqs_dir = os.environ.get("LIBOQS_DIR")
+if not _liboqs_dir:
+    raise KeyError("Missing environment variable LIBOQS_DIR")
+LIBOQS_DIR = _liboqs_dir
+
+__version__ = "0.1"
