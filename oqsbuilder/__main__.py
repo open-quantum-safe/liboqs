dc3421014ef03b021673561874f1387333adc82e
Ganyu (Bruce) Xu <g66xu@uwaterloo.ca>
2025-11-06 16:10:29 -0500

    Removed argparse because it is currently a distraction, will add later
    Signed-off-by: Ganyu (Bruce) Xu <g66xu@uwaterloo.ca>

--- oqsbuilder/__main__.py
+++ oqsbuilder/__main__.py
@@ -1,4 +1,4 @@
-from argparse import ArgumentParser
+import os
 import sys
 from tempfile import TemporaryDirectory
 import yaml
@@ -16,45 +16,40 @@ def print_version():
         print(f"-   {path}")
 
 
-if __name__ == "__main__":
-    argparser = ArgumentParser()
-    argparser.add_argument("--version", action="store_true", help="Show version")
-    # TODO: add argument to specify patch dir
-    argparser.add_argument(
-        "-f",
-        "--file",
-        type=str,
-        help="path to copy_from_upstream.yml",
-    )
-    argparser.add_argument("cmd", nargs="?", choices=["copy"])
-    args = argparser.parse_args()
+def copy_from_upstream(
+    oqsbuildfile: str, patch_dir: str, upstream_parent_dir: str = LIBOQS_DIR
+):
+    """Copy implementations from upstream
 
-    if args.version:
-        print_version()
-    elif args.cmd == "copy":
-        if not args.file:
-            print(
-                f"{__package__}: error: the following arguments are required -f/--file"
+    on MacOS, TemporaryDirectory() is created under /var, which is
+    a symlink to private/var, but git considers tracking symlinks to be
+    unsafe and will refuse to run. Hence we need to use a base directory
+    that has not symlink component, such as LIBOQS_DIR. Fortunately,
+    all of TemporaryDirectory's automatic cleanup still works.
+
+    :param oqsbuildfile: path to copy_from_upstream.yml
+    :param patch_dir: path to a directory hosting the patch files for upstream
+    :param upstream_parent_dir: upstream repositories will be cloned into
+        a temporary subdirectory under this directory
+    """
+    with open(oqsbuildfile, mode="r", encoding="utf-8") as f:
+        instructions = yaml.safe_load(f)
+    print(f"Successfully loaded {oqsbuildfile}")
+    upstreams: list[dict[str, str]] = instructions["upstreams"]
+    with TemporaryDirectory(dir=upstream_parent_dir) as tempdir:
+        for upstream in upstreams:
+            clone_remote_repo(
+                tempdir,
+                upstream["name"],
+                upstream["git_url"],
+                commit=upstream.get("git_commit", None),
+                branch_or_tag=upstream.get("git_branch", None),
             )
-            exit(1)
-        with open(args.file, mode="r", encoding="utf-8") as f:
-            instructions = yaml.safe_load(f)
-        print(f"Successfully loaded {args.file}")
-        upstreams: list[dict[str, str]] = instructions["upstreams"]
-        # NOTE: on MacOS, TemporaryDirectory() is created under /var, which is
-        #   a symlink to private/var, but git considers tracking symlinks to be
-        #   unsafe and will refuse to run. Hence we need to use a base directory
-        #   that has not symlink component, such as LIBOQS_DIR. Fortunately,
-        #   all of TemporaryDirectory's automatic cleanup still works.
-        with TemporaryDirectory(dir=LIBOQS_DIR) as tempdir:
-            for upstream in upstreams:
-                clone_remote_repo(
-                    tempdir,
-                    upstream["name"],
-                    upstream["git_url"],
-                    commit=upstream.get("git_commit", None),
-                    branch_or_tag=upstream.get("git_branch", None),
-                )
-                # TODO: apply patches
-    else:
-        argparser.print_help()
+            # TODO: apply patches
+
+
+if __name__ == "__main__":
+    print_version()
+    buildfile = os.path.join(LIBOQS_DIR, "oqsbuilder", "oqsbuildfile.yml")
+    patch_dir = os.path.join(LIBOQS_DIR, "scripts", "copy_from_upstream", "patches")
+    copy_from_upstream(buildfile, patch_dir)
