ba9dfc7f8fdc279d7287ac1ac71fe0178bee4017
Ganyu (Bruce) Xu <g66xu@uwaterloo.ca>
2026-02-16 11:18:44 -0500

    [skip ci] can generate sign_with_ctx and verify_with_ctx
    Signed-off-by: Ganyu (Bruce) Xu <g66xu@uwaterloo.ca>

--- src/sig/ml_dsa/sig_ml_dsa_44.c
+++ src/sig/ml_dsa/sig_ml_dsa_44.c
@@ -33,19 +33,13 @@ OQS_SIG *OQS_SIG_ml_dsa_44_new(void) {
 }
 
 extern int PQCP_MLDSA_NATIVE_MLDSA44_C_keypair(uint8_t *pk, uint8_t *sk);
-
 extern int PQCP_MLDSA_NATIVE_MLDSA44_C_signature(uint8_t *sig, size_t *siglen, const uint8_t *m, size_t mlen, const uint8_t *ctx, size_t ctxlen, const uint8_t *sk);
 extern int PQCP_MLDSA_NATIVE_MLDSA44_C_verify(const uint8_t *sig, size_t siglen, const uint8_t *m, size_t mlen, const uint8_t *ctx, size_t ctxlen, const uint8_t *pk);
 
-
-
 #if defined(OQS_ENABLE_SIG_ml_dsa_44_aarch64)
 extern int PQCP_MLDSA_NATIVE_MLDSA44_AARCH64_keypair(uint8_t *pk, uint8_t *sk);
-
-
 extern int PQCP_MLDSA_NATIVE_MLDSA44_AARCH64_signature(uint8_t *sig, size_t *siglen, const uint8_t *m, size_t mlen, const uint8_t *ctx, size_t ctxlen, const uint8_t *sk);
 extern int PQCP_MLDSA_NATIVE_MLDSA44_AARCH64_verify(const uint8_t *sig, size_t siglen, const uint8_t *m, size_t mlen, const uint8_t *ctx, size_t ctxlen, const uint8_t *pk);
-
 #endif /* OQS_ENABLE_SIG_ml_dsa_44_aarch64 */
 
 
@@ -66,72 +60,67 @@ OQS_API OQS_STATUS OQS_SIG_ml_dsa_44_keypair(uint8_t *public_key, uint8_t *secre
 }
 
 OQS_API OQS_STATUS OQS_SIG_ml_dsa_44_sign(uint8_t *signature, size_t *signature_len, const uint8_t *message, size_t message_len, const uint8_t *secret_key) {
-
-
 #if defined(OQS_ENABLE_SIG_ml_dsa_44_aarch64)
-
-
 #if defined(OQS_DIST_BUILD)
 	if (OQS_CPU_has_extension(OQS_CPU_EXT_ARM_NEON)) {
-
 		return (OQS_STATUS) PQCP_MLDSA_NATIVE_MLDSA44_AARCH64_signature(signature, signature_len, message, message_len, NULL, 0, secret_key);
-
 	} else {
-
 		return (OQS_STATUS) PQCP_MLDSA_NATIVE_MLDSA44_C_signature(signature, signature_len, message, message_len, NULL, 0, secret_key);
-
 	}
 #else
-
 	return (OQS_STATUS) PQCP_MLDSA_NATIVE_MLDSA44_AARCH64_signature(signature, signature_len, message, message_len, NULL, 0, secret_key);
-
 #endif /* OQS_DIST_BUILD */
-
-
-
-
 #else
-
 	return (OQS_STATUS) PQCP_MLDSA_NATIVE_MLDSA44_C_signature(signature, signature_len, message, message_len, NULL, 0, secret_key);
-
 #endif
-
 }
 
 OQS_API OQS_STATUS OQS_SIG_ml_dsa_44_verify(const uint8_t *message, size_t message_len, const uint8_t *signature, size_t signature_len, const uint8_t *public_key) {
-
-
-
 #if defined(OQS_ENABLE_SIG_ml_dsa_44_aarch64)
-
-
 #if defined(OQS_DIST_BUILD)
 	if (OQS_CPU_has_extension(OQS_CPU_EXT_ARM_NEON)) {
-
 		return (OQS_STATUS) PQCP_MLDSA_NATIVE_MLDSA44_AARCH64_verify(signature, signature_len, message, message_len, NULL, 0, secret_key);
-
 	} else {
-
 		return (OQS_STATUS) PQCP_MLDSA_NATIVE_MLDSA44_C_verify(signature, signature_len, message, message_len, NULL, 0, secret_key);
-
 	}
 #else
-
 	return (OQS_STATUS) PQCP_MLDSA_NATIVE_MLDSA44_AARCH64_verify(signature, signature_len, message, message_len, NULL, 0, secret_key);
-
 #endif /* OQS_DIST_BUILD */
-
-
-
-
 #else
-
 	return (OQS_STATUS) PQCP_MLDSA_NATIVE_MLDSA44_C_verify(signature, signature_len, message, message_len, NULL, 0, public_key);
-
 #endif
-
 }
 
-
-
+OQS_API OQS_STATUS OQS_SIG_ml_dsa_44_sign_with_ctx_str(uint8_t *signature, size_t *signature_len, const uint8_t *message, size_t message_len, const uint8_t *ctx_str, size_t ctx_str_len, const uint8_t *secret_key) {
+#if defined(OQS_ENABLE_SIG_ml_dsa_44_aarch64)
+#if defined(OQS_DIST_BUILD)
+	if (OQS_CPU_has_extension(OQS_CPU_EXT_ARM_NEON)) {
+		return (OQS_STATUS) PQCP_MLDSA_NATIVE_MLDSA44_AARCH64_signature(signature, signature_len, message, message_len, NULL, 0, secret_key);
+	} else {
+		return (OQS_STATUS) PQCP_MLDSA_NATIVE_MLDSA44_C_signature(signature, signature_len, message, message_len, NULL, 0, secret_key);
+	}
+#else
+	return (OQS_STATUS) PQCP_MLDSA_NATIVE_MLDSA44_AARCH64_signature(signature, signature_len, message, message_len, NULL, 0, secret_key);
+#endif /* OQS_DIST_BUILD */
+#else
+	return (OQS_STATUS) PQCP_MLDSA_NATIVE_MLDSA44_C_signature(signature, signature_len, message, message_len, NULL, 0, secret_key);
 #endif
+}
+
+OQS_API OQS_STATUS OQS_SIG_ml_dsa_44_verify_with_ctx_str(const uint8_t *message, size_t message_len, const uint8_t *signature, size_t signature_len, const uint8_t *ctx_str, size_t ctx_str_len, const uint8_t *public_key) {
+#if defined(OQS_ENABLE_SIG_ml_dsa_44_aarch64)
+#if defined(OQS_DIST_BUILD)
+	if (OQS_CPU_has_extension(OQS_CPU_EXT_ARM_NEON)) {
+		return (OQS_STATUS) PQCP_MLDSA_NATIVE_MLDSA44_AARCH64_verify(signature, signature_len, message, message_len, NULL, 0, secret_key);
+	} else {
+		return (OQS_STATUS) PQCP_MLDSA_NATIVE_MLDSA44_C_verify(signature, signature_len, message, message_len, NULL, 0, secret_key);
+	}
+#else
+	return (OQS_STATUS) PQCP_MLDSA_NATIVE_MLDSA44_AARCH64_verify(signature, signature_len, message, message_len, NULL, 0, secret_key);
+#endif /* OQS_DIST_BUILD */
+#else
+	return (OQS_STATUS) PQCP_MLDSA_NATIVE_MLDSA44_C_verify(signature, signature_len, message, message_len, NULL, 0, secret_key);
+#endif
+}
+
+#endif /* OQS_ENABLE_SIG_ml_dsa_44 */
--- src/sig/ml_dsa/sig_ml_dsa_65.c
+++ src/sig/ml_dsa/sig_ml_dsa_65.c
@@ -33,11 +33,8 @@ OQS_SIG *OQS_SIG_ml_dsa_65_new(void) {
 }
 
 extern int PQCP_MLDSA_NATIVE_MLDSA65_C_keypair(uint8_t *pk, uint8_t *sk);
-
 extern int PQCP_MLDSA_NATIVE_MLDSA65_C_signature(uint8_t *sig, size_t *siglen, const uint8_t *m, size_t mlen, const uint8_t *ctx, size_t ctxlen, const uint8_t *sk);
 extern int PQCP_MLDSA_NATIVE_MLDSA65_C_verify(const uint8_t *sig, size_t siglen, const uint8_t *m, size_t mlen, const uint8_t *ctx, size_t ctxlen, const uint8_t *pk);
-
-
 
 
 OQS_API OQS_STATUS OQS_SIG_ml_dsa_65_keypair(uint8_t *public_key, uint8_t *secret_key) {
@@ -45,26 +42,19 @@ OQS_API OQS_STATUS OQS_SIG_ml_dsa_65_keypair(uint8_t *public_key, uint8_t *secre
 }
 
 OQS_API OQS_STATUS OQS_SIG_ml_dsa_65_sign(uint8_t *signature, size_t *signature_len, const uint8_t *message, size_t message_len, const uint8_t *secret_key) {
-
-
-
-
 	return (OQS_STATUS) PQCP_MLDSA_NATIVE_MLDSA65_C_signature(signature, signature_len, message, message_len, NULL, 0, secret_key);
-
-
 }
 
 OQS_API OQS_STATUS OQS_SIG_ml_dsa_65_verify(const uint8_t *message, size_t message_len, const uint8_t *signature, size_t signature_len, const uint8_t *public_key) {
-
-
-
-
-
 	return (OQS_STATUS) PQCP_MLDSA_NATIVE_MLDSA65_C_verify(signature, signature_len, message, message_len, NULL, 0, public_key);
-
-
 }
 
+OQS_API OQS_STATUS OQS_SIG_ml_dsa_65_sign_with_ctx_str(uint8_t *signature, size_t *signature_len, const uint8_t *message, size_t message_len, const uint8_t *ctx_str, size_t ctx_str_len, const uint8_t *secret_key) {
+	return (OQS_STATUS) PQCP_MLDSA_NATIVE_MLDSA65_C_signature(signature, signature_len, message, message_len, NULL, 0, secret_key);
+}
 
+OQS_API OQS_STATUS OQS_SIG_ml_dsa_65_verify_with_ctx_str(const uint8_t *message, size_t message_len, const uint8_t *signature, size_t signature_len, const uint8_t *ctx_str, size_t ctx_str_len, const uint8_t *public_key) {
+	return (OQS_STATUS) PQCP_MLDSA_NATIVE_MLDSA65_C_verify(signature, signature_len, message, message_len, NULL, 0, secret_key);
+}
 
-#endif
+#endif /* OQS_ENABLE_SIG_ml_dsa_65 */
