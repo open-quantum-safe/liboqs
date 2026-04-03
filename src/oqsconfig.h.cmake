85e318520c9ea8d9c7aa0d380f48af61054763c2
Ganyu (Bruce) Xu <g66xu@uwaterloo.ca>
2026-04-03 12:22:04 -0400

    Fixed HQC PRNG domain; renamed HQC-128/192/256 to HQC-1/3/5
    Also: add newline at the end of kats.json
    
    Now KATs all pass for HQC-1/3/5. Need to work on namespacing next.
    
    Signed-off-by: Ganyu (Bruce) Xu <g66xu@uwaterloo.ca>

--- src/oqsconfig.h.cmake
+++ src/oqsconfig.h.cmake
@@ -134,9 +134,9 @@
 #cmakedefine OQS_ENABLE_KEM_classic_mceliece_8192128f_avx2 1
 
 #cmakedefine OQS_ENABLE_KEM_HQC 1
-#cmakedefine OQS_ENABLE_KEM_hqc_128 1
-#cmakedefine OQS_ENABLE_KEM_hqc_192 1
-#cmakedefine OQS_ENABLE_KEM_hqc_256 1
+#cmakedefine OQS_ENABLE_KEM_hqc_1 1
+#cmakedefine OQS_ENABLE_KEM_hqc_3 1
+#cmakedefine OQS_ENABLE_KEM_hqc_5 1
 
 #cmakedefine OQS_ENABLE_KEM_KYBER 1
 #cmakedefine OQS_ENABLE_KEM_kyber_512 1
