51e8e70965de4d9b83856343b189ca74a495b53e
Ganyu (Bruce) Xu <g66xu@uwaterloo.ca>
2026-04-07 11:10:21 -0400

    Partial fix for namespacing
    Signed-off-by: Ganyu (Bruce) Xu <g66xu@uwaterloo.ca>

--- src/kem/hqc/xuganyu96-hqc_hqc-1_ref/fft.h
+++ src/kem/hqc/xuganyu96-hqc_hqc-1_ref/fft.h
@@ -9,7 +9,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
-void fft(uint16_t *w, const uint16_t *f, size_t f_coeffs);
-void fft_retrieve_error_poly(uint8_t *error, const uint16_t *w);
+void hqc_fft(uint16_t *w, const uint16_t *f, size_t f_coeffs);
+void hqc_fft_retrieve_error_poly(uint8_t *error, const uint16_t *w);
 
 #endif  // HQC_FFT_H
