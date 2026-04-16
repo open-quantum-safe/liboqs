d33481dd8fb5caa90920923ecc6035f8dbddf00a
Ganyu (Bruce) Xu <g66xu@uwaterloo.ca>
2026-04-16 15:23:29 -0400

    [skip ci] Does 1UL really mean 32-bit on Windows x86?
    Signed-off-by: Ganyu (Bruce) Xu <g66xu@uwaterloo.ca>

--- src/kem/hqc/xuganyu96-hqc_hqc-5_ref/parameters.h
+++ src/kem/hqc/xuganyu96-hqc_hqc-5_ref/parameters.h
@@ -9,7 +9,7 @@
 #include "api.h"
 
 #define CEIL_DIVIDE(a, b) (((a) / (b)) + ((a) % (b) == 0 ? 0 : 1)) /*!< Divide a by b and ceil the result*/
-#define BITMASK(a, size)  ((1UL << (a % size)) - 1)                /*!< Create a mask*/
+#define BITMASK(a, size)  ((1ULL << (a % size)) - 1)                /*!< Create a mask*/
 
 #define PARAM_N                     57637       ///< Define the parameter n of the scheme
 #define PARAM_N1                    90          ///< Define the parameter n1 of the scheme (length of Reed-Solomon code)
