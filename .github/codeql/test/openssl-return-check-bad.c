81255c48b046c79de334853e5c10f1cd344f86fe
Vishnu Ajith <27vishnu07@gmail.com>
2026-07-15 00:06:04 +0100

    ci: add SPDX header to CodeQL test file
    Signed-off-by: Vishnu Ajith <27vishnu07@gmail.com>

--- .github/codeql/test/openssl-return-check-bad.c
+++ .github/codeql/test/openssl-return-check-bad.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 /* expect-fail: EVP return value not checked via OQS_OPENSSL_GUARD */
 #include <openssl/evp.h>
 void bad_example(EVP_MD_CTX *ctx, const EVP_MD *md) {
