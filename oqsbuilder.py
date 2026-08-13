934b947b98271708c7ec1ca175294af485dfaa3b
Ganyu (Bruce) Xu <g66xu@uwaterloo.ca>
2026-08-13 11:40:09 -0400

    [skip ci] start relational checks
    Signed-off-by: Ganyu (Bruce) Xu <g66xu@uwaterloo.ca>

--- oqsbuilder.py
+++ oqsbuilder.py
@@ -3,7 +3,6 @@
 import abc
 from enum import Enum, EnumType
 import os
-import pprint
 from typing import Any, Mapping
 import warnings
 
@@ -18,8 +17,7 @@ LIBOQS_DIR = _liboqs_dir
 
 class OQSBuilderConfig:
     def __init__(self, oqs_meta_path: str | None = None):
-        self.oqs_meta_path = oqs_meta_path or os.path.join(LIBOQS_DIR,
-                                                           "OQS_META.yml")
+        self.oqs_meta_path = oqs_meta_path or os.path.join(LIBOQS_DIR, "OQS_META.yml")
         self.demo_algfamilies = ["demo_alg"]
 
 
@@ -28,6 +26,7 @@ class FieldValidator(abc.ABC):
     def validate(self, value: Any) -> bool:
         """Return true if value satisfies all requirements of a field"""
         raise NotImplementedError()
+
 
 class Fields:
     REQUIRED = True
@@ -58,7 +57,7 @@ class Fields:
             if not self.allowempty:
                 return len(value) > 0
             return True
-        
+
     class Enumerated(FieldValidator):
         def __init__(self, enum: EnumType):
             self.enum = enum
@@ -90,8 +89,11 @@ class Fields:
             return all([self.validator.validate(data) for data in value.values()])
 
     class Mapping(FieldValidator):
-        def __init__(self, validators: Mapping[str, tuple[bool, FieldValidator]],
-                     allowextrakeys: bool = False):
+        def __init__(
+            self,
+            validators: Mapping[str, tuple[bool, FieldValidator]],
+            allowextrakeys: bool = False,
+        ):
             self.validators = validators
             self.allowextrakeys = allowextrakeys
 
@@ -118,15 +120,18 @@ class Fields:
         def validate(self, value: Any) -> bool:
             return any([dator.validate(value) for dator in self.validators])
 
+
 class AlgTypes(Enum):
     Kem = "kem"
     Sig = "sig"
     StflSig = "stfl_sig"
 
+
 class OQSSupportTiers(Enum):
     Core = 1
     Supported = 2
     Community = 3
+
 
 class UArch(Enum):
     Portable = "portable"
@@ -135,9 +140,11 @@ class UArch(Enum):
     Cuda = "cuda"
     Icicle = "icicle"
 
+
 class SupportedOS(Enum):
     Linux = "Linux"
     Darwin = "Darwin"
+
 
 class CpuFeatures(Enum):
     AVX2 = "avx2"
@@ -145,11 +152,13 @@ class CpuFeatures(Enum):
     POPCNT = "popcnt"
     NEON = "asimd"
 
+
 class CmakeInterfaces(Enum):
     Public = "public"
     Private = "private"
     Interface = "interface"
     Target = "target"
+
 
 class SecStrength(Enum):
     Cat1 = 1
@@ -158,65 +167,78 @@ class SecStrength(Enum):
     Cat4 = 4
     Cat5 = 5
 
-UPSTREAM_DATOR = Fields.Mapping({
-    "git_url": (Fields.REQUIRED, Fields.Text()),
-    "git_commit": (Fields.REQUIRED, Fields.Text()),
-    "git_branch": (Fields.OPTIONAL, Fields.Text()),
-    "patches": (Fields.OPTIONAL, Fields.Array(Fields.Text())),
-    "jasminc": (Fields.OPTIONAL, Fields.Text()),
-    "post_patches": (Fields.OPTIONAL, Fields.Text()),
-})
 
-KEM_PARAM_DATOR = Fields.Mapping({
-    "security-strength": (Fields.REQUIRED, Fields.Enumerated(SecStrength)), 
-    "security-model": (Fields.REQUIRED, Fields.Text()), 
-    "length-public-key": (Fields.REQUIRED, Fields.Integer()), 
-    "length-ciphertext": (Fields.REQUIRED, Fields.Integer()), 
-    "length-secret-key": (Fields.REQUIRED, Fields.Integer()), 
-    "length-shared-secret": (Fields.REQUIRED, Fields.Integer()), 
-    "length-keypair-seed": (Fields.OPTIONAL, Fields.Integer()), 
-    "length-encaps-seed": (Fields.OPTIONAL, Fields.Integer()), 
-    "nistkat-sha256": (Fields.OPTIONAL, Fields.Text()), 
-    "enable_by": (Fields.REQUIRED, Fields.Text()), 
-    # FIX: BIKE's implementations do not fit into the model of implementations
-    #      so we cannot require default-implementation from parameter sets.
-    #      Instead, we will need to check default-implementation in relational
-    #      checks.
-    "default-implementation": (Fields.OPTIONAL, Fields.Text()), 
-    "memopt-implementation": (Fields.OPTIONAL, Fields.Text()), 
-})
+UPSTREAM_DATOR = Fields.Mapping(
+    {
+        "git_url": (Fields.REQUIRED, Fields.Text()),
+        "git_commit": (Fields.REQUIRED, Fields.Text()),
+        "git_branch": (Fields.OPTIONAL, Fields.Text()),
+        "patches": (Fields.OPTIONAL, Fields.Array(Fields.Text())),
+        "jasminc": (Fields.OPTIONAL, Fields.Text()),
+        "post_patches": (Fields.OPTIONAL, Fields.Text()),
+    }
+)
 
-SIG_PARAM_DATOR = Fields.Mapping({
-    "security-strength": (Fields.REQUIRED, Fields.Enumerated(SecStrength)), 
-    "security-model": (Fields.REQUIRED, Fields.Text()), 
-    "length-public-key": (Fields.REQUIRED, Fields.Integer()), 
-    "length-secret-key": (Fields.REQUIRED, Fields.Integer()), 
-    "length-signature": (Fields.REQUIRED, Fields.Integer()), 
-    "length-mu": (Fields.OPTIONAL, Fields.Integer()), 
-    "nistkat-sha256": (Fields.REQUIRED, Fields.Text()), 
-    "enable_by": (Fields.REQUIRED, Fields.Text()), 
-    "default-implementation": (Fields.REQUIRED, Fields.Text()), 
-    "memopt-implementation": (Fields.OPTIONAL, Fields.Text()), 
-})
+KEM_PARAM_DATOR = Fields.Mapping(
+    {
+        "security-strength": (Fields.REQUIRED, Fields.Enumerated(SecStrength)),
+        "security-model": (Fields.REQUIRED, Fields.Text()),
+        "length-public-key": (Fields.REQUIRED, Fields.Integer()),
+        "length-ciphertext": (Fields.REQUIRED, Fields.Integer()),
+        "length-secret-key": (Fields.REQUIRED, Fields.Integer()),
+        "length-shared-secret": (Fields.REQUIRED, Fields.Integer()),
+        "length-keypair-seed": (Fields.OPTIONAL, Fields.Integer()),
+        "length-encaps-seed": (Fields.OPTIONAL, Fields.Integer()),
+        "nistkat-sha256": (Fields.OPTIONAL, Fields.Text()),
+        "enable_by": (Fields.REQUIRED, Fields.Text()),
+        # FIX: BIKE's implementations do not fit into the model of implementations
+        #      so we cannot require default-implementation from parameter sets.
+        #      Instead, we will need to check default-implementation in relational
+        #      checks.
+        "default-implementation": (Fields.OPTIONAL, Fields.Text()),
+        "memopt-implementation": (Fields.OPTIONAL, Fields.Text()),
+    }
+)
+
+SIG_PARAM_DATOR = Fields.Mapping(
+    {
+        "security-strength": (Fields.REQUIRED, Fields.Enumerated(SecStrength)),
+        "security-model": (Fields.REQUIRED, Fields.Text()),
+        "length-public-key": (Fields.REQUIRED, Fields.Integer()),
+        "length-secret-key": (Fields.REQUIRED, Fields.Integer()),
+        "length-signature": (Fields.REQUIRED, Fields.Integer()),
+        "length-mu": (Fields.OPTIONAL, Fields.Integer()),
+        "nistkat-sha256": (Fields.REQUIRED, Fields.Text()),
+        "enable_by": (Fields.REQUIRED, Fields.Text()),
+        "default-implementation": (Fields.REQUIRED, Fields.Text()),
+        "memopt-implementation": (Fields.OPTIONAL, Fields.Text()),
+    }
+)
 
 KEM_OR_SIG_PARAM_DATOR = Fields.Union({KEM_PARAM_DATOR, SIG_PARAM_DATOR})
 
-UPSTREAMSRC_OR_COMMONSRC_DATOR = Fields.Union({
-    # Upstream sources
-    Fields.Mapping({
-        "upstream": (Fields.REQUIRED, Fields.Text()),
-        "base_dir": (Fields.OPTIONAL, Fields.Text()),
-        "preserve_subdirs": (Fields.OPTIONAL, Fields.Boolean()),
-        "files": (
-            Fields.REQUIRED,
-            Fields.Array(Fields.Text()),
+UPSTREAMSRC_OR_COMMONSRC_DATOR = Fields.Union(
+    {
+        # Upstream sources
+        Fields.Mapping(
+            {
+                "upstream": (Fields.REQUIRED, Fields.Text()),
+                "base_dir": (Fields.OPTIONAL, Fields.Text()),
+                "preserve_subdirs": (Fields.OPTIONAL, Fields.Boolean()),
+                "files": (
+                    Fields.REQUIRED,
+                    Fields.Array(Fields.Text()),
+                ),
+            }
         ),
-    }),
-    # Family common sources
-    Fields.Mapping({
-        "family_common": (Fields.REQUIRED, Fields.Text()),
-    }),
-})
+        # Family common sources
+        Fields.Mapping(
+            {
+                "family_common": (Fields.REQUIRED, Fields.Text()),
+            }
+        ),
+    }
+)
 
 COMMON_IMPL_MAPPING = {
     "version": (Fields.REQUIRED, Fields.Text()),
@@ -226,45 +248,57 @@ COMMON_IMPL_MAPPING = {
     "uarch": (Fields.REQUIRED, Fields.Enumerated(UArch)),
     "common-crypto": (
         Fields.OPTIONAL,
-        Fields.Mapping({
-            "AES": (Fields.OPTIONAL, Fields.Text()),
-            "SHA3": (Fields.OPTIONAL, Fields.Text()),
-        })
+        Fields.Mapping(
+            {
+                "AES": (Fields.OPTIONAL, Fields.Text()),
+                "SHA3": (Fields.OPTIONAL, Fields.Text()),
+            }
+        ),
     ),
-    "no-secret-dependent-branching-claimed": (Fields.REQUIRED,
-                                              Fields.Boolean()),
-    "no-secret-dependent-branching-checked-by-valgrind": (Fields.REQUIRED,
-                                                          Fields.Boolean()),
-    "large-stack-usage": (Fields.REQUIRED,
-                          Fields.Boolean()),
+    "no-secret-dependent-branching-claimed": (Fields.REQUIRED, Fields.Boolean()),
+    "no-secret-dependent-branching-checked-by-valgrind": (
+        Fields.REQUIRED,
+        Fields.Boolean(),
+    ),
+    "large-stack-usage": (Fields.REQUIRED, Fields.Boolean()),
     "os": (Fields.OPTIONAL, Fields.Array(Fields.Enumerated(SupportedOS))),
-    "cpufeatures": (Fields.OPTIONAL,
-                    Fields.Array(Fields.Enumerated(CpuFeatures))),
+    "cpufeatures": (Fields.OPTIONAL, Fields.Array(Fields.Enumerated(CpuFeatures))),
     "sources": (Fields.REQUIRED, Fields.Array(UPSTREAMSRC_OR_COMMONSRC_DATOR)),
     "includes": (
         Fields.OPTIONAL,
-        Fields.Array(Fields.Mapping({
-            "scope": (Fields.REQUIRED, Fields.Enumerated(CmakeInterfaces)),
-            "dir": (Fields.REQUIRED, Fields.Text()),
-        }))
+        Fields.Array(
+            Fields.Mapping(
+                {
+                    "scope": (Fields.REQUIRED, Fields.Enumerated(CmakeInterfaces)),
+                    "dir": (Fields.REQUIRED, Fields.Text()),
+                }
+            )
+        ),
     ),
     "old_gas_syntax_if_darwin": (Fields.OPTIONAL, Fields.Boolean()),
     "compile_opts": (
         Fields.OPTIONAL,
-        Fields.Array(Fields.Mapping({
-            "scope": (Fields.REQUIRED, Fields.Enumerated(CmakeInterfaces)),
-            "opts": (Fields.REQUIRED, Fields.Text()),
-        })),
+        Fields.Array(
+            Fields.Mapping(
+                {
+                    "scope": (Fields.REQUIRED, Fields.Enumerated(CmakeInterfaces)),
+                    "opts": (Fields.REQUIRED, Fields.Text()),
+                }
+            )
+        ),
     ),
-    "link_libs": (Fields.OPTIONAL,
-                  Fields.Array(Fields.Text(), allowempty=False)),
+    "link_libs": (Fields.OPTIONAL, Fields.Array(Fields.Text(), allowempty=False)),
     "set_properties": (
         Fields.OPTIONAL,
-        Fields.Array(Fields.Mapping({
-            "scope": (Fields.REQUIRED, Fields.Enumerated(CmakeInterfaces)),
-            "name": (Fields.REQUIRED, Fields.Text()),
-            "value": (Fields.REQUIRED, Fields.Text()),
-        }))
+        Fields.Array(
+            Fields.Mapping(
+                {
+                    "scope": (Fields.REQUIRED, Fields.Enumerated(CmakeInterfaces)),
+                    "name": (Fields.REQUIRED, Fields.Text()),
+                    "value": (Fields.REQUIRED, Fields.Text()),
+                }
+            )
+        ),
     ),
 }
 
@@ -290,51 +324,84 @@ SIG_IMPL_DATOR = Fields.Mapping(COMMON_IMPL_MAPPING | SIG_API_MAPPING)
 
 KEM_OR_SIG_IMPL_DATOR = Fields.Union({KEM_IMPL_DATOR, SIG_IMPL_DATOR})
 
-COMMON_SRC_DATOR = Fields.Mapping({
-    "upstream": (Fields.REQUIRED, Fields.Text()),
-    "destdir": (Fields.OPTIONAL, Fields.Text()),
-    "upstream_base_dir": (Fields.OPTIONAL, Fields.Text()),
-    "files": (Fields.REQUIRED, Fields.Array(Fields.Text())),
-})
+COMMON_SRC_DATOR = Fields.Mapping(
+    {
+        "upstream": (Fields.REQUIRED, Fields.Text()),
+        "destdir": (Fields.OPTIONAL, Fields.Text()),
+        "upstream_base_dir": (Fields.OPTIONAL, Fields.Text()),
+        "files": (Fields.REQUIRED, Fields.Array(Fields.Text())),
+    }
+)
 
-ALGFAMILY_DATOR = Fields.Mapping({
-    "displayname": (Fields.REQUIRED, Fields.Text()),
-    "algtype": (Fields.REQUIRED, Fields.Enumerated(AlgTypes)),
-    "principal-submitters": (Fields.REQUIRED,
-                             Fields.Array(Fields.Text(), False)),
-    "auxiliary-submitters": (Fields.OPTIONAL,
-                             Fields.Array(Fields.Text())),
-    "crypto-assumption": (Fields.REQUIRED, Fields.Text()),
-    "website": (Fields.REQUIRED, Fields.Text()),
-    "nist-round": (Fields.REQUIRED, Fields.Text()),
-    "spec-version": (Fields.REQUIRED, Fields.Text()),
-    "standardization-status": (Fields.REQUIRED, Fields.Text()),
-    "upstream-maintenance": (Fields.REQUIRED, Fields.Text()),
-    "upstream-statement-url": (Fields.OPTIONAL, Fields.Text()),
-    # TODO: relational checks are too complex to implement in Fields; instead
-    #       they will be checked at parsing
-    "primary-upstream": (Fields.REQUIRED, Fields.Text()),
-    "oqs-support-tier": (Fields.REQUIRED, Fields.Enumerated(OQSSupportTiers)),
-    "upstream-ancestors": (Fields.OPTIONAL, Fields.Array(Fields.Text())),
-    "advisories": (Fields.OPTIONAL, Fields.Array(Fields.Text())),
-    # TODO: another relational check: if algtype is kem, then parameters must
-    #       be keyed array of kem parameters.
-    "parameters": (Fields.REQUIRED, 
-                   Fields.KeyedArray(KEM_OR_SIG_PARAM_DATOR)),
-    "common_src": (Fields.OPTIONAL,
-                   Fields.KeyedArray(COMMON_SRC_DATOR)),
-    "implementations-switch-on-runtime-cpu-features": (Fields.OPTIONAL,
-                                                       Fields.Boolean()),
-    "implementations": (Fields.OPTIONAL,
-                        Fields.KeyedArray(KEM_OR_SIG_IMPL_DATOR)),
-})
+ALGFAMILY_DATOR = Fields.Mapping(
+    {
+        "displayname": (Fields.REQUIRED, Fields.Text()),
+        "algtype": (Fields.REQUIRED, Fields.Enumerated(AlgTypes)),
+        "principal-submitters": (Fields.REQUIRED, Fields.Array(Fields.Text(), False)),
+        "auxiliary-submitters": (Fields.OPTIONAL, Fields.Array(Fields.Text())),
+        "crypto-assumption": (Fields.REQUIRED, Fields.Text()),
+        "website": (Fields.REQUIRED, Fields.Text()),
+        "nist-round": (Fields.REQUIRED, Fields.Text()),
+        "spec-version": (Fields.REQUIRED, Fields.Text()),
+        "standardization-status": (Fields.REQUIRED, Fields.Text()),
+        "upstream-maintenance": (Fields.REQUIRED, Fields.Text()),
+        "upstream-statement-url": (Fields.OPTIONAL, Fields.Text()),
+        # TODO: relational checks are too complex to implement in Fields; instead
+        #       they will be checked at parsing
+        "primary-upstream": (Fields.REQUIRED, Fields.Text()),
+        "oqs-support-tier": (Fields.REQUIRED, Fields.Enumerated(OQSSupportTiers)),
+        "upstream-ancestors": (Fields.OPTIONAL, Fields.Array(Fields.Text())),
+        "advisories": (Fields.OPTIONAL, Fields.Array(Fields.Text())),
+        # TODO: another relational check: if algtype is kem, then parameters must
+        #       be keyed array of kem parameters.
+        "parameters": (Fields.REQUIRED, Fields.KeyedArray(KEM_OR_SIG_PARAM_DATOR)),
+        "common_src": (Fields.OPTIONAL, Fields.KeyedArray(COMMON_SRC_DATOR)),
+        "implementations-switch-on-runtime-cpu-features": (
+            Fields.OPTIONAL,
+            Fields.Boolean(),
+        ),
+        "implementations": (Fields.OPTIONAL, Fields.KeyedArray(KEM_OR_SIG_IMPL_DATOR)),
+    }
+)
 
 
-OQS_META_DATOR = Fields.Mapping({
-    "schema_version": (Fields.REQUIRED, Fields.Integer()),
-    "upstreams": (Fields.REQUIRED, Fields.KeyedArray(UPSTREAM_DATOR)),
-    "algfamilies": (Fields.REQUIRED, Fields.KeyedArray(ALGFAMILY_DATOR)),
-})
+OQS_META_DATOR = Fields.Mapping(
+    {
+        "schema_version": (Fields.REQUIRED, Fields.Integer()),
+        "upstreams": (Fields.REQUIRED, Fields.KeyedArray(UPSTREAM_DATOR)),
+        "algfamilies": (Fields.REQUIRED, Fields.KeyedArray(ALGFAMILY_DATOR)),
+    }
+)
+
+
+class RelationalChecks:
+    @staticmethod
+    def impl_key_exists(family_meta: dict) -> bool:
+        impl_keys = family_meta.get("implementations", {}).keys()
+        for param_key, param_meta in family_meta.get("parameters", {}):
+            default_impl_key = param_meta.get("default-implementation", None)
+            if default_impl_key and (default_impl_key not in impl_keys):
+                print(f"ERROR: {param_key} default impl {default_impl_key} invalid")
+                return False
+        return True
+
+    @staticmethod
+    def param_key_exists(family_meta: dict) -> bool:
+        """The "param" field in an implementation must match a param key in the same
+        family
+        """
+        raise NotImplementedError()
+
+    @staticmethod
+    def common_src_key_exists(family_meta: dict) -> bool:
+        """If an implementation uses a set of common source files, the reference
+        must match a common_src_key in the same family
+        """
+        raise NotImplementedError()
+
+    @staticmethod
+    def run_all() -> bool:
+        raise NotImplementedError()
 
 
 if __name__ == "__main__":
