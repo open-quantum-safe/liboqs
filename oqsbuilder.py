761c7aedf8a986e727e153ab6e39cd305cb7b8e5
Ganyu (Bruce) Xu <g66xu@uwaterloo.ca>
2026-08-13 16:21:33 -0400

    [skip ci] can clone and patch
    Signed-off-by: Ganyu (Bruce) Xu <g66xu@uwaterloo.ca>

--- oqsbuilder.py
+++ oqsbuilder.py
@@ -6,8 +6,10 @@ import abc
 from enum import Enum, EnumType
 import logging
 import os
+import subprocess
 import sys
 from typing import Any, Mapping
+from tempfile import TemporaryDirectory
 import warnings
 
 import yaml
@@ -15,7 +17,7 @@ import yaml
 logger = logging.getLogger(__name__)
 handler = logging.StreamHandler(sys.stdout)
 handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s: %(message)s"))
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 logger.addHandler(handler)
 
 _liboqs_dir = os.getenv("LIBOQS_DIR")
@@ -23,11 +25,27 @@ if not _liboqs_dir:
     _liboqs_dir = os.getcwd()
     warnings.warn("LIBOQS_DIR not set; defaulting to cwd")
 LIBOQS_DIR = _liboqs_dir
+DEFAULT_OQS_META_PATH = os.path.join(LIBOQS_DIR, "OQS_META.yml")
+DEFAULT_PATCH_DIR = os.path.join(LIBOQS_DIR, "scripts", "copy_from_upstream", "patches")
 
 
 class OQSBuilderConfig:
-    def __init__(self, oqs_meta_path: str | None = None):
-        self.oqs_meta_path = oqs_meta_path or os.path.join(LIBOQS_DIR, "OQS_META.yml")
+    def __init__(
+        self,
+        oqs_meta_path: str | None = None,
+        upstreams_dir: str | None = None,
+        patch_dir: str | None = None,
+    ):
+        """
+        :param upstreams_dir: Specify a directory to clone upstream repositories
+            into. Defaults to $LIBOQS_DIR
+        :param patch_dir: Specify the directory that hosts all the patch files.
+            Defaults to LIBOQS_DIR/scripts/copy_from_upstream/patches
+        """
+        self.oqs_meta_path = oqs_meta_path or DEFAULT_OQS_META_PATH
+        self.upstreams_dir = upstreams_dir or LIBOQS_DIR
+        self.patch_dir = patch_dir or DEFAULT_PATCH_DIR
+        self.delete_upstreams = True
         self.demo_algfamilies = ["demo_alg"]
 
 
@@ -117,10 +135,10 @@ class Fields:
             for key, (required, dator) in self.validators.items():
                 subtarget = value.get(key, None)
                 if (subtarget is not None) and (not dator.validate(subtarget)):
-                    logger.error(f"failed to validate {key}")
+                    logger.error("failed to validate %s", key)
                     return False
                 if (subtarget is None) and required:
-                    logger.error(f"missing key {key}")
+                    logger.error("missing key %s", key)
                     return False
             return True
 
@@ -392,7 +410,7 @@ class RelationalChecks:
         for param_key, param_meta in family_meta.get("parameters", {}):
             default_impl_key = param_meta.get("default-implementation", None)
             if default_impl_key and (default_impl_key not in impl_keys):
-                logger.error(f"{param_key} default impl {default_impl_key} invalid")
+                logger.error("%s default impl %s invalid", param_key, default_impl_key)
                 return False
         return True
 
@@ -411,8 +429,94 @@ class RelationalChecks:
         raise NotImplementedError()
 
     @staticmethod
-    def run_all() -> bool:
+    def run_all(oqs_meta: dict) -> bool:
         raise NotImplementedError()
+
+
+def run_subprocess(cmd: list[str], cwd: str | None = None, expected_ret=0):
+    """Run a command in a sub-process, pipe stdout and stderr to logger."""
+    logger.debug("Subprocess call: `%s`", " ".join(cmd))
+
+    proc = subprocess.Popen(
+        cmd, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True
+    )
+    assert proc.stdout is not None
+
+    for line in proc.stdout:
+        logger.debug("%s", line.rstrip())
+
+    ret = proc.wait()
+    if ret != expected_ret:
+        raise subprocess.CalledProcessError(ret, cmd)
+
+
+class Upstream:
+    def __init__(
+        self,
+        upstream_key: str,
+        git_url: str,
+        git_commit: str,
+        patch_full_paths: list[str],
+        post_patch: str | None,
+    ):
+        self.upstream_key = upstream_key
+        self.git_url = git_url
+        self.git_commit = git_commit
+        self.patch_full_paths = patch_full_paths
+        self.post_patch = post_patch
+
+        self._dir = None
+        self._patched = False
+        self._posted = False
+
+    @staticmethod
+    def from_dict(patch_dir: str, key: str, meta: dict):
+        patch_full_paths = []
+        for patch_name in meta.get("patches", []):
+            patch_path = os.path.join(patch_dir, patch_name)
+            if not os.path.isfile(patch_path):
+                raise FileNotFoundError(f"Cannot find patch {patch_path}")
+            patch_full_paths.append(patch_path)
+
+        return Upstream(
+            key,
+            meta["git_url"],
+            meta["git_commit"],
+            patch_full_paths,
+            meta.get("post_patch", None),
+        )
+
+    def clone(self, parent_dir: str):
+        """Clone the specified commit into {{ parent_dir }}/{{ upstream_key }},
+        then set self._dir to this path, indicating successful cloning
+        """
+        upstream_dir = os.path.join(parent_dir, self.upstream_key)
+        os.mkdir(upstream_dir)
+
+        run_subprocess(["git", "init"], upstream_dir)
+        run_subprocess(["git", "remote", "add", "origin", self.git_url], upstream_dir)
+        run_subprocess(
+            ["git", "fetch", "--depth=1", "origin", self.git_commit], upstream_dir
+        )
+        run_subprocess(["git", "checkout", "--detach", "FETCH_HEAD"], upstream_dir)
+
+        self._dir = upstream_dir
+
+    def patch(self):
+        if self.patch_full_paths:
+            print(self.patch_full_paths)
+            run_subprocess(
+                ["git", "apply", "--whitespace=fix"] + self.patch_full_paths, self._dir
+            )
+
+
+def clone_upstreams(upstreams_dir: str, patch_dir: str, upstreams_meta: dict):
+    for upstream_key, upstream_meta in upstreams_meta.items():
+        logger.info("Cloning %s", upstream_key)
+        upstream = Upstream.from_dict(patch_dir, upstream_key, upstream_meta)
+        upstream.clone(upstreams_dir)
+        upstream.patch()
+        # upstream.post()
 
 
 if __name__ == "__main__":
@@ -423,6 +527,21 @@ if __name__ == "__main__":
     for demo_algfamily in builderconfig.demo_algfamilies:
         demo_algfamily_meta = oqs_meta["algfamilies"].pop(demo_algfamily)
         if demo_algfamily_meta:
-            logger.info(f"Removed demo alg {demo_algfamily}")
+            logger.info("Removed demo alg %s", demo_algfamily)
 
-    assert OQS_META_DATOR.validate(oqs_meta), "Error"
+    if not OQS_META_DATOR.validate(oqs_meta):
+        logger.error("%s failed schema validation", builderconfig.oqs_meta_path)
+    # if not RelationalChecks.run_all(oqs_meta):
+    #     logger.error("%s failed relational checks", builderconfig.oqs_meta_path)
+
+    with TemporaryDirectory(
+        dir=builderconfig.upstreams_dir,
+        prefix="_upstreams_",
+        delete=builderconfig.delete_upstreams,
+    ) as upstreams_dir:
+        logger.info("Cloning repositories into %s", upstreams_dir)
+        upstreams = clone_upstreams(
+            upstreams_dir, builderconfig.patch_dir, oqs_meta["upstreams"]
+        )
+        # TODO: fill in remote metadata
+        # TODO: copy files
