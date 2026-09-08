67ca82d4535e33d8ec2f94707f7202754ec1c012
Ganyu (Bruce) Xu <g66xu@uwaterloo.ca>
2026-09-08 14:11:14 -0400

    .spdxignore is more in line with .gitignore
    Signed-off-by: Ganyu (Bruce) Xu <g66xu@uwaterloo.ca>

--- tests/test_spdx.sh
+++ tests/test_spdx.sh
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: MIT
 
 SOURCE_DIR="${BASH_SOURCE%/*}"
-IGNORE_FILE="$SOURCE_DIR/.spdx_ignore.txt"
+IGNORE_FILE="$SOURCE_DIR/.spdxignore"
 
 BD=${OQS_BUILD_DIR:-build}
 find . \( -name '*.[chsS]' -or -name '*.cmake' -or -name '*.py' -or -name '*.sh' -or -name 'CMakeLists.txt' \) -and -type f \
