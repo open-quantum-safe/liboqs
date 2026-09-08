356b7e58c1b8ff174685228ef35c63e6cdb4fc3a
Owen Waldron <owen.r.waldron@gmail.com>
2026-09-08 11:29:05 -0400

    Added spdx-ignore.txt
    Signed-off-by: Owen Waldron <owen.r.waldron@gmail.com>

--- tests/test_spdx.sh
+++ tests/test_spdx.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 # SPDX-License-Identifier: MIT
 
+IGNORE_FILE='spdx-ignore.txt'
+if [ ! -f "$IGNORE_FILE" ]; then
+    echo "Error: $IGNORE_FILE not found. Please create it with a list of files to ignore."
+    exit 1
+fi
+
+# Strip blank lines
+IGNORE_PATTERNS=$(grep -v '^[[:space:]]*$' "$IGNORE_FILE")
+IGNORE_PATTERNS=${IGNORE_PATTERNS:-a^}
+
 BD=${OQS_BUILD_DIR:-build}
-find . \( -name '*.[chsS]' -or -name '*.cmake' -or -name '*.py' -or -name '*.sh' -or -name 'CMakeLists.txt' \) -and -type f | grep -v '/external/' | grep -v 'kem/.*/.*/.*' | grep -v 'sig/.*/.*/.*' | grep -v 'xkcp_low' | grep -v 'patches' | grep -v 'scripts/copy_from_upstream/repos' | grep -v `basename $BD` | xargs grep -L 'SPDX-License-Identifier' | sort
+find . \( -name '*.[chsS]' -or -name '*.cmake' -or -name '*.py' -or -name '*.sh' -or -name 'CMakeLists.txt' \) -and -type f | grep -v -f <(printf '%s\n' "$IGNORE_PATTERNS") | grep -v `basename $BD` | xargs grep -L 'SPDX-License-Identifier' | sort
