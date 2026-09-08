f123f1feff01bb7a809f62c1fd74b7599cf0c5a3
Owen Waldron <orpwaldr@uwaterloo.ca>
2026-09-08 12:13:41 -0400

    Simplified ignore pattern logic and moved spdx_ignore file.
    Signed-off-by: Owen Waldron <orpwaldr@uwaterloo.ca>

--- tests/test_spdx.sh
+++ tests/test_spdx.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 # SPDX-License-Identifier: MIT
 
-IGNORE_FILE='spdx-ignore.txt'
-if [ ! -f "$IGNORE_FILE" ]; then
-    echo "Error: $IGNORE_FILE not found. Please create it with a list of files to ignore."
-    exit 1
-fi
-
-# Strip blank lines
-IGNORE_PATTERNS=$(grep -v '^[[:space:]]*$' "$IGNORE_FILE")
-IGNORE_PATTERNS=${IGNORE_PATTERNS:-a^}
+SOURCE_DIR="${BASH_SOURCE%/*}"
+IGNORE_FILE="$SOURCE_DIR/.spdx_ignore.txt"
 
 BD=${OQS_BUILD_DIR:-build}
-find . \( -name '*.[chsS]' -or -name '*.cmake' -or -name '*.py' -or -name '*.sh' -or -name 'CMakeLists.txt' \) -and -type f | grep -v -f <(printf '%s\n' "$IGNORE_PATTERNS") | grep -v `basename $BD` | xargs grep -L 'SPDX-License-Identifier' | sort
+find . \( -name '*.[chsS]' -or -name '*.cmake' -or -name '*.py' -or -name '*.sh' -or -name 'CMakeLists.txt' \) -and -type f \
+    | grep -v -f $IGNORE_FILE \
+    | grep -v `basename $BD` \
+    | xargs grep -L 'SPDX-License-Identifier' \
+    | sort
